6e1764903ded2fcb9dfc3163ae3a85ec97bb7add
lgmartins <lgmartins@0e98fd9d-079e-457f-9c5c-d482923f975d>
2006-10-17 00:16:38 +0000

    tested peakClustering main function with Accumulator and Shredder objects
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@221 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/peakClustering.cpp
+++ src/peakClustering.cpp
@@ -26,7 +26,7 @@ int winSize_ = 512;
 int dopt = 64;
 int iopt = 64;
 // nb Sines
-int sopt = 6;
+int sopt = 80;
 // output buffer Size
 int bopt = 128;
 mrs_real gopt_ = 1.0;
@@ -39,10 +39,7 @@ bool auto_ = false;
 
 bool microphone_ = false;
 
-
 CommandLineOptions cmd_options;
-
-
 
 void 
 printUsage(string progName)
@@ -85,103 +82,109 @@ phasevocSeries(string sfName, mrs_natural N, mrs_natural Nw,
 							 mrs_natural D, mrs_natural I, mrs_real P, 
 							 string outsfname, mrs_natural accSize)
 {
-	cout << "phasevocSeries" << endl;
+	cout << "phasevocSeries2" << endl;
 	MarSystemManager mng;
 
-	// create the peakClustering network
+	// create the phasevocoder network
 	MarSystem* pvseries = mng.create("Series", "pvseries");
 
+	//create accumulator series
+	MarSystem* preNet = mng.create("Series", "preNet");
 	if (microphone_) 
-		pvseries->addMarSystem(mng.create("AudioSource", "src"));
+		preNet->addMarSystem(mng.create("AudioSource", "src"));
 	else 
-		pvseries->addMarSystem(mng.create("SoundFileSource", "src"));
+		preNet->addMarSystem(mng.create("SoundFileSource", "src"));
+	preNet->addMarSystem(mng.create("ShiftInput", "si"));
+	preNet->addMarSystem(mng.create("PvFold", "fo"));
+	preNet->addMarSystem(mng.create("Spectrum", "spk"));
+	preNet->addMarSystem(mng.create("PvConvert", "conv"));
+	//create accumulator
+	MarSystem* accumNet = mng.create("Accumulator", "accumNet");
+	accumNet->addMarSystem(preNet);
 
-	MarSystem* pvAccu = mng.create("Accumulator", "pvAccu");
-  MarSystem* preSeries = mng.create("Series", "preSeries");
-	preSeries->addMarSystem(mng.create("ShiftInput", "si"));
-	preSeries->addMarSystem(mng.create("PvFold", "fo"));
-	preSeries->addMarSystem(mng.create("Spectrum", "spk"));
-	preSeries->addMarSystem(mng.create("PvConvert", "conv"));
-	pvAccu->addMarSystem(preSeries);
-	
-	//pvAccu->addMarSystem(mng.create("ShiftInput", "si"));
-	pvseries->addMarSystem(pvAccu);
-	//pvAccu->addMarSystem(mng.create("ShiftInput", "si"));
-	
-	MarSystem* pvShred = mng.create("Shredder", "pvShred");
-	MarSystem* postSeries = mng.create("Series", "postSeries");
-
-	postSeries->addMarSystem(mng.create("PvOscBank", "ob"));
-	postSeries->addMarSystem(mng.create("ShiftOutput", "so"));
-	postSeries->addMarSystem(mng.create("Gain", "gain"));
-  pvShred->addMarSystem(postSeries);
-
-	pvseries->addMarSystem(pvShred);
-
+	//create Shredder series
+	MarSystem* postNet = mng.create("Series", "postNet");
+	postNet->addMarSystem(mng.create("PvOscBank", "ob"));
+	postNet->addMarSystem(mng.create("ShiftOutput", "so"));
+	postNet->addMarSystem(mng.create("Gain", "gain"));
 	MarSystem *dest;
 	if (outsfname == EMPTYSTRING) 
 		dest = new AudioSink("dest");
 	else
 	{
 		dest = new SoundFileSink("dest");
-		dest->updctrl("mrs_string/filename", outsfname);
+		//dest->updctrl("mrs_string/filename", outsfname);
 	}
-	pvseries->addMarSystem(dest);
+	postNet->addMarSystem(dest);
+	//create shredder
+	MarSystem* shredNet = mng.create("Shredder", "shredNet");
+	shredNet->addMarSystem(postNet);
 
+	//create the main network
+	pvseries->addMarSystem(accumNet);
+	pvseries->addMarSystem(shredNet);
+
+	////////////////////////////////////////////////////////////////
+	// update the controls
+	////////////////////////////////////////////////////////////////
+	pvseries->updctrl("Accumulator/accumNet/mrs_natural/nTimes", 1);
+	pvseries->updctrl("Shredder/shredNet/mrs_natural/nTimes", 1);
 
 	if (outsfname == EMPTYSTRING) 
-		pvseries->updctrl("AudioSink/dest/mrs_natural/bufferSize", bopt);
+		pvseries->updctrl("Shredder/shredNet/Series/postNet/AudioSink/dest/mrs_natural/bufferSize", bopt);
 
-
-	// update the controls
 	if (microphone_) 
 	{
-		pvseries->updctrl("AudioSource/src/mrs_natural/inSamples", D);
-		pvseries->updctrl("AudioSource/src/mrs_natural/inObservations", 1);
+		pvseries->updctrl("Accumulator/accumNet/Series/preNet/AudioSource/src/mrs_natural/inSamples", D);
+		pvseries->updctrl("Accumulator/accumNet/Series/preNet/AudioSource/src/mrs_natural/inObservations", 1);
 	}
 	else
 	{
-		pvseries->updctrl("SoundFileSource/src/mrs_string/filename", sfName);
-		pvseries->updctrl("SoundFileSource/src/mrs_natural/inSamples", D);
-		pvseries->updctrl("SoundFileSource/src/mrs_natural/inObservations", 1);
+		pvseries->updctrl("Accumulator/accumNet/Series/preNet/SoundFileSource/src/mrs_string/filename", sfName);
+		pvseries->updctrl("Accumulator/accumNet/Series/preNet/SoundFileSource/src/mrs_natural/inSamples", D);
+		pvseries->updctrl("Accumulator/accumNet/Series/preNet/SoundFileSource/src/mrs_natural/inObservations", 1);
+
+		// if audio output loop to infinity and beyond 
+		if (outsfname == EMPTYSTRING) 
+			pvseries->updctrl("Accumulator/accumNet/Series/preNet/SoundFileSource/src/mrs_real/repetitions", -1.0);
 	}
 
+	pvseries->updctrl("Accumulator/accumNet/Series/preNet/ShiftInput/si/mrs_natural/Decimation", D);
+	pvseries->updctrl("Accumulator/accumNet/Series/preNet/ShiftInput/si/mrs_natural/WindowSize", Nw);
+	pvseries->updctrl("Accumulator/accumNet/Series/preNet/PvFold/fo/mrs_natural/FFTSize", N);
+	pvseries->updctrl("Accumulator/accumNet/Series/preNet/PvFold/fo/mrs_natural/WindowSize", Nw);
+	pvseries->updctrl("Accumulator/accumNet/Series/preNet/PvFold/fo/mrs_natural/Decimation", D);
+	pvseries->updctrl("Accumulator/accumNet/Series/preNet/PvConvert/conv/mrs_natural/Decimation",D);      
+	pvseries->updctrl("Accumulator/accumNet/Series/preNet/PvConvert/conv/mrs_natural/Sinusoids", (mrs_natural) sopt);  
 
-	preSeries->updctrl("ShiftInput/si/mrs_natural/Decimation", D);
-	preSeries->updctrl("ShiftInput/si/mrs_natural/WindowSize", Nw);
-	preSeries->updctrl("PvFold/fo/mrs_natural/FFTSize", N);
-	preSeries->updctrl("PvFold/fo/mrs_natural/WindowSize", Nw);
-	preSeries->updctrl("PvFold/fo/mrs_natural/Decimation", D);
-	preSeries->updctrl("PvConvert/conv/mrs_natural/Decimation",D);      
-	preSeries->updctrl("PvConvert/conv/mrs_natural/Sinusoids", (mrs_natural) sopt);  
-	
-	pvseries->updctrl("Accumulator/pvAccu/mrs_natural/nTimes", accSize);
-	pvseries->updctrl("Shredder/pvShred/mrs_natural/nTimes", accSize);
+	pvseries->updctrl("Shredder/shredNet/Series/postNet/PvOscBank/ob/mrs_natural/Interpolation", I);
+	pvseries->updctrl("Shredder/shredNet/Series/postNet/PvOscBank/ob/mrs_real/PitchShift", P);
+	pvseries->updctrl("Shredder/shredNet/Series/postNet/ShiftOutput/so/mrs_natural/Interpolation", I);
+	pvseries->updctrl("Shredder/shredNet/Series/postNet/ShiftOutput/so/mrs_natural/WindowSize", Nw);      
+	pvseries->updctrl("Shredder/shredNet/Series/postNet/ShiftOutput/so/mrs_natural/Decimation", D);
+	pvseries->updctrl("Shredder/shredNet/Series/postNet/Gain/gain/mrs_real/gain", gopt_);
 
-	postSeries->updctrl("PvOscBank/ob/mrs_natural/Interpolation", I);
-	postSeries->updctrl("PvOscBank/ob/mrs_real/PitchShift", P);
-	postSeries->updctrl("ShiftOutput/so/mrs_natural/Interpolation", I);
-	postSeries->updctrl("ShiftOutput/so/mrs_natural/WindowSize", Nw);      
-	postSeries->updctrl("ShiftOutput/so/mrs_natural/Decimation", D);
-	postSeries->updctrl("Gain/gain/mrs_real/gain", gopt_);
+	dest->updctrl("mrs_string/filename", outsfname);//[!]
 
-	int i=0;
+	cout << *pvseries;
+
 	while(1)
 	{
-		cout << i++ << " ";
 		pvseries->tick();
 
-		if (!microphone_) 
-			if (pvseries->getctrl("SoundFileSource/src/mrs_bool/notEmpty").toBool() == false)
+		if (!microphone_)
+		{
+			bool temp = pvseries->getctrl("Accumulator/accumNet/Series/preNet/SoundFileSource/src/mrs_bool/notEmpty").toBool();
+			bool temp1 = accumNet->getctrl("Series/preNet/SoundFileSource/src/mrs_bool/notEmpty").toBool();
+			bool temp2 = preNet->getctrl("SoundFileSource/src/mrs_bool/notEmpty").toBool();
+			string fname = pvseries->getctrl("Accumulator/accumNet/Series/preNet/SoundFileSource/src/mrs_string/filename").toString();
+
+			///*bool*/ temp = pvseries->getctrl("Accumulator/accumNet/Series/preNet/SoundFileSource/src/mrs_bool/notEmpty").toBool();
+			if (temp2 == false)
 				break;
+		}
 	}
 }
-
-
-
-
-
-
 
 void 
 initOptions()
@@ -225,7 +228,6 @@ int
 main(int argc, const char **argv)
 {
   MRSDIAG("sftransform.cpp - main");
-
 
   initOptions();
   cmd_options.readOptions(argc, argv);
