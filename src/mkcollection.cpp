e1ef689f167bdfbf903eff1c341b606c921a493e
gtzan <gtzan@0e98fd9d-079e-457f-9c5c-d482923f975d>
2006-10-28 20:06:54 +0000

    Added mkcollection target
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@285 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/mkcollection.cpp
+++ src/mkcollection.cpp
@@ -1,6 +1,6 @@
 /*
 ** Copyright (C) 2000 George Tzanetakis <gtzan@cs.princeton.edu>
-**  
+**   
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
