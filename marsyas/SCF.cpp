81db214ec1a36232d946eee109a03b2efb214dcc
lgmartins <lgmartins@0e98fd9d-079e-457f-9c5c-d482923f975d>
2006-11-17 21:59:28 +0000

    fixed feature names in SFM and SCF
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@360 0e98fd9d-079e-457f-9c5c-d482923f975d

--- marsyas/SCF.cpp
+++ marsyas/SCF.cpp
@@ -17,16 +17,16 @@
 */
 
 /** 
-    \class Spectral Crest Factor
-    \brief Spectral Crest Factor
-    
-    Spectral Crest Factor is a feature set very similar to the
-    Spectral Flatness Measure defined in the MPEG-7 audio standard.
-    Although it is not defined in the MPEG7 standard, its calculation
-    is here based on the same definitions used for the MPEG7 SFM:
-    1/4 octave frequency resolution, resulting in 24 frequency bands between
-    250Hz and 16kHz.
-    Based on code provided by Luis Gustavo Martins <lmartins@inescporto.pt>
+\class Spectral Crest Factor
+\brief Spectral Crest Factor
+
+Spectral Crest Factor is a feature set very similar to the
+Spectral Flatness Measure defined in the MPEG-7 audio standard.
+Although it is not defined in the MPEG7 standard, its calculation
+is here based on the same definitions used for the MPEG7 SFM:
+1/4 octave frequency resolution, resulting in 24 frequency bands between
+250Hz and 16kHz.
+Based on code provided by Luis Gustavo Martins <lmartins@inescporto.pt>
 */
 
 #include "SCF.h"
@@ -36,8 +36,8 @@ using namespace Marsyas;
 
 SCF::SCF(string name):MarSystem("SCF",name)
 {
-  //type_ = "SCF";
-  //name_ = name;
+	//type_ = "SCF";
+	//name_ = name;
 }
 
 SCF::~SCF()
@@ -47,7 +47,7 @@ SCF::~SCF()
 MarSystem* 
 SCF::clone() const 
 {
-  return new SCF(*this);
+	return new SCF(*this);
 }
 
 void
@@ -56,112 +56,117 @@ SCF::myUpdate()
 	MRSDIAG("SCF.cpp - SCF:myUpdate");
 
 	//MPEG-7 audio standard:
-  //assumes an 1/4 octave frequency resolution,
-  //resulting in 24 frequency bands between 250Hz and 16kHz.
-  //If the signal under analysis does not contain frequencies
-  //above a determined value (e.g. due to signal sampling rate or
-  //bandwidth limitations), the nr of bands should be reduced. 
-  
-  //nrBands_ = getctrl("mrs_natural/nrbands");// can this be received as a control value?
-  nrBands_ = 24;
-  //can this parameter be dinamically modified, depending on the
-  //sampling frequency?!?
-  nrValidBands_ = nrBands_; 
+	//assumes an 1/4 octave frequency resolution,
+	//resulting in 24 frequency bands between 250Hz and 16kHz.
+	//If the signal under analysis does not contain frequencies
+	//above a determined value (e.g. due to signal sampling rate or
+	//bandwidth limitations), the nr of bands should be reduced. 
 
-  setctrl("mrs_natural/onSamples", (mrs_natural)1);
-  setctrl("mrs_natural/onObservations", (mrs_natural)nrBands_); 
-  setctrl("mrs_real/osrate", getctrl("mrs_real/israte"));
+	mrs_natural i;
 
-  mrs_natural i;
+	//nrBands_ = getctrl("mrs_natural/nrbands");// can this be received as a control value?
+	nrBands_ = 24;
+	//can this parameter be dinamically modified, depending on the
+	//sampling frequency?!?
+	nrValidBands_ = nrBands_; 
 
-  edge_.create(nrBands_ + 1);
-  bandLoEdge_.create(nrBands_);
-  bandHiEdge_.create(nrBands_);
+	setctrl("mrs_natural/onSamples", (mrs_natural)1);
+	setctrl("mrs_natural/onObservations", (mrs_natural)nrBands_); 
+	setctrl("mrs_real/osrate", getctrl("mrs_real/israte"));
 
-  //nominal band edges (Hz) -> reference = 1kHz (MPEG7)
-  for(i = 0 ; i < nrBands_ + 1 ; i++)
-    {
-      // 1/4 octave resolution (MPEG7)
-      edge_(i)= 1000.0f * pow(2.0f, (0.25f * (i - 8))); 
-    }
-  // overlapped low and high band edges (Hz)
-  for (i = 0; i < nrBands_; i++)
-    {
-      bandLoEdge_(i) = edge_(i) * 0.95f; //band overlapping (MPEG7) 
-      bandHiEdge_(i) = edge_(i+1) * 1.05f; //band overlapping (MPEG7)
-    }
+	//features names
+	ostringstream oss;
+	for (i = 0; i < nrBands_; i++)
+		oss << "SCF_" << i+1 << ",";
+	setctrl("mrs_string/onObsNames", oss.str());
 
-  fftSize_ = getctrl("mrs_natural/inObservations")->toNatural();
-  //fftBinFreqs_.create(fftSize_);
+	edge_.create(nrBands_ + 1);
+	bandLoEdge_.create(nrBands_);
+	bandHiEdge_.create(nrBands_);
 
-  // spectrum sampling rate - not audio 
-  df_ = getctrl("mrs_real/israte")->toReal();
-
-  //calculate the frequency (Hz) of each FFT bin
-  //for (mrs_natural k=0; k < fftSize_ ; k++)
-  //	fftBinFreqs_(k) = (float) k * df_;
-
-  //calculate FFT bin indexes for each band's edges
-  il_.resize(nrBands_);
-  ih_.resize(nrBands_);
-  for(i = 0; i < nrBands_; i++)
-    {
-      //round to nearest int (MPEG7)
-      il_[i] = (mrs_natural)(bandLoEdge_(i)/df_ + 0.5f); 
-      ih_[i] = (mrs_natural)(bandHiEdge_(i)/df_ + 0.5f); 
- 
-      //must verify if sampling rate is enough
-      //for the specified nr of bands. If not, 
-      //reduce nr of valid freq. bands
-      if(ih_[i] >= fftSize_/2) //marsyas FFT returns fftSize/2 points  
+	//nominal band edges (Hz) -> reference = 1kHz (MPEG7)
+	for(i = 0 ; i < nrBands_ + 1 ; i++)
 	{
-	  nrValidBands_ = i;
-	  il_.resize(nrValidBands_);
-	  ih_.resize(nrValidBands_);
-	  break;
+		// 1/4 octave resolution (MPEG7)
+		edge_(i)= 1000.0f * pow(2.0f, (0.25f * (i - 8))); 
 	}
-    }
-}
+	// overlapped low and high band edges (Hz)
+	for (i = 0; i < nrBands_; i++)
+	{
+		bandLoEdge_(i) = edge_(i) * 0.95f; //band overlapping (MPEG7) 
+		bandHiEdge_(i) = edge_(i+1) * 1.05f; //band overlapping (MPEG7)
+	}
 
+	fftSize_ = getctrl("mrs_natural/inObservations")->toNatural();
+	//fftBinFreqs_.create(fftSize_);
+
+	// spectrum sampling rate - not audio 
+	df_ = getctrl("mrs_real/israte")->toReal();
+
+	//calculate the frequency (Hz) of each FFT bin
+	//for (mrs_natural k=0; k < fftSize_ ; k++)
+	//	fftBinFreqs_(k) = (float) k * df_;
+
+	//calculate FFT bin indexes for each band's edges
+	il_.resize(nrBands_);
+	ih_.resize(nrBands_);
+	for(i = 0; i < nrBands_; i++)
+	{
+		//round to nearest int (MPEG7)
+		il_[i] = (mrs_natural)(bandLoEdge_(i)/df_ + 0.5f); 
+		ih_[i] = (mrs_natural)(bandHiEdge_(i)/df_ + 0.5f); 
+
+		//must verify if sampling rate is enough
+		//for the specified nr of bands. If not, 
+		//reduce nr of valid freq. bands
+		if(ih_[i] >= fftSize_/2) //marsyas FFT returns fftSize/2 points  
+		{
+			nrValidBands_ = i;
+			il_.resize(nrValidBands_);
+			ih_.resize(nrValidBands_);
+			break;
+		}
+	}
+}
 
 void 
 SCF::myProcess(realvec& in, realvec& out)
 {
-  //checkFlow(in,out);
-  
-  mrs_natural i, k, bandwidth;
-  double c, maxc;
-  double aritMean ;
+	//checkFlow(in,out);
 
-  //default SCF value = 1.0; (defines SCF=1.0 for silence)
-  out.setval(1.0);
-  
-  //MPEG7 defines a grouping mechanism for the frequency bands above 1KHz
-  //in order to reduce computational effort of the following calculation.
-  //For now such grouping mechanism is not implemented...
-  for(i = 0; i < nrValidBands_; i++)
-    {
-      aritMean = 0.0;
-      maxc = 0.0;
-	bandwidth = ih_[i] - il_[i] + 1;
-	for(k = il_[i]; k <= ih_[i]; k++)
-	  {
-	    c = in(k); //power spectrum coef
-	    aritMean += c / bandwidth;
-	    if(c > maxc)
-	      maxc = c;
-	  }
-	if (aritMean != 0.0)
-	  {
-	    out(i) = (float)(maxc/aritMean);
-	  }
-	//else //mean power = 0 => silence...
-	//  out(i) = 1.0; //as used for the SFM (MPEG-7)...
-    }
-  //for freq bands above the nyquist freq
-  //return SFM value defined in MPEG7 for silence
-  //for(i = nrValidBands_; i < nrBands_; i++)
-  //	out(i) = 1.0;
+	mrs_natural i, k, bandwidth;
+	double c, maxc;
+	double aritMean ;
+
+	//default SCF value = 1.0; (defines SCF=1.0 for silence)
+	out.setval(1.0);
+
+	//MPEG7 defines a grouping mechanism for the frequency bands above 1KHz
+	//in order to reduce computational effort of the following calculation.
+	//For now such grouping mechanism is not implemented...
+	for(i = 0; i < nrValidBands_; i++)
+	{
+		aritMean = 0.0;
+		maxc = 0.0;
+		bandwidth = ih_[i] - il_[i] + 1;
+		for(k = il_[i]; k <= ih_[i]; k++)
+		{
+			c = in(k); //power spectrum coef
+			aritMean += c / bandwidth;
+			if(c > maxc)
+				maxc = c;
+		}
+		if (aritMean != 0.0)
+		{
+			out(i) = (float)(maxc/aritMean);
+		}
+		//else //mean power = 0 => silence...
+		//  out(i) = 1.0; //as used for the SFM (MPEG-7)...
+	}
+	//for freq bands above the nyquist freq
+	//return SFM value defined in MPEG7 for silence
+	//for(i = nrValidBands_; i < nrBands_; i++)
+	//	out(i) = 1.0;
 }
 
 
@@ -170,8 +175,8 @@ SCF::myProcess(realvec& in, realvec& out)
 
 
 
-	
 
-	
 
-	
+
+
+
--- marsyas/SFM.cpp
+++ marsyas/SFM.cpp
@@ -17,13 +17,13 @@
 */
 
 /** 
-    \class Spectral Flatness Measure
-    \brief Spectral Flatness Measure
-    
-    Spectral Flatness Measure is a feature set defined in the MPEG-7 
-    audio standard. It assumes an 1/4 octave frequency resolution,
-    resulting in 24 frequency bands between 250Hz and 16kHz.
-    Based on code provided by Luis Gustavo Martins <lmartins@inescporto.pt>
+\class Spectral Flatness Measure
+\brief Spectral Flatness Measure
+
+Spectral Flatness Measure is a feature set defined in the MPEG-7 
+audio standard. It assumes an 1/4 octave frequency resolution,
+resulting in 24 frequency bands between 250Hz and 16kHz.
+Based on code provided by Luis Gustavo Martins <lmartins@inescporto.pt>
 */
 
 #include "SFM.h"
@@ -33,8 +33,8 @@ using namespace Marsyas;
 
 SFM::SFM(string name):MarSystem("SFM",name)
 {
-  //type_ = "SFM";
-  //name_ = name;
+	//type_ = "SFM";
+	//name_ = name;
 }
 
 SFM::~SFM()
@@ -44,119 +44,125 @@ SFM::~SFM()
 MarSystem* 
 SFM::clone() const 
 {
-  return new SFM(*this);
+	return new SFM(*this);
 }
 
 void
 SFM::myUpdate()
 {
-  MRSDIAG("SFM.cpp - SFM:myUpdate");
+	MRSDIAG("SFM.cpp - SFM:myUpdate");
 
 	//MPEG-7 audio standard:
-  //assumes an 1/4 octave frequency resolution,
-  //resulting in 24 frequency bands between 250Hz and 16kHz.
-  //If the signal under analysis does not contain frequencies
-  //above a determined value (e.g. due to signal sampling rate or
-  //bandwidth limitations), the nr of bands should be reduced. 
-  
-  //nrBands_ = getctrl("mrs_natural/nrbands");// can this be received as a control value?
-  nrBands_ = 24;
-  //can this parameter be dinamically modified, depending on the
-  //sampling frequency?!?
-  nrValidBands_ = nrBands_; 
+	//assumes an 1/4 octave frequency resolution,
+	//resulting in 24 frequency bands between 250Hz and 16kHz.
+	//If the signal under analysis does not contain frequencies
+	//above a determined value (e.g. due to signal sampling rate or
+	//bandwidth limitations), the nr of bands should be reduced. 
 
-  setctrl("mrs_natural/onSamples", (mrs_natural)1);
-  setctrl("mrs_natural/onObservations", (mrs_natural)nrBands_); 
-  setctrl("mrs_real/osrate", getctrl("mrs_real/israte"));
+	mrs_natural i;
 
-  mrs_natural i;
-  
-  edge_.create(nrBands_ + 1);
-  bandLoEdge_.create(nrBands_);
-  bandHiEdge_.create(nrBands_);
-  
-  //nominal band edges (Hz)
-  for(i = 0 ; i < nrBands_ + 1 ; i++)
-  {
-	 edge_(i)= 1000.0f * pow(2.0f, (0.25f * (i - 8))); // 1/4 octave resolution (MPEG7)
-  }
-  // overlapped low and high band edges (Hz)
-  for (i = 0; i < nrBands_; i++)
-  {
-     bandLoEdge_(i) = edge_(i) * 0.95f; //band overlapping (MPEG7) 
-	 bandHiEdge_(i) = edge_(i+1) * 1.05f; //band overlapping (MPEG7)
-  }
-  
-  fftSize_ = getctrl("mrs_natural/inObservations")->toNatural();
-  //fftBinFreqs_.create(fftSize_);
-  
-  // spectrum sampling rate - not audio 
-  df_ = getctrl("mrs_real/israte")->toReal();
+	//nrBands_ = getctrl("mrs_natural/nrbands");// can this be received as a control value?
+	nrBands_ = 24;
+	//can this parameter be dinamically modified, depending on the
+	//sampling frequency?!?
+	nrValidBands_ = nrBands_; 
 
-  //calculate the frequency (Hz) of each FFT bin
-  //for (mrs_natural k=0; k < fftSize_ ; k++)
-  //  fftBinFreqs_(k) = (float) k * df_;
+	setctrl("mrs_natural/onSamples", (mrs_natural)1);
+	setctrl("mrs_natural/onObservations", (mrs_natural)nrBands_); 
+	setctrl("mrs_real/osrate", getctrl("mrs_real/israte"));
 
-  //calculate FFT bin indexes for each band's edges
-  il_.resize(nrBands_);
-  ih_.resize(nrBands_);
-  for(i = 0; i < nrBands_; i++)
-    {
-      
-      il_[i] = (mrs_natural)(bandLoEdge_(i)/df_ + 0.5f); //round to nearest int (MPEG7)
-      ih_[i] = (mrs_natural)(bandHiEdge_(i)/df_ + 0.5f); //round to nearest int (MPEG7)
-	  
-      //must verify if sampling rate is enough
-      //for the specified nr of bands. If not, 
-      //reduce nr of valid freq. bands
-      if(ih_[i] >= fftSize_/2) //marsyas FFT returns fftSize/2 points  
+	//features names
+	ostringstream oss;
+	for (i = 0; i < nrBands_; i++)
+		oss << "SFM_" << i+1 << ",";
+	setctrl("mrs_string/onObsNames", oss.str());
+
+	edge_.create(nrBands_ + 1);
+	bandLoEdge_.create(nrBands_);
+	bandHiEdge_.create(nrBands_);
+
+	//nominal band edges (Hz)
+	for(i = 0 ; i < nrBands_ + 1 ; i++)
 	{
-	  nrValidBands_ = i;
-	  il_.resize(nrValidBands_);
-	  ih_.resize(nrValidBands_);
-	  break;
+		edge_(i)= 1000.0f * pow(2.0f, (0.25f * (i - 8))); // 1/4 octave resolution (MPEG7)
 	}
-    }
+	// overlapped low and high band edges (Hz)
+	for (i = 0; i < nrBands_; i++)
+	{
+		bandLoEdge_(i) = edge_(i) * 0.95f; //band overlapping (MPEG7) 
+		bandHiEdge_(i) = edge_(i+1) * 1.05f; //band overlapping (MPEG7)
+	}
+
+	fftSize_ = getctrl("mrs_natural/inObservations")->toNatural();
+	//fftBinFreqs_.create(fftSize_);
+
+	// spectrum sampling rate - not audio 
+	df_ = getctrl("mrs_real/israte")->toReal();
+
+	//calculate the frequency (Hz) of each FFT bin
+	//for (mrs_natural k=0; k < fftSize_ ; k++)
+	//  fftBinFreqs_(k) = (float) k * df_;
+
+	//calculate FFT bin indexes for each band's edges
+	il_.resize(nrBands_);
+	ih_.resize(nrBands_);
+	for(i = 0; i < nrBands_; i++)
+	{
+
+		il_[i] = (mrs_natural)(bandLoEdge_(i)/df_ + 0.5f); //round to nearest int (MPEG7)
+		ih_[i] = (mrs_natural)(bandHiEdge_(i)/df_ + 0.5f); //round to nearest int (MPEG7)
+
+		//must verify if sampling rate is enough
+		//for the specified nr of bands. If not, 
+		//reduce nr of valid freq. bands
+		if(ih_[i] >= fftSize_/2) //marsyas FFT returns fftSize/2 points  
+		{
+			nrValidBands_ = i;
+			il_.resize(nrValidBands_);
+			ih_.resize(nrValidBands_);
+			break;
+		}
+	}
 }
 
 void 
 SFM::myProcess(realvec& in, realvec& out)
 {
-  //checkFlow(in,out);
+	//checkFlow(in,out);
 
-  mrs_natural i, k, bandwidth;
-  mrs_real c;
-  mrs_real aritMean;
-  mrs_real geoMean;
+	mrs_natural i, k, bandwidth;
+	mrs_real c;
+	mrs_real aritMean;
+	mrs_real geoMean;
 
-  //default SFM value = 1.0; (MPEG7 defines SFM=1.0 for silence)
-  out.setval(1.0);
+	//default SFM value = 1.0; (MPEG7 defines SFM=1.0 for silence)
+	out.setval(1.0);
 
-  //MPEG7 defines a grouping mechanism for the frequency bands above 1KHz
-  //in order to reduce computational effort of the following calculation.
-  //For now such grouping mechanism is not implemented...
-  for(i = 0; i < nrValidBands_; i++)
-  {
-    geoMean = 1.0;
-    aritMean = 0.0;
-    bandwidth = ih_[i] - il_[i] + 1;
-    for(k = il_[i]; k <= ih_[i]; k++)
-      {
-	c = in(k); //power spectrum coef
-	aritMean += c / bandwidth;
-	  geoMean *= pow(c, 1.0/bandwidth);
-	    }
-    if (aritMean != 0.0)
-      {
-		out(i) = geoMean/aritMean;
-      }
-    //else //mean power = 0 => silence...
-    //  out(i) = 1.0; //MPEG-7
-  }
-  //for freq bands above the nyquist freq
-  //return SFM value defined in MPEG7 for silence
-  //for(i = nrValidBands_; i < nrBands_; i++)
-  //	out(i) = 1.0;
+	//MPEG7 defines a grouping mechanism for the frequency bands above 1KHz
+	//in order to reduce computational effort of the following calculation.
+	//For now such grouping mechanism is not implemented...
+	for(i = 0; i < nrValidBands_; i++)
+	{
+		geoMean = 1.0;
+		aritMean = 0.0;
+		bandwidth = ih_[i] - il_[i] + 1;
+		for(k = il_[i]; k <= ih_[i]; k++)
+		{
+			c = in(k); //power spectrum coef
+			aritMean += c / bandwidth;
+			geoMean *= pow(c, 1.0/bandwidth);
+		}
+		if (aritMean != 0.0)
+		{
+			out(i) = geoMean/aritMean;
+		}
+		//else //mean power = 0 => silence...
+		//  out(i) = 1.0; //MPEG-7
+	}
+	//for freq bands above the nyquist freq
+	//return SFM value defined in MPEG7 for silence
+	//for(i = nrValidBands_; i < nrBands_; i++)
+	//	out(i) = 1.0;
 }
 
 
@@ -165,9 +171,9 @@ SFM::myProcess(realvec& in, realvec& out)
 
 
 
-	
 
-	
 
-	
-	
+
+
+
+
