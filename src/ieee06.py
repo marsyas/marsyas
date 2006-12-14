35cf4cc11ffe61e1f906838225982ef2db4c3700
murdoch_j <murdoch_j@0e98fd9d-079e-457f-9c5c-d482923f975d>
2006-12-14 22:57:05 +0000

    ieee06.py updated again
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@484 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/ieee06.py
+++ src/ieee06.py
@@ -1,7 +1,6 @@
 import os
 from copy import copy 
 from glob import glob
-from UserString import MutableString
 
 bin_path = "/Users/jennifermurdoch/src/marsyas_svn/marsyas/trunk/xcode/peakClustering/Debug_MAD_MATLAB_MRSASSERT_MRSAUDIO_MRSMIDI_MRSWARNING_READLINE/"
 out_path = "/Users/jennifermurdoch/src/marsyas_svn/marsyas/trunk/src/batch_stuff/output"
@@ -15,8 +14,7 @@ beginCommand = bin_path + "peakClustering "
 
 type = ['a','f','h']
 norm = ['o','n','b','l']
-#wav_primary = ["noise.wav","saxo.wav","sweep.wav","violin.wav"]
-wav_primary = ["saxo.wav","violin.wav","blah.wav"]
+wav_primary = ["saxo.wav","violin.wav"]
 wav_interfere = ["noise.wav","sweep.wav"]
 
 n = len(type)
@@ -37,7 +35,9 @@ for k in range(1,n+1): # length of string
          type_params.append(copy(t));            
          r = r-1;
       if t[0]==n-k:
-         break         
+         break  
+         
+#print type_params                       
 
 # Compute normalization combinations
 norm_params = [];
@@ -54,47 +54,30 @@ for k in range(len(type)):
       for k in range(len(norm)):
          next.append(copy([k]))
       norm_params.append(next);   
+      
+#print norm_params
    
 # Combine the two and compute all possible values of -t arg
 params_full = [];
 for j in range(len(type_params)):
    l = len(type_params[j])
    for k in range(len(norm_params[l-1])):
-      cur = MutableString()
+      cur = "" 
       for n in range(len(type_params[j])):
          cur += type[type_params[j][n]]     
          cur += norm[norm_params[l-1][k][n]]
       params_full.append(copy(cur));
       
-# Compute all possible pairs of sound files
-n = len(wav_primary)+len(wav_interfere)
-wav_params = [];
-k=2
-
-t = range(0,k)    # t = [ 0...(k-1) ] 
-if t[0] < len(wav_primary) and t[1] >= len(wav_primary):  
-   wav_params.append(copy(t));   
-while k < n:
-   t[k-1] += 1;     # t = t + 1     
-   if t[0] < len(wav_primary) and t[1] >= len(wav_primary): 
-      wav_params.append(copy(t));      
-   r = k-1
-   while t[r] == n - (k-r) and r>0 and t[r-1] < n-r:
-      t[r-1] += 1
-      for c in range(r,k):
-         t[c] = t[c-1] + 1;     
-      if t[0] < len(wav_primary) and t[1] >= len(wav_primary):
-         wav_params.append(copy(t));            
-      r = r-1;
-   if t[0]==n-k:
-      break    
+#print params_full      
       
-print wav_params
+# Compute all possible pairs of sound files, 1 from wav_primary, 1 from wav_interfere
 wav_full = [];
-for i in range(len(wav_params)):
-   t = [ wav_primary[wav_params[i][0]] , wav_interfere[wav_params[i][1]-len(wav_primary)] ]
-   wav_full.append(copy( t ))
-print wav_full
+for i in range(len(wav_primary)):
+   for j in range(len(wav_interfere)):
+      t = [ wav_primary[i] , wav_interfere[j] ]
+      wav_full.append(copy(t))
+      
+#print wav_full
       
 #for name in wav_full:
 #   for param in params_full:
