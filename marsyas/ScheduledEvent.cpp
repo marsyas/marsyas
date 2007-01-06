f84dbc849b700c135b9401b458d35080e92e44a5
inb <inb@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-01-06 04:17:44 +0000

    Changes to work with expression events
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@524 0e98fd9d-079e-457f-9c5c-d482923f975d

--- marsyas/ScheduledEvent.cpp
+++ marsyas/ScheduledEvent.cpp
@@ -24,33 +24,54 @@
 
 #include "ScheduledEvent.h"
 #include "MarSystem.h"
-
+#include "EvExpr.h"
 using namespace std;
 using namespace Marsyas;
 
 ScheduledEvent::ScheduledEvent() { event_=NULL; }
 
-ScheduledEvent::ScheduledEvent(mrs_natural time, MarEvent* event) {
+ScheduledEvent::ScheduledEvent(mrs_natural time, MarEvent* event)
+{
     setEvent(time,event);
 }
-ScheduledEvent::ScheduledEvent(mrs_natural time, Repeat rep, MarEvent* event) {
+ScheduledEvent::ScheduledEvent(mrs_natural time, Repeat rep, MarEvent* event)
+{
     setEvent(time,rep,event);
 }
-ScheduledEvent::ScheduledEvent(const ScheduledEvent& s) {
+ScheduledEvent::ScheduledEvent(const ScheduledEvent& s)
+{
     setEvent(s.time_,s.repetition,s.event_->clone());
     setTimer(s.timer_);
 }
-ScheduledEvent::~ScheduledEvent() {
+ScheduledEvent::~ScheduledEvent()
+{
     if (event_!=NULL) { delete(event_); event_=NULL; }
 }
 
-void ScheduledEvent::setEvent(mrs_natural time, MarEvent* event) {
+void ScheduledEvent::setEvent(mrs_natural time, MarEvent* event)
+{
     setEvent(time,Repeat(),event);
 }
-void ScheduledEvent::setEvent(mrs_natural time, Repeat rep, MarEvent* event) {
-    time_=time; event_=event; repetition=rep;
+void ScheduledEvent::setEvent(mrs_natural time, Repeat rep, MarEvent* event)
+{
+    time_=time;
+    event_=event;
+    repetition=rep;
 }
-void ScheduledEvent::setTimer(TmTimer* t) { timer_=t; }
+void ScheduledEvent::setTimer(TmTimer* t)
+{
+    timer_=t;
+    if (event_->getType()=="EvExpr") {
+        EvExpr* e = dynamic_cast<EvExpr*>(event_);
+        if (e!=NULL) {
+            Expr* x=e->getExpression();
+            if (x!=NULL) {
+                x->setTimer(timer_);
+                x->post(); // evaluate init expressions
+            }
+        }
+    }
+}
 
 mrs_natural ScheduledEvent::getTime() const { return time_; }
 MarEvent* ScheduledEvent::getEvent() { return event_; }
@@ -59,17 +80,22 @@ mrs_natural ScheduledEvent::getRepetitionCount() { return repetition.count; }
 string ScheduledEvent::getRepetitionInterval() { return repetition.interval; }
 Repeat ScheduledEvent::getRepetition() { return repetition; }
 
-bool ScheduledEvent::repeat() {
-    return (repetition.infinite || repetition.count>0);
+bool ScheduledEvent::repeat()
+{
+    return event_->repeat();
 }
 /* Note there is a potential problem if time_ is much less than current
    scheduler time, then it may do a bunch of quick events (depending on
    repetition_time_) until it catches up to current time. */
-void ScheduledEvent::doRepeat() {
+void ScheduledEvent::doRepeat()
+{
     if (repeat()) {
         repetition--;
         if (timer_ != NULL) {
-            time_ = time_ + timer_->intervalsize(repetition.interval);
+            std::string t = event_->repeat_interval();
+            time_ = timer_->getTime() + timer_->intervalsize(t);
+//            printf("time=%ld  interval=%ld\n",time_,timer_->intervalsize(repetition.interval));
+//            time_ = time_ + timer_->intervalsize(repetition.interval);
         } else {
             // otherwise kill it
             repetition.infinite=false;
@@ -84,20 +110,24 @@ int ScheduledEvent::operator=(ScheduledEvent& a) {
     return 1;
 }
 */
-ScheduledEvent& ScheduledEvent::operator=(ScheduledEvent& a) {
+ScheduledEvent& ScheduledEvent::operator=(ScheduledEvent& a)
+{
     setEvent(a.time_,a.repetition,a.event_->clone());
     setTimer(a.timer_);
     return *this;
 }
-bool ScheduledEvent::operator<(const ScheduledEvent &se) const {
+bool ScheduledEvent::operator<(const ScheduledEvent &se) const
+{
     return (getTime() < se.getTime());
 }
-bool ScheduledEvent::operator>(const ScheduledEvent &se) const {
+bool ScheduledEvent::operator>(const ScheduledEvent &se) const
+{
     return (getTime() > se.getTime());
 }
 
 ostream&
-Marsyas::operator<< (ostream& o, ScheduledEvent& sys) {
+Marsyas::operator<< (ostream& o, ScheduledEvent& sys)
+{
 //    sys.put(o);
     o << "ScheduledEvent<" << sys.time_ << "," << sys.event_ << ">";
     return o;
