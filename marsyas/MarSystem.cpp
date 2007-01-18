9181371e7e8102fb523c22540955a6f5ca1f3011
lgmartins <lgmartins@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-01-18 14:40:12 +0000

    reverted to previous implementation: - what was the problem with the previous implementation? - the new one was causing a memory leakage (cmap was never deleted)
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@588 0e98fd9d-079e-457f-9c5c-d482923f975d

--- marsyas/MarSystem.cpp
+++ marsyas/MarSystem.cpp
@@ -925,8 +925,8 @@ MarSystem::getControls(map<string, MarControlPtr>* cmap)
 {
 	if(!cmap)
 	{
-	  cmap = new map<string, MarControlPtr>;
-	  // cmap = &controlsmap;
+	  map<string, MarControlPtr> controlsmap;
+	  cmap = &controlsmap;
 	}
 
 	//fill list with local controls
