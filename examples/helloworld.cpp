5881b1b1408d04fb8877a14ec6c6695cca21587b
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-02-10 06:54:32 +0000

    More code->doc infrastructure.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@659 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ examples/helloworld.cpp
@@ -0,0 +1,37 @@
+#include "MarSystemManager.h"
+using namespace std;
+using namespace Marsyas;
+
+void sfplay(string sfName, float gain) {
+	MarSystemManager mng;
+
+	MarSystem* playbacknet = mng.create("Series", "playbacknet");
+
+	playbacknet->addMarSystem(mng.create("SoundFileSource", "src"));
+	playbacknet->addMarSystem(mng.create("Gain", "gt"));
+	playbacknet->addMarSystem(mng.create("AudioSink", "dest"));
+
+	playbacknet->updctrl("SoundFileSource/src/mrs_string/filename", sfName);
+	playbacknet->updctrl("Gain/gt/mrs_real/gain", gain);
+	playbacknet->updctrl("AudioSink/dest/mrs_bool/initAudio", true);
+
+	while ( playbacknet->getctrl("SoundFileSource/src/mrs_bool/notEmpty")->toBool() ) {
+		playbacknet->tick();
+	}
+	delete playbacknet;
+}
+
+int main(int argc, const char **argv) {
+	string fileName;
+	float gain;
+	if (argc<2) { cout<<"Please enter filename."<<endl; exit(1); } else 
+		{ fileName = argv[1]; }
+	if (argc<3) { gain = 1; } else
+		{ gain = atof(argv[2]); }
+	cout << "Playing file " << fileName << " at volume " <<
+		gain << endl;
+
+	sfplay(fileName,gain);
+	exit(0);
+}
+
