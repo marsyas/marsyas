8c6ae786e3880d7c00df934c27ff4afe0a265181
mlagrange <mlagrange@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-02-21 18:28:24 +0000

    fix makefiles for compiling under linux
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@752 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/batch.py
+++ src/batch.py
@@ -2,8 +2,8 @@ import os
 from glob import glob
 
 beginCommand = "./peakClustering  "
-endCommand = " -a -s -p 2 -c 3 -o ~/output -N music  ";
-for name in glob("../../../Database/*V.wav"):
+endCommand = " -a -s -p 2 -c 3 -N music -o ~/output  ";
+for name in glob("../../../Database/*V*.wav"):
   command = beginCommand+name+endCommand
   print command
   os.system(command)
