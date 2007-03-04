db04be349fc2cf0e7964c290915cdd2cbcfd91a4
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-03-04 21:08:12 +0000

    Second part of qt4-tutorial move.  (grr, silly svn!)
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@859 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ examples/Qt4-tutorial/mainwindow.cpp
@@ -0,0 +1,68 @@
+/*
+** Copyright (C) 2007 Graham Percival <gperciva@uvic.ca>
+**	
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software 
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#include "mainwindow.h"
+
+MarQTwindow::MarQTwindow(string fileName) {
+// typical Qt front-end
+	QPushButton *quit = new QPushButton(tr("Quit"));
+	connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+	QPushButton *updatePos = new QPushButton(tr("Update position"));
+
+	QSlider *volume = new QSlider (Qt::Horizontal);
+	volume->setRange(0,100);
+	volume->setValue(50);
+
+	lcd = new QLCDNumber();
+	lcd->setNumDigits(10);
+
+	QVBoxLayout *layout = new QVBoxLayout;
+	layout->addWidget(volume);
+	layout->addWidget(updatePos);
+	layout->addWidget(lcd);
+	layout->addWidget(quit);
+	setLayout(layout);
+
+// make the Marsyas backend
+	marBackend = new MarBackend();
+	marBackend->openBackendSoundfile(fileName);
+
+// make connections between the Qt front-end and the Marsyas backend:
+
+//		Qt -> Marsyas
+	connect(volume, SIGNAL(valueChanged(int)),
+			marBackend, SLOT(setBackendVolume(int)));
+
+//		Marsyas -> Qt
+	connect(marBackend, SIGNAL(changedBackendPosition(int)),
+			this, SLOT(setMainPosition(int)));
+
+//		Qt -> Marsyas (getBackendPosition) -> Qt (changedBackendPosition)
+	connect(updatePos, SIGNAL(clicked()),
+			marBackend, SLOT(getBackendPosition()));
+}
+
+MarQTwindow::~MarQTwindow() {
+	delete marBackend;
+}
+
+void MarQTwindow::setMainPosition(int newPos) {
+	lcd->display(newPos);
+}
+
