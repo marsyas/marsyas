5e4fe459c0e55e7fc5febcc5e5682cb32dfa06ee
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-03-04 19:55:47 +0000

    Fix sample rates of examples/dataflow-split.cpp
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@851 0e98fd9d-079e-457f-9c5c-d482923f975d

--- examples/dataflow-split.cpp
+++ examples/dataflow-split.cpp
@@ -8,27 +8,29 @@ using namespace Marsyas;
 void sfplay(string sfName, float gain1, float gain2) {
 	MarSystemManager mng;
 
-// start at 44100
 	MarSystem* playbacknet = mng.create("Series", "playbacknet");
 	playbacknet->addMarSystem(mng.create("SoundFileSource", "src"));
 	playbacknet->updctrl("SoundFileSource/src/mrs_string/filename", sfName);
+// get the sample rate
+  mrs_real sample_rate = playbacknet->getctrl("mrs_real/israte")->toReal();
 
 	playbacknet->addMarSystem(mng.create("SoundFileSink", "snk"));
-// srate 44100
 	playbacknet->updctrl("SoundFileSink/snk/mrs_string/filename", "both.wav");
 
 	MarSystem* left = mng.create("Series", "left");
 	left->addMarSystem(mng.create("Gain", "gain1"));
 	left->updctrl("Gain/gain1/mrs_real/gain", gain1);
 	left->addMarSystem(mng.create("SoundFileSink", "destL"));
-// srate 22050: not good.
+// set the sample rate of this marsystem:
+  left->updctrl("mrs_real/israte",sample_rate);
 	left->updctrl("SoundFileSink/destL/mrs_string/filename", "left.wav");
 
 	MarSystem* right = mng.create("Series", "right");
 	right->addMarSystem(mng.create("Gain", "gain2"));
 	right->updctrl("Gain/gain2/mrs_real/gain", gain2);
 	right->addMarSystem(mng.create("SoundFileSink", "destR"));
-// srate 22050
+// set the sample rate of this marsystem:
+  right->updctrl("mrs_real/israte",sample_rate);
 	right->updctrl("SoundFileSink/destR/mrs_string/filename", "right.wav");
 
 	MarSystem* para = mng.create("Parallel", "para");
