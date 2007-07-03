66602db6014cf5d33f485d9d42a90266ea5ed058
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-07-03 06:05:15 +0000

    Add *NotMar.py  scripts.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@1427 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ scripts/addNotMar.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import os
+import sys
+import modifyBuild
+
+# action: 1 == add filename.   2 == remove filename.
+action = 1
+
+try:
+	source_filename = sys.argv[1]
+except:
+	print "Please enter the name of your new MarSystem"
+	sys.exit()
+
+marsyasBaseDir = os.path.join(os.path.dirname(sys.argv[0])+os.sep+'..'+os.sep)
+
+print marsyasBaseDir
+modifyBuild.set_base_dir(marsyasBaseDir)
+
+#modifyBuild.modify_manager(source_filename,action)
+modifyBuild.modify_lib_release_makefile(source_filename,action)
+
+buildfile = os.path.join(marsyasBaseDir, 'marsyas', 'Makefile.am')
+modifyBuild.modify_h_file(source_filename, buildfile, action)
+
+buildfile = os.path.join(marsyasBaseDir, 'marsyas', 'marsyas.pro')
+modifyBuild.modify_h_file(source_filename, buildfile, action)
+
+buildfile = os.path.join(marsyasBaseDir, 'marsyas', 'marsyas.pro')
+modifyBuild.modify_cpp_file(source_filename, buildfile, action)
+
+
+print "All done; please renerate autools with:"
+print "aclocal && automake"
+
