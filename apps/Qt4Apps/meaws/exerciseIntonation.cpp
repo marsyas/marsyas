a06e3ed0f59804a498ad8c0a23949a7f66d60a23
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-07-04 06:46:08 +0000

    Meaws: does transcription now.  Kind-of.  Very suckily.
    ...
    
    OK, I got impatient.  I have to go back and re-do all of this stuff, but
    at least I have the entire thing together.  Now I can make incremental
    improvements to this working version.  "closing the loop" and all that.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@1443 0e98fd9d-079e-457f-9c5c-d482923f975d

--- apps/Qt4Apps/meaws/exerciseIntonation.cpp
+++ apps/Qt4Apps/meaws/exerciseIntonation.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 #include "exerciseIntonation.h"
+#include <QFile>
+#include <QTextStream>
 
 ExerciseIntonation::ExerciseIntonation() {
 }
@@ -34,11 +36,25 @@ void ExerciseIntonation::displayAnalysis(MarBackend *results) {
 	realvec durations = results->getDurations();
 	realvec notes = results->getNotes();
 
+    QFile out_file("/tmp/notes.txt");
+    out_file.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream out(&out_file);
+
 	for (int i=0; i<durations.getSize(); i++) {
 		if (notes(i)>0) {
-			cout<<notes(i)<<"   "<<durations(i)<<endl;
+			out<<(int) floor(notes(i)+0.5)<<"\t"<<durations(i)<<endl;;
+			cout<<(int) floor(notes(i)+0.5)<<"\t"<<durations(i)<<endl;;
 		}
 	}
+	out_file.close();
+
+	system("/Users/gperciva/progs/python/libbabelpond/reldurs.py /tmp/notes.txt");
+	sleep(1);
+	system("cd /tmp; lilypond -dpreview tempscore.ly");
+	sleep(5);
+
+	resultLabel->setPixmap(QPixmap::fromImage(QImage("/tmp/tempscore.preview.png")));
+
 }
 
 
