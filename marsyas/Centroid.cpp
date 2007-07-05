48d130280c9c22d40630f06e89699fe7ca7f508d
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-07-05 18:48:54 +0000

    Whitespace improvements.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@1451 0e98fd9d-079e-457f-9c5c-d482923f975d

--- marsyas/Centroid.cpp
+++ marsyas/Centroid.cpp
@@ -1,30 +1,30 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
- 
 
-/** 
+
+/**
     \class Centroid
 	\ingroup Analysis
     \brief Centroid of each time slice of observations
 
-    Centroid computes the centroid of the observations for each 
-time samle. The center is defined as the normalized first moment 
-(center of gravity) of the observation vector. 
+    Centroid computes the centroid of the observations for each
+time samle. The center is defined as the normalized first moment
+(center of gravity) of the observation vector.
 */
 
 #include "Centroid.h"
@@ -34,8 +34,8 @@ using namespace Marsyas;
 
 Centroid::Centroid(string name):MarSystem("Centroid", name)
 {
-  m0_ = 0.0;
-  m1_ = 0.0;
+    m0_ = 0.0;
+    m1_ = 0.0;
 }
 
 
@@ -44,56 +44,45 @@ Centroid::~Centroid()
 }
 
 
-MarSystem* 
-Centroid::clone() const 
+MarSystem*
+Centroid::clone() const
 {
-  return new Centroid(*this);
+    return new Centroid(*this);
 }
 
 void
 Centroid::myUpdate(MarControlPtr sender)
 {
-  
-  MRSDIAG("Centroid.cpp - Centroid:myUpdate");
-  ctrl_onSamples_->setValue(ctrl_inSamples_, NOUPDATE);
-  ctrl_onObservations_->setValue((mrs_natural)1, NOUPDATE);
-  ctrl_osrate_->setValue(ctrl_israte_, NOUPDATE);
-  ctrl_onObsNames_->setValue("Centroid,", NOUPDATE);
+
+    MRSDIAG("Centroid.cpp - Centroid:myUpdate");
+    ctrl_onSamples_->setValue(ctrl_inSamples_, NOUPDATE);
+    ctrl_onObservations_->setValue((mrs_natural)1, NOUPDATE);
+    ctrl_osrate_->setValue(ctrl_israte_, NOUPDATE);
+    ctrl_onObsNames_->setValue("Centroid,", NOUPDATE);
 }
 
-void 
+void
 Centroid::myProcess(realvec& in, realvec& out)
 {
-  //checkFlow(in,out);
-  
-  
-  // computer centroid of observations for each time sample 
-  // using zero and first-order moments 
-  for (t = 0; t < inSamples_; t++)
+    //checkFlow(in,out);
+
+
+    // computer centroid of observations for each time sample
+    // using zero and first-order moments
+    for (t = 0; t < inSamples_; t++)
     {
-      m0_ = 0.0;
-      m1_ = 0.0;
-      for (o=0; o < inObservations_; o++)
-	{
-	  m1_ += o * in(o,t);
-	  m0_ += in(o,t);
-	}
-      if (m0_ != 0.0) 
-	out(0,t) = (m1_ / m0_) / inObservations_;
-      else 
-	out(0,t) = 0.5;
+        m0_ = 0.0;
+        m1_ = 0.0;
+        for (o=0; o < inObservations_; o++)
+        {
+            m1_ += o * in(o,t);
+            m0_ += in(o,t);
+        }
+        if (m0_ != 0.0)
+            out(0,t) = (m1_ / m0_) / inObservations_;
+        else
+            out(0,t) = 0.5;
     }
-  
+
 }
 
-      
-      
-
-
-
-
-
-
-
-	
-	
