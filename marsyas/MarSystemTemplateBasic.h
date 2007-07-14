1120811bca6247eb64c3f1f3f2dcc200a1c316f0
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-07-14 05:47:53 +0000

    Clarify MarSystemTemplateBasic.  Now a newly created MarSystem has existing extra material called fooEXAMPLE, so it's obvious which lines should be modified to suit your needs, and which ones should be left alone.
    So now even Jen won't get confused.  :)    (too bad she doesn't get
    these messages)
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@1578 0e98fd9d-079e-457f-9c5c-d482923f975d

--- marsyas/MarSystemTemplateBasic.h
+++ marsyas/MarSystemTemplateBasic.h
@@ -1,65 +1,63 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-/** 
-\class MarSystemTemplateBasic
-\brief Basic example on how to use controls efficiently in MarSystems
+/**
+	\class MarSystemTemplateBasic
+	\brief Basic example on how to use controls efficiently in MarSystems
 
+	This example is the same as Gain; it scales the output by
+multiplying each sample with a real number.
 */
 
 #ifndef MARSYAS_MARSYSTEMTEMPLATEBASIC_H
 #define MARSYAS_MARSYSTEMTEMPLATEBASIC_H
 
-#include "MarSystem.h"	
+#include "MarSystem.h"
 
 namespace Marsyas
 {
-  
-  class MarSystemTemplateBasic: public MarSystem
-  {
-  private:
-    //Add specific controls needed by this MarSystem.
-    void addControls();
-    
-    //"Pointers" to controls allow efficient access to their values.
-    //(for clarity sake, we use the ctrl_ prefix so these "pointers"
-    //can be easly identified through out the code.. but this is not
-    //mandatory, just recommended)
-    MarControlPtr ctrl_gain_EXAMPLE_;
-    MarControlPtr ctrl_repeats_EXAMPLE_;
-    
-    void myUpdate(MarControlPtr sender);
-    
-  public:
-    MarSystemTemplateBasic(std::string name);
-	//copy ctor
-    MarSystemTemplateBasic(const MarSystemTemplateBasic& a);
-    ~MarSystemTemplateBasic();
-    MarSystem* clone() const;  
-    
-    void myProcess(realvec& in, realvec& out);
-  };
-  
+
+class MarSystemTemplateBasic: public MarSystem
+{
+private:
+	// Add specific controls needed by this MarSystem.
+	void addControls();
+	// Reads changed controls and sets up variables if necessary.
+	void myUpdate(MarControlPtr sender);
+
+	// Pointers to controls allow efficient access to their
+	// values.  For clarity, we use the ctrl_ prefix so these
+	// pointers can be easly identified throughout the code...
+	// but this is not mandatory, just recommended)
+	MarControlPtr ctrl_gain_EXAMPLE_;
+
+public:
+	MarSystemTemplateBasic(std::string name);
+	MarSystemTemplateBasic(const MarSystemTemplateBasic& a);
+	~MarSystemTemplateBasic();
+	MarSystem* clone() const;
+
+	void myProcess(realvec& in, realvec& out);
+};
+
 }
 //namespace Marsyas
 
 #endif
 //MARSYAS_MARSYSTEMTEMPLATEBASIC_H
-
-
 
