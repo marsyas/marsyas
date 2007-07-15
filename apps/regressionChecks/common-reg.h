1bae42c7c620b127738dae60dc66e92a9a1746d0
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-07-15 00:03:26 +0000

    Add regressionChecks to build process.  I _think_ that it works with qmake, but I'm not certain.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@1585 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ apps/regressionChecks/common-reg.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "MarSystemManager.h"
+#include "CommandLineOptions.h"
+
+using namespace std;
+using namespace Marsyas;
+
+CommandLineOptions cmd_options;
+string testName;
+
+
