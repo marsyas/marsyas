435d18c1143ebed2f8104effb3d8624d26192c94
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-07-15 02:12:29 +0000

    Remove audiodevices test (this has been moved to regressionChecks).
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@1594 0e98fd9d-079e-457f-9c5c-d482923f975d

--- apps/marsyasTests/marsyasTests.cpp
+++ apps/marsyasTests/marsyasTests.cpp
@@ -50,7 +50,6 @@ printHelp(string progName)
   cerr << "Usage : " << progName << "-t testName file1 file2 file3" << endl;
   cerr << endl;
   cerr << "Supported tests:" << endl;
-  cerr << "audiodevices    : test audio devices " << endl;
   cerr << "cascade         : test Cascade composite " << endl;
   cerr << "collection      : test Collection " << endl;
   cerr << "drumclassify    : test drumclassify (mplfile argument)" << endl;
@@ -398,75 +397,6 @@ test_mono2stereo(string sfName)
     }
   
   delete playbacknet;
-}
-
-void 
-test_audiodevices()
-{
-  std::cout << "Testing audio devices" << endl;
-  
-  RtAudio *audio = 0;
-  RtAudioDeviceInfo info;
-  try {
-    audio = new RtAudio();
-  }
-  catch (RtError &error) {
-    error.printMessage();
-    exit(EXIT_FAILURE);
-  }
-
-  int devices = audio->getDeviceCount();
-  std::cout << "\nFound " << devices << " device(s) ...\n";
-
-  for (int i=1; i<=devices; i++) {
-    try {
-      info = audio->getDeviceInfo(i);
-    }
-    catch (RtError &error) {
-      error.printMessage();
-      break;
-    }
-
-    std::cout << "\nDevice Name = " << info.name << '\n';
-    if (info.probed == false)
-      std::cout << "Probe Status = UNsuccessful\n";
-    else {
-      std::cout << "Probe Status = Successful\n";
-      std::cout << "Output Channels = " << info.outputChannels << '\n';
-      std::cout << "Input Channels = " << info.inputChannels << '\n';
-      std::cout << "Duplex Channels = " << info.duplexChannels << '\n';
-      if (info.isDefault) std::cout << "This is the default device.\n";
-      else std::cout << "This is NOT the default device.\n";
-      if ( info.nativeFormats == 0 )
-        std::cout << "No natively supported data formats(?)!";
-      else {
-        std::cout << "Natively supported data formats:\n";
-        if ( info.nativeFormats & RTAUDIO_SINT8 )
-          std::cout << "  8-bit int\n";
-        if ( info.nativeFormats & RTAUDIO_SINT16 )
-          std::cout << "  16-bit int\n";
-        if ( info.nativeFormats & RTAUDIO_SINT24 )
-          std::cout << "  24-bit int\n";
-        if ( info.nativeFormats & RTAUDIO_SINT32 )
-          std::cout << "  32-bit int\n";
-        if ( info.nativeFormats & RTAUDIO_FLOAT32 )
-          std::cout << "  32-bit float\n";
-        if ( info.nativeFormats & RTAUDIO_FLOAT64 )
-          std::cout << "  64-bit float\n";
-      }
-      if ( info.sampleRates.size() < 1 )
-        std::cout << "No supported sample rates found!";
-      else {
-        std::cout << "Supported sample rates = ";
-        for (unsigned int j=0; j<info.sampleRates.size(); j++)
-          std::cout << info.sampleRates[j] << " ";
-      }
-      std::cout << std::endl;
-    }
-  }
-  std::cout << std::endl;
-
-  delete audio;
 }
 
 void 
@@ -3185,7 +3115,7 @@ main(int argc, const char **argv)
   
 
   if (testName == "audiodevices")
-    test_audiodevices();
+    {}
   else if (testName == "cascade") 
     test_cascade();
   else if (testName == "collection")
