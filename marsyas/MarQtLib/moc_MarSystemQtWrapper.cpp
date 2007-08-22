8bf93352843dc7b861f1112c4fb7a71c7573f4dd
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-08-22 09:40:15 +0000

    Install MarQtLib via autotools.  Currently commented out, since I don't yet check to see if qt is installed (so this would break "make install" for those people if it wasn't commented out).
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@1848 0e98fd9d-079e-457f-9c5c-d482923f975d

--- marsyas/MarQtLib/moc_MarSystemQtWrapper.cpp
+++ marsyas/MarQtLib/moc_MarSystemQtWrapper.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MarSystemQtWrapper.h'
 **
-** Created: Tue Aug 21 19:37:11 2007
+** Created: Wed Aug 22 02:34:33 2007
 **      by: The Qt Meta Object Compiler version 59 (Qt 4.3.0)
 **
 ** WARNING! All changes made in this file will be lost!
