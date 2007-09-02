a9d1704c34459469c2aeffe2b549a9476b7d3bd0
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-09-02 14:50:00 +0000

    Improvements to doc front page, plus downloadable tarball.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@1999 0e98fd9d-079e-457f-9c5c-d482923f975d

--- doc/upload.sh
+++ doc/upload.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-tar -czf marsyas-docs.tar.gz out-www/*
+tar -czf marsyas-docs.tar.gz out-www/
 scp marsyas-docs.tar.gz marsyas@marsyas.sness.net:
-ssh -l marsyas marsyas.sness.net "tar -xf marsyas-docs.tar.gz"
+ssh -l marsyas marsyas.sness.net "rm -rf out-www/ ; tar -xf marsyas-docs.tar.gz ; mv marsyas-docs.tar.gz out-www/"
+
 rm marsyas-docs.tar.gz
 
