475f34fe0b80a044db0001e0bb9237749f48ecdf
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-09-05 17:29:41 +0000

    Fix buildscripts for new locations.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@2109 0e98fd9d-079e-457f-9c5c-d482923f975d

--- scripts/buildscripts/upload-coffee.sh
+++ scripts/buildscripts/upload-coffee.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+### change as necessary
 coffeeDir=~/usr/src/marsyas-coffee
+
 
 VERSION=`cat $coffeeDir/VERSION`
 echo "Uploading Marsyas Coffee Dataset $VERSION"
@@ -9,9 +11,4 @@ cd ..
 tar -cjf $coffeeFile marsyas-coffee
 scp $coffeeFile marsyas@marsyas.sness.net:
 ssh -l marsyas marsyas.sness.net "mv $coffeeFile marsyas-coffee; cd marsyas-coffee; ln -sf $coffeeFile marsyas-coffee-latest.tar.bz2"
-#tar -cjf marsyas-docs.tar.gz out-www/
-#scp marsyas-docs.tar.gz marsyas@marsyas.sness.net:
-#ssh -l marsyas marsyas.sness.net "rm -rf out-www/ ; tar -xf marsyas-docs.tar.gz ; mv marsyas-docs.tar.gz out-www/"
-
-#rm marsyas-docs.tar.gz
 
--- scripts/buildscripts/upload-docs.sh
+++ scripts/buildscripts/upload-docs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+### change as necessary
+marsyasDir=~/usr/src/marsyas/
+
+cd $marsyasDir/doc/
 tar -czf marsyas-docs.tar.gz out-www/
 scp marsyas-docs.tar.gz marsyas@marsyas.sness.net:
 ssh -l marsyas marsyas.sness.net "rm -rf out-www/ ; tar -xf marsyas-docs.tar.gz ; mv marsyas-docs.tar.gz out-www/"
