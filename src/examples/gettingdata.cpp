6c362a9315f20b255f40f015e68da5602d486dd4
lgmartins <lgmartins@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-09-15 08:10:36 +0000

    added MarControlAccessor usage example into gettingdata.cpp. Could not test because example.pro files is unusable in win32!!
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@2178 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/examples/gettingdata.cpp
+++ src/examples/gettingdata.cpp
@@ -6,7 +6,7 @@ void recognize(string sfName)
 {
 	MarSystemManager mng;
 	MarSystem* pnet = mng.create("Series", "pnet");
-// standard network
+	// standard network
 	pnet->addMarSystem(mng.create("SoundFileSource", "src"));
 	pnet->updctrl("SoundFileSource/src/mrs_string/filename", sfName);
 	pnet->addMarSystem(mng.create("Spectrum","spk"));
@@ -15,13 +15,22 @@ void recognize(string sfName)
 	while ( pnet->getctrl("SoundFileSource/src/mrs_bool/notEmpty")->to<mrs_bool>() )
 	{
 		pnet->tick();
-		// gets data from the Spectrum
+		// gets data from the Spectrum for read only!
 		const realvec& processedData =
-pnet->getctrl("Spectrum/spk/mrs_realvec/processedData")->to<mrs_realvec>();
-		// get data from the end of the MarSystem
-		// TODO: clarify these two points.
-		//const realvec& processedData = pnet->getctrl("mrs_realvec/processedData")->to<mrs_realvec>();
-		cout << processedData << endl;
+			pnet->getctrl("Spectrum/spk/mrs_realvec/processedData")->to<mrs_realvec>();
+		cout << "Original Spectrum = " << processedData << endl;
+
+		//if we need to get the Spectrum and modify it, here is the way to do it.
+		//Notice that we must open a new scope using curly brackets so that MarControlAccessor
+		//is automatically destroyed when we are finished modifing the realvec control.
+		{
+			MarControlAccessor acc(pnet->getctrl("Spectrum/spk/mrs_realvec/processedData"));
+			realvec& processedData2 = acc.to<mrs_realvec>();
+
+			//we can now write to processedData without worries of breaking encapsulation
+			processedData2 *= 2.0;
+			cout << "Modified Spectrum = " << processedData2 << endl;
+		}
 	}
 	delete pnet;
 }
