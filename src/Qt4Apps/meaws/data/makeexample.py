22e29b6be0bb65fb20f865511fc734c6aec16947
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2007-10-22 21:49:13 +0000

    Meaws: improve lilypond->meaws_example translation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@2323 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/Qt4Apps/meaws/data/makeexample.py
+++ src/Qt4Apps/meaws/data/makeexample.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # 384 = 128*3
-DIVFACTOR = 384/384
+#DIVFACTOR = 384/384
 
 try:
   data_file = sys.argv[1]
@@ -16,19 +16,29 @@ os.system("python midi.py -i "+data_file+" -p > "+temp_file)
 
 inlines = open(temp_file).readlines()
 
-for line in inlines:
-	if ( line.find("NOTE_ON") >0 ):
+time = 0
+for i in range(len(inlines)):
+	line = inlines[i]
+	if ( line.find("NOTE_ON") > 0 ):
 		splitline = line.split()
 		for element in splitline:
 			if ( element[:6] == 'pitch='):
 				pitch = int( element[7:-1] )
-	if ( line.find("DeltaTime") >0 ):
-		splitline = line.split()
+				print str(pitch) + " " + str(time)
+				time = 0
+	if ( line.find("NOTE_ON") > 0 ):
+		splitline = inlines[i-1].split()
+#		print splitline
 		for element in splitline:
 			if ( element[:2] == 't='):
-				time = int( element[3:-1] )
-				if ( time>0 ):
-					print str(pitch) + " " + str(time/DIVFACTOR)
+				time += int( element[3:-1] )
+	if ( line.find("NOTE_OFF") > 0 ):
+		splitline = inlines[i-1].split()
+#		print splitline
+		for element in splitline:
+			if ( element[:2] == 't='):
+				time += int( element[3:-1] )
 
+print str(0) + " " + str(time)
 
 
