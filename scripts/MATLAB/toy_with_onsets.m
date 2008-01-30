685fccc8e457a1b72533ee099ad71126e6a08632
lgmartins <lgmartins@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-01-30 16:42:46 +0000

    added MATLAB script used for some of the plots used for testing the toy_with_onsets() stuff
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@2560 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ scripts/MATLAB/toy_with_onsets.m
@@ -0,0 +1,38 @@
+
+
+% figure(1)
+% plot(srcAudio(1,:));
+% hold on
+% plot(1:hopSize:hopSize*length(FluxTS),FluxTS/max(FluxTS), 'r');
+% %hold on
+% stem(1:hopSize:hopSize*length(onsetTS2),onsetTS2, 'k');
+% plot(onsetAudio, 'g');
+% hold off
+
+figure(1)
+onsetTS2 = [onsetTS(onsetWinSize+1:end), zeros(1,onsetWinSize)];
+plot(segmentAudio);
+hold on
+plot(1:hopSize:hopSize*length(FluxTS),FluxTS/max(FluxTS), 'r');
+%hold on
+stem(1:hopSize:hopSize*length(onsetTS2),onsetTS2, 'k');
+plot(onsetAudio, 'g');
+hold off
+
+figure(2)
+l = length(Accum_out);
+e = length(segmentAudio);
+b = e-l+1;
+plot(b:1:e, segmentAudio(b:1:e));
+
+hold on
+ll = l/hopSize+1;
+ee = (length(FluxTS)-onsetWinSize);
+bb = ee-ll+1;
+plot((bb-1)*hopSize:hopSize:(ee-1)*hopSize,FluxTS(bb:ee)/max(FluxTS), 'r');
+%hold on
+stem((bb-1)*hopSize:hopSize:(ee-1)*hopSize,onsetTS2(bb:ee), 'k');
+hold off
+
+wavplay(segmentAudio(b:1:e), 44100);
+
