120954e575941c8f6e134914cb2863ee78b3f40b
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-04-16 00:44:51 +0000

    Add example file that does nothing.  (useful for certain tests)
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@2775 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ src/examples/do-nothing.cpp
@@ -0,0 +1,13 @@
+#include "MarSystemManager.h"
+using namespace Marsyas;
+
+int main(int argc, const char **argv)
+{
+	MarSystemManager mng;
+
+	MarSystem* nothingnet = mng.create("Series", "nothingnet");
+
+	delete nothingnet;
+	exit(0);
+}
+
