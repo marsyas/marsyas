cbde96eecccb643733ceaf2f124faed0b6bfd72a
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-08-06 07:35:05 +0000

    simplify builds
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3008 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ cmake-modules/marsyas-global.cmake
@@ -0,0 +1,15 @@
+set (LIBRARY_OUTPUT_PATH ${PROJECT_BINARY_DIR}/lib)
+set (EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin)
+
+## setup Marsyas includes
+include_directories(${CMAKE_SOURCE_DIR}/marsyas/)
+
+if (MARSYAS_AUDIOIO) 
+	include_directories(${CMAKE_SOURCE_DIR}/otherlibs/RtAudio/)
+endif (MARSYAS_AUDIOIO) 
+
+if (MARSYAS_MIDIIO) 
+	include_directories(${CMAKE_SOURCE_DIR}/otherlibs/RtMidi/)
+endif (MARSYAS_MIDIIO) 
+
+
