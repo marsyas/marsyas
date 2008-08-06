83bd9f50ce0cae8156a4ab5382192b5a9e4c829b
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-08-06 04:54:09 +0000

    Begin cleanup of basic cmake stuffs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3003 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ cmake-modules/marsyas-options.cmake
@@ -0,0 +1,45 @@
+## user options
+option(MARSYAS_AUDIOIO "Build the audio I/O interface" ON)
+option(MARSYAS_MIDIIO "Build the MIDI I/O interface" ON)
+if (MARSYAS_LINUX)
+	option(OSS "[LINUX ONLY] Use OSS instead of ALSA" OFF)
+endif (MARSYAS_LINUX)
+
+
+# FIXME: waiting to confirm
+option(MARSYAS_ASSERT "Build with assertions" OFF)
+mark_as_advanced (MARSYAS_ASSERT)
+option(MARSYAS_PROFILING "Build with profiling" OFF)
+mark_as_advanced (MARSYAS_PROFILING)
+option(MARSYAS_DEBUG "Build with debugging info (large performance penalty)" OFF)
+
+option(MARSYAS_LOG_WARNINGS "Build with warnings" OFF)
+mark_as_advanced (MARSYAS_LOG_WARNINGS)
+option(MARSYAS_LOG_DEBUGS "Log debugs" OFF)
+mark_as_advanced (MARSYAS_LOG_DEBUGS)
+option(MARSYAS_LOG_DIAGNOSTICS "Log debugs" OFF)
+mark_as_advanced (MARSYAS_LOG_DIAGNOSTICS)
+option(MARSYAS_LOG2FILE "Log messages to a file" OFF)
+mark_as_advanced (MARSYAS_LOG2FILE)
+option(MARSYAS_LOG2STDOUT "Log messages to a standard out" OFF)
+mark_as_advanced (MARSYAS_LOG2STDOUT)
+option(MARSYAS_LOG2GUI "Log messages to GUI" OFF)
+mark_as_advanced (MARSYAS_LOG2GUI)
+
+# not used?!?!
+#option(DISTRIBUTED "[EXPERIMENTAL] compile code for distributed
+#audio processing" OFF)
+
+
+#### will finish these later -- they all require special checks
+#### and whatnot.
+option(WITH_MAD "Link to libmad (mp3 audio decoding)" OFF)
+#option(WITH_VORBIS "Link to libvorbis (ogg audio decoding)" OFF)
+#option(WITH_READLINE "Readline functionality for Marsyas Scripting
+#Language (MSL)" OFF)
+option(WITH_MATLAB "Activate MATLAB enging interface" OFF)
+option(WITH_SWIG "Enable SWIG generated bindings" OFF)
+option(WITH_QT "Build the QT library and applications" OFF)
+#option(WITH_QGL "Enable qglviewer (Qt-based OpenGL viewer)" OFF)
+
+
