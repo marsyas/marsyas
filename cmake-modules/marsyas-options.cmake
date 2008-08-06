45823b71101189bd9febb1577b4655bbbc3c7167
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-08-06 23:49:24 +0000

    Enable all the options
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3014 0e98fd9d-079e-457f-9c5c-d482923f975d

--- cmake-modules/marsyas-options.cmake
+++ cmake-modules/marsyas-options.cmake
@@ -12,66 +12,74 @@ if (MARSYAS_LINUX)
 endif (MARSYAS_LINUX)
 
 
-# FIXME: waiting to confirm
+## code message stuff
 option(MARSYAS_ASSERT "Build with assertions" OFF)
-mark_as_advanced (MARSYAS_ASSERT)
+#mark_as_advanced (MARSYAS_ASSERT)
 option(MARSYAS_PROFILING "Build with profiling" OFF)
-mark_as_advanced (MARSYAS_PROFILING)
+#mark_as_advanced (MARSYAS_PROFILING)
 option(MARSYAS_DEBUG "Build with debugging info (large performance penalty)" OFF)
+#mark_as_advanced (MARSYAS_DEBUG)
 
+
+## logging stuff
 option(MARSYAS_LOG_WARNINGS "Build with warnings" OFF)
-mark_as_advanced (MARSYAS_LOG_WARNINGS)
+#mark_as_advanced (MARSYAS_LOG_WARNINGS)
 option(MARSYAS_LOG_DEBUGS "Log debugs" OFF)
-mark_as_advanced (MARSYAS_LOG_DEBUGS)
+#mark_as_advanced (MARSYAS_LOG_DEBUGS)
 option(MARSYAS_LOG_DIAGNOSTICS "Log debugs" OFF)
-mark_as_advanced (MARSYAS_LOG_DIAGNOSTICS)
+#mark_as_advanced (MARSYAS_LOG_DIAGNOSTICS)
 option(MARSYAS_LOG2FILE "Log messages to a file" OFF)
-mark_as_advanced (MARSYAS_LOG2FILE)
+#mark_as_advanced (MARSYAS_LOG2FILE)
 option(MARSYAS_LOG2STDOUT "Log messages to a standard out" OFF)
-mark_as_advanced (MARSYAS_LOG2STDOUT)
+#mark_as_advanced (MARSYAS_LOG2STDOUT)
 option(MARSYAS_LOG2GUI "Log messages to GUI" OFF)
-mark_as_advanced (MARSYAS_LOG2GUI)
-
-# not used?!?!
-#option(DISTRIBUTED "[EXPERIMENTAL] compile code for distributed
-#audio processing" OFF)
+#mark_as_advanced (MARSYAS_LOG2GUI)
 
 
-#### will finish these later -- they all require special checks
-#### and whatnot.
+## using other software
 option(WITH_MAD "Link to libmad (mp3 audio decoding)" OFF)
-#option(WITH_VORBIS "Link to libvorbis (ogg audio decoding)" OFF)
-#option(WITH_READLINE "Readline functionality for Marsyas Scripting
-#Language (MSL)" OFF)
+option(WITH_VORBIS "Link to libvorbis (ogg audio decoding)" OFF)
 option(WITH_MATLAB "Activate MATLAB enging interface" OFF)
 option(WITH_SWIG "Enable SWIG generated bindings" OFF)
 option(WITH_QT "Build the QT library and applications" OFF)
 #option(WITH_QGL "Enable qglviewer (Qt-based OpenGL viewer)" OFF)
+#option(WITH_READLINE "Readline functionality for Marsyas Scripting
+#Language (MSL)" OFF)
+
+
+## other
+if (MARSYAS_LINUX OR MARSYAS_MACOSX)
+	option(DISTRIBUTED "[EXPERIMENTAL] compile code for distributed
+		audio processing" OFF)
+endif (MARSYAS_LINUX OR MARSYAS_MACOSX)
+
 
 
 
 ### DETECT PACKAGES
-
-if (WITH_QT)
-	include(FindQt4)
-endif (WITH_QT)
-
-if (WITH_SWIG) 
-	include(FindSWIG)
-endif (WITH_SWIG) 
 
 if (WITH_MAD)
 	find_package(mad REQUIRED)
 	set (MARSYAS_MAD 1)
 endif (WITH_MAD)
 
+if (WITH_VORBIS)
+	find_package(oggvorbis REQUIRED)
+	set (MARSYAS_VORBIS 1)
+endif (WITH_VORBIS)
 
 if (WITH_MATLAB) 
-	include(FindMatlab)
+#	include(FindMatlab)
+	find_package(matlab REQUIRED)
+	set (MARSYAS_MATLAB 1) 
 endif (WITH_MATLAB) 
 
-if (MATLAB_FOUND) 
-	set (MARSYAS_MATLAB 1) 
-endif (MATLAB_FOUND)
+if (WITH_SWIG) 
+#	include(FindSWIG)
+	find_package(swig REQUIRED)
+endif (WITH_SWIG) 
 
+if (WITH_QT)
+	include(FindQt4)
+endif (WITH_QT)
 
