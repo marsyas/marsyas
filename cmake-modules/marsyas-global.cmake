27228d2197b9b50965e2a951695d2eaf4c2d04cb
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-08-07 00:19:30 +0000

    Better fix for swig.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3016 0e98fd9d-079e-457f-9c5c-d482923f975d

--- cmake-modules/marsyas-global.cmake
+++ cmake-modules/marsyas-global.cmake
@@ -3,7 +3,6 @@ set (EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin)
 
 ## setup Marsyas includes
 include_directories(${CMAKE_SOURCE_DIR}/marsyas/)
-include_directories(${CMAKE_SOURCE_DIR})
 
 
 if (MARSYAS_AUDIOIO) 
