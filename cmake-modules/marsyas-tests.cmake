1520a4b6d84b4ce4dbe040e9bd2906bab5afbf1e
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-08-13 04:57:35 +0000

    Begin ctest.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3044 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ cmake-modules/marsyas-tests.cmake
@@ -0,0 +1,15 @@
+enable_testing()
+
+set(TEST_SOURCE ${CMAKE_SOURCE_DIR}/../regressionTests/input/)
+set(TEST_OUT ${CMAKE_BINARY_DIR}/test-output)
+make_directory(${TEST_OUT})
+
+
+add_test(sfplay1
+	${EXECUTABLE_OUTPUT_PATH}/sfplay
+	-ws 100 -s 0.25 -l 0.1 -g 0.7 -f ${TEST_OUT}/out.au
+	${TEST_SOURCE}/right.wav)
+
+add_test(sfplay2 ${EXECUTABLE_OUTPUT_PATH}/sfplay --version)
+
+
