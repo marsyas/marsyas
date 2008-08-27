4732c8029c79e93f28f10f197dc5f62e48ca7fe7
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-08-27 08:58:44 +0000

    maybe fix?
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3120 0e98fd9d-079e-457f-9c5c-d482923f975d

--- cmake-modules/FindDirectX.cmake
+++ cmake-modules/FindDirectX.cmake
@@ -1,38 +1,38 @@
 
 if (WIN32)
-	find_path(dsound_INCLUDE_DIR dsound.h
+	find_path(DSOUND_INCLUDE_DIR dsound.h
 		PATHS	
 			"$ENV{DXSDK_DIR}/Include"
 			"$ENV{PROGRAMFILES}/Microsoft DirectX SDK/Include"
 		DOC "The directory where dsound.h resides"
 	)
-	mark_as_advanced(dsound_INCLUDE_DIR)
+	mark_as_advanced(DSOUND_INCLUDE_DIR)
 
 # maybe?
 #	find_library(dsound_LIBRARY NAMES dsound
-	find_library(dsound_LIBRARY dsound
+	find_library(DSOUND_LIBRARY dsound
 		PATHS	
 			"$ENV{DXSDK_DIR}/Lib/x86"
-			"$ENV{PROGRAMFILES}/Microsoft DirectX SDK/Lib"
+			"$ENV{PROGRAMFILES}/Microsoft DirectX SDK/Lib/x86"
 		DOC "The directory where dsound.h resides"
 	)
-	mark_as_advanced(dsound_LIBRARY)
+	mark_as_advanced(DSOUND_LIBRARY)
 
 
 
-	if (dsound_INCLUDE_DIR AND dsound_LIBRARY)
-		set(dsound_FOUND TRUE)
-	endif (dsound_INCLUDE_DIR AND dsound_LIBRARY)
+	if (DSOUND_INCLUDE_DIR AND DSOUND_LIBRARY)
+		set(DSOUND_FOUND TRUE)
+	endif (DSOUND_INCLUDE_DIR AND DSOUND_LIBRARY)
 
-	if (dsound_FOUND)
-		if (NOT dsound_FIND_QUIETLY)
-			message (STATUS "Found dsound: ${dsound_LIBRARY}")
-		endif (NOT dsound_FIND_QUIETLY)
-	else (dsound_FOUND)
-		if (dsound_FIND_REQUIRED)
+	if (DSOUND_FOUND)
+		if (NOT DSOUND_FIND_QUIETLY)
+			message (STATUS "Found dsound: ${DSOUND_LIBRARY}")
+		endif (NOT DSOUND_FIND_QUIETLY)
+	else (DSOUND_FOUND)
+		if (DSOUND_FIND_REQUIRED)
 			message (FATAL_ERROR "Could find: dsound")
-		endif (dsound_FIND_REQUIRED)
-	endif (dsound_FOUND)
+		endif (DSOUND_FIND_REQUIRED)
+	endif (DSOUND_FOUND)
 
 endif(WIN32)
 
