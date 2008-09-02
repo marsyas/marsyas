8ab24c806eacdf5fc63fcad1d89ab33efbb6a9ac
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-09-02 16:04:40 +0000

    update test for cmake
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3171 0e98fd9d-079e-457f-9c5c-d482923f975d

--- scripts/dailytest.sh
+++ scripts/dailytest.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 ######### update as necessary
-baseDir=~/marsyas-svn
+#baseDir=~/marsyas-svn
+baseDir=~/usr/src/marsyas/
 matDir=~/marsyas-autotest
-coffeeDir=~/marsyas-coffee
+#coffeeDir=~/marsyas-coffee
 
 
 ### get latest SVN
 cd $baseDir
-svn update
+#svn update
 
 ######### non-changing definitions
 version=`svn info | grep Revision | cut -c 11-`
@@ -15,12 +16,13 @@ subjectBase="Marsyas Auto-Tester ("`date +%y%m%d`", rev $version):"
 
 buildDir=$matDir/testbuild
 logDir=$matDir/logs
+cmakeDir=$buildDir/build
 logBase=$logDir/`date +%y%m%d`
 
 configLog=$logBase-config.log
 buildLog=$logBase-build.log
 sanityLog=$logBase-sanity.log
-coffeeLog=$logBase-coffee.log
+#coffeeLog=$logBase-coffee.log
 distLog=$logBase-dist.log
 distcheckLog=$logBase-distcheck.log
 
@@ -37,8 +39,9 @@ sendreport() {
 	cat $report
 	if [ `which mail` ]
 	then
-		mail -s "$subject" gtzan@cs.uvic.ca < $report
-		mail -s "$subject" lgmartins@users.sourceforge.net < $report
+		mail -s "$subject" gperciva@uvic.ca < $report
+#		mail -s "$subject" gtzan@cs.uvic.ca < $report
+#		mail -s "$subject" lgmartins@users.sourceforge.net < $report
 	fi
 }
 
@@ -72,12 +75,10 @@ fi
 
 ### setup clean dir
 mkdir -p $matDir
+#rm -rf $buildDir
 cd $baseDir
-# play games because make dist marks stuff read-only
-chmod -R u+w $buildDir
-rm -rf $buildDir
-
-svn export . $buildDir
+#svn export . $buildDir
+mkdir -p $cmakeDir
 
 
 ### setup report
@@ -88,24 +89,29 @@ echo >> $report
 
 
 ### Do tests
-cd $buildDir
-./configure &> $configLog
+cd $cmakeDir
+cmake ../src/ &> $configLog
+
 testthing make $buildLog "Build"
 
-testthing "scripts/regtest_sanity.py" $sanityLog Sanity
+## re-use the name "sanity"
+testthing "make test" $sanityLog Sanity
+#testthing "scripts/regtest_sanity.py" $sanityLog Sanity
+#testthing "scripts/regtest_coffee.py $coffeeDir" $coffeeLog Coffee
 
-testthing "scripts/regtest_coffee.py $coffeeDir" $coffeeLog Coffee
-
-cd doc/
-testthing "make sources" $doxyLog "source-highlight examples"
+cd ..
+mkdir doc-build
+cd doc-build
+cmake ../doc/
+#testthing "make sources" $doxyLog "source-highlight examples"
 testthing "make doxy" $doxyLog "Doxygen docs"
-testthing "make pdf" $manualsLog "PDF manuals"
-testthing "make html" $manualsLog "HTML manuals"
+testthing "make" $manualsLog "Manuals"
+#testthing "make html" $manualsLog "HTML manuals"
 cd ..
 
-testthing "make dist" $distLog "Make dist"
+#testthing "make dist" $distLog "Make dist"
 
-testthing "make distcheck" $distcheckLog "Make distcheck"
+#testthing "make distcheck" $distcheckLog "Make distcheck"
 
 sendreport "Pass"
 echo $version > $lastGoodVersion
