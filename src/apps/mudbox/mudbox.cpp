23f53f118f75c9ec4da0ac446367af3cf9876de5
gtzan <gtzan@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-09-20 02:17:07 +0000

    Added toy_with_margrid in mudbox
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3223 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/apps/mudbox/mudbox.cpp
+++ src/apps/mudbox/mudbox.cpp
@@ -4357,6 +4357,283 @@ void toy_phisem()
 
 }
 
+void
+toy_with_margrid(string sfName)
+{
+	MarSystemManager mng;
+	MarSystem* total_;
+	
+	string trainFname = sfName;
+	
+
+
+	cout << "Toying with MarGrid" << endl;
+	// Build network for feature extraction 
+	MarSystem* extractNet = mng.create("Series", "extractNet");
+	extractNet->addMarSystem(mng.create("SoundFileSource", "src"));
+	extractNet->addMarSystem(mng.create("Stereo2Mono", "s2m"));
+	MarSystem* spectralNet = mng.create("Series", "spectralNet");
+	spectralNet->addMarSystem(mng.create("Windowing", "ham"));
+	spectralNet->addMarSystem(mng.create("Spectrum", "spk"));
+	spectralNet->addMarSystem(mng.create("PowerSpectrum", "pspk"));
+	
+	MarSystem* featureFanout = mng.create("Fanout", "featureFanout");
+	featureFanout->addMarSystem(mng.create("Centroid", "centroid"));
+	featureFanout->addMarSystem(mng.create("Rolloff", "rolloff"));
+	featureFanout->addMarSystem(mng.create("MFCC", "mfcc"));
+	
+	spectralNet->addMarSystem(featureFanout);
+	extractNet->addMarSystem(spectralNet);
+	extractNet->addMarSystem(mng.create("Memory", "mem"));
+	
+	MarSystem* stats = mng.create("Fanout", "stats");
+	stats->addMarSystem(mng.create("Mean", "mn1"));
+	stats->addMarSystem(mng.create("StandardDeviation", "std1"));
+	extractNet->addMarSystem(stats);
+	
+	MarSystem* acc = mng.create("Accumulator", "acc");
+	acc->updctrl("mrs_natural/nTimes", 1200);
+	acc->addMarSystem(extractNet);
+	
+	total_ = mng.create("Series", "total");
+	total_->addMarSystem(acc);
+	MarSystem* stats2 = mng.create("Fanout", "stats2");
+	stats2->addMarSystem(mng.create("Mean", "mn2"));
+	stats2->addMarSystem(mng.create("StandardDeviation", "std2"));
+	
+	total_->addMarSystem(stats2);
+	total_->addMarSystem(mng.create("Annotator", "ann"));
+	
+	
+	
+	total_->linkctrl("mrs_string/filename",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_string/filename");  
+	
+	
+	total_->linkctrl("mrs_string/currentlyPlaying",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_string/currentlyPlaying");  
+	
+	
+	total_->linkctrl("mrs_bool/shuffle",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_bool/shuffle");  
+	
+	total_->linkctrl("mrs_natural/pos",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_natural/pos");  
+	
+	total_->linkctrl("mrs_real/repetitions",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_real/repetitions");  
+	
+	
+	total_->linkctrl("mrs_natural/cindex",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_natural/cindex");  
+	
+	total_->linkctrl("mrs_natural/numFiles",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_natural/numFiles");  
+	
+	total_->linkctrl("mrs_string/allfilenames",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_string/allfilenames");  
+	
+	total_->linkctrl("mrs_natural/numFiles",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_natural/numFiles");  
+	
+	
+	total_->linkctrl("mrs_bool/notEmpty",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_bool/notEmpty");  
+	total_->linkctrl("mrs_bool/advance",
+					 "Accumulator/acc/Series/extractNet/SoundFileSource/src/mrs_bool/advance");  
+	
+	total_->linkctrl("mrs_bool/memReset",
+					 "Accumulator/acc/Series/extractNet/Memory/mem/mrs_bool/reset");  
+	
+	total_->linkctrl("mrs_natural/label",
+					 "Annotator/ann/mrs_natural/label");
+	
+	
+	
+	total_->updctrl("mrs_natural/inSamples", 512);
+	
+	
+	string predictFname = "test.mf";
+	total_->updctrl("mrs_string/filename", trainFname);
+	total_->updctrl("mrs_real/repetitions", 1.0);
+	
+  // EXTRACT FEATURES 
+  int index= 0;
+  int numFiles = total_->getctrl("mrs_natural/numFiles")->to<mrs_natural>();
+
+  int som_height = 12;
+  int som_width = 12;
+  
+
+  realvec som_in;
+  realvec som_res;
+  realvec som_fmatrix;
+
+  mrs_natural total_onObservations = 
+    total_->getctrl("mrs_natural/onObservations")->to<mrs_natural>();
+  
+  som_in.create(total_->getctrl("mrs_natural/inObservations")->to<mrs_natural>(), 
+		total_->getctrl("mrs_natural/inSamples")->to<mrs_natural>());
+  
+  som_res.create(total_onObservations, 
+		 total_->getctrl("mrs_natural/onSamples")->to<mrs_natural>());
+  
+  som_fmatrix.create(total_onObservations, 
+		     numFiles);
+
+  // calculate features 
+  cout << "Calculating features" << endl;
+  for (index=0; index < numFiles; index++)
+    {
+      total_->updctrl("mrs_natural/label", index);
+      total_->updctrl("mrs_bool/memReset", true);
+      total_->updctrl("mrs_natural/cindex", index);
+      string current = total_->getctrl("mrs_string/currentlyPlaying")->to<mrs_string>();
+      cout << current  << " - ";
+      
+      cout << "Processed " << index << " files " << endl; 
+
+      total_->process(som_in,som_res);
+	  
+      for (int o=0; o < total_onObservations; o++) 
+		  som_fmatrix(o, index) = som_res(o, 0);
+      total_->updctrl("mrs_bool/advance", true);      
+	  
+    }
+
+  ofstream oss;
+  oss.open("som_fmatrix.txt");
+  oss << som_fmatrix << endl;
+
+
+  // Read the feature matrix from file som_fmatrix.txt 
+  realvec train_som_fmatrix;
+  ifstream iss;
+  iss.open("som_fmatrix.txt");
+  iss >> train_som_fmatrix;
+  
+  MarSystem*  norm_;
+  MarSystem*  som_;
+  realvec norm_som_fmatrix;
+  
+
+  
+  // Normalize the feature matrix so that all features are between 0 and 1
+  norm_som_fmatrix.create(train_som_fmatrix.getRows(),
+			  train_som_fmatrix.getCols());
+  norm_ = mng.create("NormMaxMin", "norm");
+  norm_->updctrl("mrs_natural/inSamples", train_som_fmatrix.getCols());
+  norm_->updctrl("mrs_natural/inObservations", 
+  total_->getctrl("mrs_natural/onObservations")->to<mrs_natural>());
+  norm_->updctrl("mrs_string/mode", "train");
+  norm_->process(train_som_fmatrix, norm_som_fmatrix);
+  norm_->updctrl("mrs_string/mode", "predict");  
+  norm_->process(train_som_fmatrix, norm_som_fmatrix);
+
+
+
+  // Create netork for training the self-organizing map 
+  som_ = mng.create("SOM", "som");  
+  som_->updctrl("mrs_natural/grid_width", som_width);
+  som_->updctrl("mrs_natural/grid_height", som_height);
+  som_->updctrl("mrs_natural/inSamples", norm_som_fmatrix.getCols());
+  som_->updctrl("mrs_natural/inObservations", norm_som_fmatrix.getRows());  
+  som_->updctrl("mrs_string/mode", "train");
+
+  realvec som_fmatrixres;
+  som_fmatrixres.create(som_->getctrl("mrs_natural/onObservations")->to<mrs_natural>(), 
+			som_->getctrl("mrs_natural/onSamples")->to<mrs_natural>());
+  
+  cout << "Starting training" << endl;
+
+  for (int i=0; i < 2000; i ++) 
+    {
+      cout << "Training iteration" << i << endl;
+      norm_som_fmatrix.shuffle();
+      som_->process(norm_som_fmatrix, som_fmatrixres);
+    }
+
+  cout << "Training done" << endl;
+  som_->updctrl("mrs_bool/done", true);
+  som_->tick();
+
+  // write the trained som network and the feature normalization networks 
+  ofstream oss1;
+  oss1.open("som.mpl");
+  oss1 << *som_ << endl;
+  delete som_;
+
+  ofstream noss;
+  noss.open("norm.mpl");
+  noss << *norm_ << endl;
+  delete norm_;
+
+  // read trained som network from file som.mpl and normalization network norm.mpl 
+  ifstream iss1;
+  iss1.open("som.mpl");
+  som_ = mng.getMarSystem(iss1);
+
+  ifstream niss1;
+  niss1.open("norm.mpl");
+  norm_ = mng.getMarSystem(niss1);
+
+  cout << "Starting prediction" << endl;
+  som_->updctrl("mrs_string/mode", "predict");  
+  
+  Collection l1;
+  l1.read(predictFname);
+  cout << "Read collection" << endl;
+
+  total_->updctrl("mrs_natural/pos", 0);
+
+  total_->updctrl("mrs_string/filename", predictFname); 
+
+  som_->updctrl("mrs_natural/inSamples", 1);
+
+  realvec predict_res(som_->getctrl("mrs_natural/onObservations")->to<mrs_natural>(), 
+		      som_->getctrl("mrs_natural/onSamples")->to<mrs_natural>());
+  norm_->updctrl("mrs_natural/inSamples", 1);
+
+
+  realvec norm_som_res;
+
+  som_in.create(total_->getctrl("mrs_natural/inObservations")->to<mrs_natural>(), 
+		total_->getctrl("mrs_natural/inSamples")->to<mrs_natural>()); 
+ 
+ som_res.create(total_->getctrl("mrs_natural/onObservations")->to<mrs_natural>(), 
+		 total_->getctrl("mrs_natural/onSamples")->to<mrs_natural>());
+ 
+  norm_som_res.create(total_->getctrl("mrs_natural/onObservations")->to<mrs_natural>(), 
+		      total_->getctrl("mrs_natural/onSamples")->to<mrs_natural>());
+
+
+  for (int index = 0; index < l1.size(); index++)
+    {
+      total_->updctrl("mrs_natural/label", index);
+      total_->updctrl("mrs_bool/memReset", true);
+      total_->updctrl("mrs_natural/cindex", index);
+      string current = total_->getctrl("mrs_string/currentlyPlaying")->to<mrs_string>();
+
+      total_->process(som_in, som_res);
+      norm_->process(som_res, norm_som_res);
+      som_->process(norm_som_res, predict_res); 
+      cout << predict_res(0) << endl;
+      cout << predict_res(1) << endl;
+	  
+      total_->updctrl("mrs_bool/advance", true);            
+    }
+
+
+
+  cout << "end_prediction" << endl;
+
+
+
+
+
+}
+
+
 
 void
 toy_with_multichannel_merge(string sfName) 
@@ -4533,6 +4810,8 @@ main(int argc, const char **argv)
 		toy_with_marsystemIO();
 	else if (toy_withName == "matlab")
 		toy_with_matlab(fname0);
+	else if (toy_withName == "margrid")
+		toy_with_margrid(fname0);
 	else if (toy_withName == "mixer")
 		toy_with_mixer(fname0, fname1);
 	else if (toy_withName == "mono2stereo")
