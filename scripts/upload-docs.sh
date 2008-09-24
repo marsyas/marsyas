076f63741187a2f2e9a777d12c9789acdff1a3c5
gtzan <gtzan@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-09-24 17:10:06 +0000

    Reverted upload-docs.sh to work on opihi
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3242 0e98fd9d-079e-457f-9c5c-d482923f975d

--- scripts/upload-docs.sh
+++ scripts/upload-docs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ### change as necessary
-marsyasDir=~/src/cxx/marsyas-0.2.17
+marsyasDir=~/marsyas-svn/
 
-cd $marsyasDir/doc_build/
+cd $marsyasDir/build/
 tar -czf marsyas-docs.tar.gz out-www/
 scp marsyas-docs.tar.gz marsyas@marsyas.sness.net:
 ssh -l marsyas marsyas.sness.net "rm -rf out-www/ ; tar -xf marsyas-docs.tar.gz ; mv marsyas-docs.tar.gz out-www/"
