f13e23d5d3b539a5867c4ad2b0a8211c428e1a6d
lgmartins <lgmartins@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-10-07 10:03:40 +0000

    fixed evaluateTextwin.m
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3274 0e98fd9d-079e-457f-9c5c-d482923f975d

--- scripts/MATLAB/peakClusteringEval/evaluateTextWin.m
+++ scripts/MATLAB/peakClusteringEval/evaluateTextWin.m
@@ -8,55 +8,48 @@ if resynthFramesCount == refFramesCount
                         
     %get the last audio frame
     resynthAudio = [resynthAudio, PlotSink_send2MATLAB_indata];
-    
-
-    
+   
     %get reference audio for current texture window
     %(ignore mix track, i.e. 1st row, and "silent" 
     %audio channels in this texture window)
-%     refAudio = [];
-%     for k=1:numChannels
-%         if sum(audio(k+1,textWinStart:textWinEnd).^2) > eps*1e12
-%             refAudio = [refAudio; audio(k+1,textWinStart:textWinEnd)];
-%         end
-%     end
-    %optimized version
-    %channelSel = sum(audio(2:end,textWinStart:textWinEnd).^2, 2) > eps*1e15; %!!!!!
+    %     refAudio = [];
+    %     for k=1:numChannels
+    %         if sum(audio(k+1,textWinStart:textWinEnd).^2) > eps*1e12
+    %             refAudio = [refAudio; audio(k+1,textWinStart:textWinEnd)];
+    %         end
+    %     end
+    % (optimized version below)
+    %channelSel = sum(audio(2:end,textWinStart:textWinEnd).^2, 2) > eps*1e15; 
     channelSel = activeChannels;
     channelSel = [false; channelSel]; %also ignore mix channel
     channelSelIdx = [1:1:size(channelSel,1)];
     refAudio = audio(channelSel,textWinStart:textWinEnd); 
   
-    %make sure both audio vectors have the same length (PHASE PROBLEMS?!?!)
+    %make sure both audio vectors have the same length (check for PHASE PROBLEMS [!])
     audioLen = min(size(refAudio,2), size(resynthAudio,2));
     refAudio = refAudio(:,1:audioLen);
     resynthAudio = resynthAudio(1:numActiveNotes,1:audioLen);
           
     %compute SDR for current texture window
-    %correspondence = [];
-    %if numActiveNotes > 0
-    %    [SDRresults, correspondence] = computeSDR(refAudio, resynthAudio);
-    %else
-        SDRresults = 0; %?!?
-        correspondence = [];
-    %end
+    correspondence = [];
+    [SDRresults, correspondence] = computeSDR(refAudio, resynthAudio);
     SDRTextWinds = [SDRTextWinds, SDRresults];
 
     %place clustered audio into the corresponding audio channel
-    %temp = channelSelIdx(channelSel);
-    %temp = temp(correspondence);
+    temp = channelSelIdx(channelSel);
+    temp = temp(correspondence);
                                         
     BBB = [BBB, 1];
                                         
-    %segregatedAudio = zeros(size(audio, 1), size(resynthAudio,2));
-    %segregatedAudio(temp, :) = resynthAudio;
+    segregatedAudio = zeros(size(audio, 1), size(resynthAudio,2));
+    segregatedAudio(temp, :) = resynthAudio;
     
 %%%%%%%%%%%%%
     
     %store results for entire file
     refAudioTextWinds = [refAudioTextWinds, refAudio];
     %resynthAudioTextWinds = [resynthAudioTextWinds, resynthAudio];
-    %segregatedAudioTextWinds = [segregatedAudioTextWinds, segregatedAudio];
+    segregatedAudioTextWinds = [segregatedAudioTextWinds, segregatedAudio];
     numActiveNotesTextWinds = [numActiveNotesTextWinds, numActiveNotes];
  
     % init vars for next texture window
