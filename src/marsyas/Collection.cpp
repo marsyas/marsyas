60e3f7b5ffdf0b3b8c4b79fc7d883ebba14df7f1
jsundram <jsundram@0e98fd9d-079e-457f-9c5c-d482923f975d>
2008-11-26 22:52:21 +0000

    1) Made spacing consistent (all spaces, not a mix of spaces/tabs) 2) Changed collection reserve to be a power of 2. 3) Used reserve to avoid bad growth behavior. 4) Minor refactoring for clarity/succinctness. 5) Changed shuffle() to avoid possible crash, use a better algorithm. 6) rewrote operator>> for clarity/brevity/efficiency.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3330 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/Collection.cpp
+++ src/marsyas/Collection.cpp
@@ -27,10 +27,19 @@ using namespace std;
 namespace Marsyas 
 {
 
+// Utility function. Should move this somewhere publicly accessible for code re-use.
+string join(const vector<string>& v, const string delim)
+{    
+    ostringstream os;
+    copy(v.begin(), v.end (), ostream_iterator<string>(os, delim.c_str()));
+
+    return os.str();
+}
+
 Collection::Collection()
 {
-  collectionList_.reserve(1000);
-  hasLabels_ = false;
+    collectionList_.reserve(1024);
+    hasLabels_ = false;
 }
 
 Collection::~Collection()
@@ -40,57 +49,57 @@ Collection::~Collection()
 void 
 Collection::setName(string name) 
 {
-  name_ = name;
+    name_ = name;
 }
-
 
 void 
 Collection::read(string filename)
 {
-  ifstream is(filename.c_str());
-  name_ = filename.substr(0, filename.rfind(".", filename.length()));
-  
-  is >> (*this);
+    ifstream is(filename.c_str());
+    name_ = filename.substr(0, filename.rfind(".", filename.length()));
+
+    is >> (*this);
 }
 
 
 void 
 Collection::write(string filename)
 {
-  ofstream os(filename.c_str());
-  os << (*this) << endl;
+    ofstream os(filename.c_str());
+    os << (*this) << endl;
 }
 
 void 
 Collection::labelAll(string label) 
 {
-  if (hasLabels_ == false) 
+    if (hasLabels_ == false) 
     {
-      hasLabels_ = true;
-      for (unsigned int i=0; i < collectionList_.size(); i++) 
-	labelList_.push_back(label);
+        hasLabels_ = true;
+        labelList_.reserve(collectionList_.size());
+        for (unsigned int i = 0; i < collectionList_.size(); i++) 
+            labelList_.push_back(label);
     }
-  else 
+    else 
     {
-      for (unsigned int i=0; i < collectionList_.size(); i++) 
-	labelList_[i] = label;
+        for (unsigned int i=0; i < collectionList_.size(); i++) 
+            labelList_[i] = label;
     }
 }
 
 ostream& 
 operator<<(ostream& o, const Collection& l)
 {
-  // o << "# MARSYAS Collection " << endl;
-  // o << "# name = " << l.name_ << endl << endl;
-  for (unsigned int i=0; i < l.collectionList_.size(); i++)
+    // o << "# MARSYAS Collection " << endl;
+    // o << "# name = " << l.name_ << endl << endl;
+    for (unsigned int i=0; i < l.collectionList_.size(); i++)
     {
-      if (!l.hasLabels_) 
-	o << l.collectionList_[i] << endl;
-      else 
-	o << l.collectionList_[i] << "\t" << l.labelList_[i] << endl;
+        o << l.collectionList_[i];
+        if (l.hasLabels_) 
+            o << "\t" << l.labelList_[i];
+        o << endl;
     }
-  o << endl;
-  return o;
+    o << endl;
+    return o;
 }
 
 
@@ -98,158 +107,127 @@ operator<<(ostream& o, const Collection& l)
 mrs_natural
 Collection::size()
 {
-  return (mrs_natural)collectionList_.size();
+    return (mrs_natural)collectionList_.size();
 }
 
 
 string
 Collection::name()
 {
-  return name_;
+    return name_;
 }
 
 void 
 Collection::add(string entry)
 {
-  collectionList_.push_back(entry);
+    collectionList_.push_back(entry);
 }
 
 void 
 Collection::add(string entry, string label)
 {
-  collectionList_.push_back(entry);
-  labelList_.push_back(label);
-  
-
-  vector<string>::const_iterator it;
-  it = find(labelNames_.begin(), labelNames_.end(), label);
-  if (it == labelNames_.end()) // label not found add it to list 
-    labelNames_.push_back(label);
-  
-  
+    collectionList_.push_back(entry);
+    labelList_.push_back(label);
+    
+    hasLabels_ = true;
+    if (find(labelNames_.begin(), labelNames_.end(), label) == labelNames_.end())
+        labelNames_.push_back(label);
 }
-
-  
 
 
 mrs_natural 
 Collection::getSize() 
 {
-  mrs_natural size = collectionList_.size();
-  return size;
+    return collectionList_.size();
 }
 
 mrs_natural 
 Collection::getNumLabels() 
 {
-  return labelNames_.size();
+    return labelNames_.size();
 }
 
 mrs_string 
 Collection::labelName(mrs_natural i) 
 {
-  if ((unsigned)i < labelNames_.size())
-    return labelNames_[i];
-      
-  return EMPTYSTRING;
+    if ((unsigned)i < labelNames_.size())
+        return labelNames_[i];
+
+    return EMPTYSTRING;
 }
 
 mrs_string 
 Collection::getLabelNames() 
 {
-  mrs_string labelNames;
-  vector<string>::iterator vi;
-  for (vi = labelNames_.begin(); vi != labelNames_.end(); ++vi)
-    labelNames += (*vi + ',');
-  return labelNames;
+    return join(labelNames_, ",");
 }
 
 mrs_bool 
 Collection::hasLabels() 
 {
-  return hasLabels_; 
+    return hasLabels_; 
 }
 
 void 
 Collection::shuffle()
 {
-  unsigned int i;
-  unsigned int size = collectionList_.size();
-  unsigned int rind;
-  string temp;
-  
-  for (i=0;  i < size; i++)
+    // Use a Fisher-Yates shuffle
+    // http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+    unsigned int n = collectionList_.size();
+    while (n > 1)
     {
-      rind = (unsigned int)(((mrs_real)rand() / (mrs_real)(RAND_MAX))*size);
-      temp = collectionList_[i];
-      collectionList_[i] = collectionList_[rind];
-      collectionList_[rind] = temp;
-      if (hasLabels_) 
-	{
-	  temp = labelList_[i];
-	  labelList_[i] = labelList_[rind];
-	  labelList_[rind] = temp;
-	}
+        // Generate a random index in the range [0, n).
+        int k = (unsigned int)(n * ((mrs_real)rand() / ((mrs_real)(RAND_MAX) + (mrs_real)1)));
+
+        n--;
+        swap(collectionList_[n], collectionList_[k]);
+        if (hasLabels_)
+            swap(labelList_[n], labelList_[k]);
     }
 }
-
 
 string 
 Collection::toLongString()
 {
-  string result;
-  
-  vector<string>::iterator sfi;
-  for (sfi = collectionList_.begin(); sfi != collectionList_.end(); ++sfi)
-    {
-      string sfname = *sfi;
-      result += sfname +',';
-    }
-  return result;
+    return join(collectionList_, ",");
 }
 
 mrs_natural 
 Collection::labelNum(mrs_string label) 
 {
-  unsigned int i;
-  for (i=0; i < labelNames_.size(); i++) 
-    {
-      if (labelNames_[i] == label) 
-	return i;
-    }
-  return 0;
+    vector<string>::iterator it = find(labelNames_.begin(), labelNames_.end(), label);
+    if (it == labelNames_.end())
+        return 0; // why not -1?
+
+    return distance(labelNames_.begin(), it);
 }
 
 string 
 Collection::labelEntry(unsigned int i) 
 {
-  if (hasLabels_)
-    return labelList_[i]; 
-  return "No label"; 
+    if (hasLabels_)
+        return labelList_[i]; 
+    return "No label"; 
 }
 
 string 
 Collection::entry(unsigned int i)
 {
-  return collectionList_[i];
+    return collectionList_[i];
 }
 
 
 void 
 Collection::concatenate(vector<Collection> cls) 
 {
-  unsigned int cj;
-  int i;
-  
-  for (cj = 0; cj < cls.size(); cj++)
+    for (unsigned int cj = 0; cj < cls.size(); cj++)
     {
-      Collection l = cls[cj];
-      if (l.hasLabels_)
-	{
-	  hasLabels_ = true;
-	}
-      for (i=0; i < l.size(); i++) 
-	add(l.entry(i), l.labelEntry(i));
+        Collection l = cls[cj];
+        if (l.hasLabels_)
+            hasLabels_ = true;
+
+        for (int i = 0; i < l.size(); i++) 
+            add(l.entry(i), l.labelEntry(i));
     }
 }
 
@@ -258,53 +236,32 @@ Collection::concatenate(vector<Collection> cls)
 istream& 
 operator>>(istream& i, Collection& l)
 {
-  
-  MRSDIAG("Collection.cpp - operator>>");
-  string str0, str1, str2;
-  string fileEntry;
-  string fname;
-  
-  while (getline(i,fileEntry))
+    MRSDIAG("Collection.cpp - operator>>");
+
+    string fileEntry;
+    while (getline(i, fileEntry))
     {
-      if (fileEntry.empty())
-	{ 
-	  ; // do nothing
-	}
-      else 
-	{ 
-	  char c = fileEntry[0];
-	  istringstream iss(fileEntry);
-	  iss >> fname;
-	  string::size_type loc = fileEntry.find( "\t", 0 );
-	  if (loc != string::npos) 
-	    {
-	      if (c != '#') 
-		{
-		  l.collectionList_.push_back(fileEntry.substr(0, loc));
-		  string label = fileEntry.substr(loc+1, fileEntry.size());
-		  l.labelList_.push_back(label);
+        // Skip blank lines.
+        if (fileEntry.empty())
+            continue;
 
-		  vector<string>::const_iterator it;
-		  it = find(l.labelNames_.begin(), l.labelNames_.end(), label);
-		  if (it == l.labelNames_.end()) // label not found add it to list 
-		    l.labelNames_.push_back(label);
-		  
-		  l.hasLabels_ = true;
-		}
-	      
-	    } 
-	  else 
-	    {
-	      if (c != '#') 
-		{
-		  l.collectionList_.push_back(fileEntry);
-		}
-	    }
-	}
+        // Skip comment lines.
+        if (fileEntry[0] == '#')
+            continue;
+
+        // Check to see if there is a label. Could use rfind for efficiency
+        // if we were sure there weren't tabs after the label.
+        string::size_type loc = fileEntry.find('\t', 0);
+        if (loc != string::npos) 
+        {
+            string file = fileEntry.substr(0, loc);
+            string label = fileEntry.substr(loc+1, fileEntry.size());
+            l.add(file, label);
+        } 
+        else 
+            l.add(fileEntry);
     }
-  
-  
-  return i;
+   
+    return i;
 }
-
 }
