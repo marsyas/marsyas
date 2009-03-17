502271609c8f313b654a8ab062660149a13a17d0
sness <sness@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-03-17 00:43:21 +0000

    Added TestAubioYin for comparing the output of AubioYin to what theYin algorithm from Aubio gives us
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3426 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ src/tests/unit_tests/AubioYin_runner.cpp
@@ -0,0 +1,48 @@
+/* Generated file, do not edit */
+
+#ifndef CXXTEST_RUNNING
+#define CXXTEST_RUNNING
+#endif
+
+#define _CXXTEST_HAVE_STD
+#include <cxxtest/TestListener.h>
+#include <cxxtest/TestTracker.h>
+#include <cxxtest/TestRunner.h>
+#include <cxxtest/RealDescriptions.h>
+#include <cxxtest/ErrorPrinter.h>
+
+int main() {
+ return CxxTest::ErrorPrinter().run();
+}
+#include "TestAubioYin.h"
+
+static AubioYin_runner suite_AubioYin_runner;
+
+static CxxTest::List Tests_AubioYin_runner = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_AubioYin_runner( "TestAubioYin.h", 34, "AubioYin_runner", suite_AubioYin_runner, Tests_AubioYin_runner );
+
+static class TestDescription_AubioYin_runner_test_one_window_1000hz : public CxxTest::RealTestDescription {
+public:
+ TestDescription_AubioYin_runner_test_one_window_1000hz() : CxxTest::RealTestDescription( Tests_AubioYin_runner, suiteDescription_AubioYin_runner, 51, "test_one_window_1000hz" ) {}
+ void runTest() { suite_AubioYin_runner.test_one_window_1000hz(); }
+} testDescription_AubioYin_runner_test_one_window_1000hz;
+
+static class TestDescription_AubioYin_runner_test_ten_windows_1000hz : public CxxTest::RealTestDescription {
+public:
+ TestDescription_AubioYin_runner_test_ten_windows_1000hz() : CxxTest::RealTestDescription( Tests_AubioYin_runner, suiteDescription_AubioYin_runner, 78, "test_ten_windows_1000hz" ) {}
+ void runTest() { suite_AubioYin_runner.test_ten_windows_1000hz(); }
+} testDescription_AubioYin_runner_test_ten_windows_1000hz;
+
+static class TestDescription_AubioYin_runner_test_ten_windows_1000hz_500hz : public CxxTest::RealTestDescription {
+public:
+ TestDescription_AubioYin_runner_test_ten_windows_1000hz_500hz() : CxxTest::RealTestDescription( Tests_AubioYin_runner, suiteDescription_AubioYin_runner, 145, "test_ten_windows_1000hz_500hz" ) {}
+ void runTest() { suite_AubioYin_runner.test_ten_windows_1000hz_500hz(); }
+} testDescription_AubioYin_runner_test_ten_windows_1000hz_500hz;
+
+static class TestDescription_AubioYin_runner_test_ten_windows_1000_990_smooth_hz : public CxxTest::RealTestDescription {
+public:
+ TestDescription_AubioYin_runner_test_ten_windows_1000_990_smooth_hz() : CxxTest::RealTestDescription( Tests_AubioYin_runner, suiteDescription_AubioYin_runner, 218, "test_ten_windows_1000_990_smooth_hz" ) {}
+ void runTest() { suite_AubioYin_runner.test_ten_windows_1000_990_smooth_hz(); }
+} testDescription_AubioYin_runner_test_ten_windows_1000_990_smooth_hz;
+
+#include <cxxtest/Root.cpp>
---
+++ src/tests/unit_tests/TestAubioYin.h
@@ -0,0 +1,291 @@
+// TestAubioYin.h
+//
+// The AubioYin class is a fundamental frequency estimator (Cheveigne
+// and Kawahara 2002) that is implemented in the Aubio
+// (http://aubio.org) framework.  We have ported this code to Marsyas,
+// and present here a series of functional tests to ensure that the
+// results from the Aubio implementation of YIN is the same as our
+// Marsyas implementation.
+//
+// One note to make is that Aubio uses floats to calculate values
+// instead of doubles like Marsyas does.  I have extracted just the
+// YIN code from Aubio, have made a separate directory for it, and
+// have changed all instances of floats to doubles.  The results from
+// this standalone code base.
+//
+//
+// (c) 2009 - sness@sness.net - GPL
+//
+
+// #include <../../otherlibs/cxxtest/TestSuite.h>
+#include <cxxtest/TestSuite.h>
+
+#include <cstdio>
+#include "Collection.h"
+#include "MarSystemManager.h"
+#include "AubioYin.h"
+#include "CommandLineOptions.h"
+
+#include <vector>
+
+using namespace std;
+using namespace Marsyas;
+
+class AubioYin_runner : public CxxTest::TestSuite
+{
+public:
+  realvec in,out;
+  MarSystemManager mng;
+  AubioYin *yin;
+
+  void
+  setUp()
+  {
+	yin = new AubioYin("yin");
+	yin->updctrl("mrs_real/israte", 44100.0);
+  }
+
+  //
+  // Test one single buffer of audio with a 1000Hz tone
+  //
+  void test_one_window_1000hz(void) 
+  {
+	// Create input and output realvecs
+	int length = 512;
+	realvec input_realvec;
+	in.create(1,length);
+	out.create(1,length);	
+
+	// Fill up the input realvec with a sine wave
+	for (int i = 0; i < length; i++) {
+	  double d = sin(i/((double)length)*(M_PI*2.0)*((double)length/44100.0)*1000.0);
+	  in(0,i) = d;
+	}
+
+	// Process the data with the YIN algorithm
+ 	yin->myProcess(in,out);
+
+	// Check to see if the pitch is correct.
+	// sness - By correct, we mean that it's the same as output by the
+	// Aubio implementation of the yin algorithm.
+	double pitch = out(0,0);
+  	TS_ASSERT_DELTA(pitch,989.011,0.001);
+  }
+
+  //
+  // Test ten windows of audio with a 1000Hz tone
+  //
+  void test_ten_windows_1000hz(void) 
+  {
+	int buffer_size = 256;
+	int num_buffers = 10;
+
+	// Create a realvec that we will fill
+	realvec nbuf;
+	nbuf.create(1,buffer_size*num_buffers);
+
+	// Create a realvec that we will process
+	realvec ibuf;
+	ibuf.create(1,buffer_size);
+
+	// Fill up the realvec with a sine wave
+	for (int i = 0; i < buffer_size*num_buffers; i++) {
+	  double d = sin(i/((double)buffer_size)*(M_PI*2.0)*((double)buffer_size/44100.0)*1000.0);
+	  nbuf(0,i) = d;
+	}
+
+	MarSystemManager mng;
+
+	// A series containing a RealvecSource followed by an AubioYin
+	MarSystem* net = mng.create("Series", "series");
+	net->addMarSystem(mng.create("RealvecSource", "src"));
+	net->addMarSystem(mng.create("AubioYin", "yin"));
+	
+	net->updctrl("mrs_natural/inSamples", buffer_size);
+	net->updctrl("mrs_real/israte", 44100.0);
+
+	// A realvec to store our answers in
+	realvec check(num_buffers,1);
+
+	for (int i=0; i < num_buffers; i++) {
+	  // Copy over the section of nbuf to ibuf
+	  int offset = i * buffer_size;
+	  for (int j = 0; j < ibuf.getSize(); j++) {
+		ibuf(0,j) = nbuf(0,j+offset);
+	  }
+
+	  // Copy ibuf to the RealvecSource
+	  net->updctrl("RealvecSource/src/mrs_realvec/data", ibuf);
+
+	  // Tick the network once to push the data through the network
+	  net->tick();
+
+	  // Get the output of the network
+	  realvec r;
+	  r = net->getctrl("mrs_realvec/processedData")->to<mrs_realvec>();
+  	  check(i,0) = r(0,0);
+	}
+
+	// These numbers are what Aubio outputs
+  	TS_ASSERT_DELTA(check(0,0),989.011,0.001);
+  	TS_ASSERT_DELTA(check(1,0),989.011,0.001);
+  	TS_ASSERT_DELTA(check(2,0),989.011,0.001);
+  	TS_ASSERT_DELTA(check(3,0),989.011,0.001);
+  	TS_ASSERT_DELTA(check(4,0),989.122,0.001);
+  	TS_ASSERT_DELTA(check(5,0),989.011,0.001);
+  	TS_ASSERT_DELTA(check(6,0),989.011,0.001);
+  	TS_ASSERT_DELTA(check(7,0),989.122,0.001);
+  	TS_ASSERT_DELTA(check(8,0),989.011,0.001);
+  	TS_ASSERT_DELTA(check(9,0),989.122,0.001);
+  }
+
+  //
+  // Test ten windows of audio with a 1000Hz tone followed by a 500Hz tone
+  //
+  void test_ten_windows_1000hz_500hz(void) 
+  {
+	int buffer_size = 256;
+	int num_buffers = 10;
+
+	// Create a realvec that we will fill
+	realvec nbuf;
+	nbuf.create(1,buffer_size*num_buffers);
+
+	// Create a realvec that we will process
+	realvec ibuf;
+	ibuf.create(1,buffer_size);
+
+	// Fill up the realvec with a sine wave
+	for (int i = 0; i < buffer_size*num_buffers/2; i++) {
+	  double d = sin(i/((double)buffer_size)*(M_PI*2.0)*((double)buffer_size/44100.0)*1000.0);
+	  nbuf(0,i) = d;
+	}
+
+	// Fill up the realvec with a sine wave
+	for (int i = (buffer_size*num_buffers)/2; i < buffer_size*num_buffers; i++) {
+	  double d = sin(i/((double)buffer_size)*(M_PI*2.0)*((double)buffer_size/44100.0)*500.0);
+	  nbuf(0,i) = d;
+	}
+
+	MarSystemManager mng;
+
+	// A series containing a RealvecSource followed by an AubioYin
+	MarSystem* net = mng.create("Series", "series");
+	net->addMarSystem(mng.create("RealvecSource", "src"));
+	net->addMarSystem(mng.create("AubioYin", "yin"));
+	
+	net->updctrl("mrs_natural/inSamples", buffer_size);
+	net->updctrl("mrs_real/israte", 44100.0);
+
+	// A realvec to store our answers in
+	realvec check(num_buffers,1);
+
+	for (int i=0; i < num_buffers; i++) {
+	  // Copy over the section of nbuf to ibuf
+	  int offset = i * buffer_size;
+	  for (int j = 0; j < ibuf.getSize(); j++) {
+		ibuf(0,j) = nbuf(0,j+offset);
+	  }
+
+	  // Copy ibuf to the RealvecSource
+	  net->updctrl("RealvecSource/src/mrs_realvec/data", ibuf);
+
+	  // Tick the network once to push the data through the network
+	  net->tick();
+
+	  // Get the output of the network
+	  realvec r;
+	  r = net->getctrl("mrs_realvec/processedData")->to<mrs_realvec>();
+ 	  check(i,0) = r(0,0);
+	}
+
+ 	TS_ASSERT_DELTA(check(0,0),989.011,0.001);
+ 	TS_ASSERT_DELTA(check(1,0),989.011,0.001);
+ 	TS_ASSERT_DELTA(check(2,0),989.011,0.001);
+ 	TS_ASSERT_DELTA(check(3,0),989.011,0.001);
+ 	TS_ASSERT_DELTA(check(4,0),989.122,0.001);
+ 	TS_ASSERT_DELTA(check(5,0),497.210,0.001);
+ 	TS_ASSERT_DELTA(check(6,0),497.210,0.001);
+ 	TS_ASSERT_DELTA(check(7,0),497.210,0.001);
+ 	TS_ASSERT_DELTA(check(8,0),497.210,0.001);
+ 	TS_ASSERT_DELTA(check(9,0),497.210,0.001);
+  }
+
+  //
+  // Test ten windows of audio with 1000Hz tone sweeping smoothly to a
+  // 990Hz tone
+  //
+  void test_ten_windows_1000_990_smooth_hz(void) 
+  {
+	int buffer_size = 256;
+	int num_buffers = 10;
+
+	// Create a realvec that we will fill
+	realvec nbuf;
+	nbuf.create(1,buffer_size*num_buffers);
+
+	// Create a realvec that we will process
+	realvec ibuf;
+	ibuf.create(1,buffer_size);
+
+	// The starting and ending frequencies for the sine wave sweep
+	double start_freq = 1000.0;
+	double end_freq = 990.0;
+	double freq;
+	double pos;
+
+	// Fill up the realvec with a sine wave
+	for (int i = 0; i < buffer_size*num_buffers; i++) {
+	  pos = ((double)i+1) / (buffer_size*num_buffers);
+	  freq = start_freq + ((end_freq - start_freq) * pos);
+	  double d = sin(i/((double)buffer_size)*(M_PI*2.0)*((double)buffer_size/44100.0)*freq);
+	  nbuf(0,i) = d;
+	}
+
+	MarSystemManager mng;
+
+	// A series containing a RealvecSource followed by an AubioYin
+	MarSystem* net = mng.create("Series", "series");
+	net->addMarSystem(mng.create("RealvecSource", "src"));
+	net->addMarSystem(mng.create("AubioYin", "yin"));
+	
+	net->updctrl("mrs_natural/inSamples", buffer_size);
+	net->updctrl("mrs_real/israte", 44100.0);
+
+	// A realvec to store our answers in
+	realvec check(num_buffers,1);
+
+	for (int i=0; i < num_buffers; i++) {
+	  // Copy over the section of nbuf to ibuf
+	  int offset = i * buffer_size;
+	  for (int j = 0; j < ibuf.getSize(); j++) {
+		ibuf(0,j) = nbuf(0,j+offset);
+	  }
+
+	  // Copy ibuf to the RealvecSource
+	  net->updctrl("RealvecSource/src/mrs_realvec/data", ibuf);
+
+	  // Tick the network once to push the data through the network
+	  net->tick();
+
+	  // Get the output of the network
+	  realvec r;
+	  r = net->getctrl("mrs_realvec/processedData")->to<mrs_realvec>();
+  	  check(i,0) = r(0,0);
+	}
+
+  	TS_ASSERT_DELTA(check(0,0),988.346,0.001);
+  	TS_ASSERT_DELTA(check(1,0),986.357,0.001);
+  	TS_ASSERT_DELTA(check(2,0),984.375,0.001);
+  	TS_ASSERT_DELTA(check(3,0),982.401,0.001);
+  	TS_ASSERT_DELTA(check(4,0),980.327,0.001);
+  	TS_ASSERT_DELTA(check(5,0),978.478,0.001);
+  	TS_ASSERT_DELTA(check(6,0),976.528,0.001);
+  	TS_ASSERT_DELTA(check(7,0),974.586,0.001);
+  	TS_ASSERT_DELTA(check(8,0),972.651,0.001);
+  	TS_ASSERT_DELTA(check(9,0),970.831,0.001);
+  }
+
+
+};
+
