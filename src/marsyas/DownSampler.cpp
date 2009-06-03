43215381f1b91143c1ce1b42c6dcd81b19c36bf3
stefaanlippens <stefaanlippens@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-06-03 11:45:32 +0000

    worked on DownSampler: Added documentation, added comments in the code Fixed bug when inSamples is not divisible by factor (onSamples was one off) Updated usage of MarControl stuff.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3525 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/DownSampler.cpp
+++ src/marsyas/DownSampler.cpp
@@ -1,18 +1,18 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
@@ -25,9 +25,6 @@ using namespace Marsyas;
 
 DownSampler::DownSampler(string name):MarSystem("DownSampler",name)
 {
-  //type_ = "DownSampler";
-  //name_ = name;
-
 	addControls();
 }
 
@@ -35,50 +32,58 @@ DownSampler::~DownSampler()
 {
 }
 
-MarSystem* 
-DownSampler::clone() const 
+DownSampler::DownSampler(const DownSampler& a) : MarSystem(a)
 {
-  return new DownSampler(*this);
+	/// All member MarControlPtr have to be explicitly reassigned in
+	/// the copy constructor.
+	ctrl_factor_ = getControl("mrs_natural/factor");
 }
 
-void 
+MarSystem*
+DownSampler::clone() const
+{
+	return new DownSampler(*this);
+}
+
+void
 DownSampler::addControls()
 {
-  addctrl("mrs_natural/factor", 2);
-  setctrlState("mrs_natural/factor", true);
+	addControl("mrs_natural/factor", 2, ctrl_factor_);
+	ctrl_factor_->setState(true);
 }
 
 void
 DownSampler::myUpdate(MarControlPtr sender)
 {
-	(void) sender;
-  MRSDIAG("DownSampler.cpp - DownSampler:myUpdate");
-  
-  setctrl("mrs_natural/onSamples", getctrl("mrs_natural/inSamples")->to<mrs_natural>() / getctrl("mrs_natural/factor")->to<mrs_natural>());
-  setctrl("mrs_natural/onObservations", getctrl("mrs_natural/inObservations"));
-  mrs_natural factor = getctrl("mrs_natural/factor")->to<mrs_natural>();
-  setctrl("mrs_real/osrate", getctrl("mrs_real/israte")->to<mrs_real>() / factor);
+	MRSDIAG("DownSampler.cpp - DownSampler:myUpdate");
+
+	// Use the default MarSystem setup with equal input/output stream format ...
+	MarSystem::myUpdate(sender);
+
+	// ... but override the onSamples and osrate settings based on the
+	// downsample factor.
+	mrs_natural factor = ctrl_factor_->to<mrs_natural>();
+	ctrl_osrate_->setValue(ctrl_israte_->to<mrs_real>() / factor, NOUPDATE);
+	// When inSamples is divisible by factor, the number of output samples
+	// onSamples is just the integer division inSamples / factor.
+	// Otherwise, onSamples is the biggest integer obeying
+	//   factor * (onSamples - 1) < inSamples
+	// which is ceil(inSamples / factor).
+	mrs_natural onSamples = (mrs_natural) ceil((mrs_real)(ctrl_inSamples_->to<mrs_natural>()) / factor);
+	ctrl_onSamples_->setValue(onSamples, NOUPDATE);
 }
 
-void 
+void
 DownSampler::myProcess(realvec& in, realvec& out)
 {
-  //checkFlow(in,out);
+	mrs_natural factor = ctrl_factor_->to<mrs_natural>();
 
-  mrs_natural factor = getctrl("mrs_natural/factor")->to<mrs_natural>();
-  for (o=0; o < inObservations_; o++)
-    for (t = 0; t < inSamples_ / factor; t++)
-    {
-			out(o,t) = in(o,t * factor);
-    }
+	// Only copy samples at multiples of factor.
+	for (o=0; o < inObservations_; o++)
+	{
+		for (t = 0; t < onSamples_; t++)
+		{
+			out(o, t) = in(o, t * factor);
+		}
+	}
 }
-
-
-
-
-
-
-
-	
-
-	
