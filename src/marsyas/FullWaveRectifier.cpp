6e1196553dc098cf595399a1dcf08267ba02c82b
stefaanlippens <stefaanlippens@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-06-03 12:33:42 +0000

    code style cleanup added documentation and comments
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3527 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/FullWaveRectifier.cpp
+++ src/marsyas/FullWaveRectifier.cpp
@@ -1,18 +1,18 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
@@ -21,10 +21,8 @@
 using namespace std;
 using namespace Marsyas;
 
-FullWaveRectifier::FullWaveRectifier(string name):MarSystem("FullWaveRectifier",name)
+FullWaveRectifier::FullWaveRectifier(string name) : MarSystem("FullWaveRectifier",name)
 {
-	//type_ = "FullWaveRectifier";
-	//name_ = name;
 }
 
 
@@ -32,8 +30,8 @@ FullWaveRectifier::~FullWaveRectifier()
 {
 }
 
-MarSystem* 
-FullWaveRectifier::clone() const 
+MarSystem*
+FullWaveRectifier::clone() const
 {
 	return new FullWaveRectifier(*this);
 }
@@ -41,33 +39,19 @@ FullWaveRectifier::clone() const
 void
 FullWaveRectifier::myUpdate(MarControlPtr sender)
 {
-	(void) sender;
-	MRSDIAG("FullWaveRectifier.cpp - FullWaveRectifier:myUpdate");
-
-	setctrl("mrs_natural/onSamples", getctrl("mrs_natural/inSamples"));
-	setctrl("mrs_natural/onObservations", getctrl("mrs_natural/inObservations"));
-	setctrl("mrs_real/osrate", getctrl("mrs_real/israte"));
+	/// Use the default MarSystem setup with equal input/output stream format.
+	MarSystem::myUpdate(sender);
 }
 
-void 
+void
 FullWaveRectifier::myProcess(realvec& in, realvec& out)
 {
-	for (o=0; o < inObservations_; o++)
+	// Copy the absolute values of the samples from the input to the output.
+	for (o = 0; o < inObservations_; o++)
+	{
 		for (t = 0; t < inSamples_; t++)
 		{
-			if (in(o,t) < 0.0) 
-				out(o,t) = -in(o,t);
-			else 
-				out(o,t) = in(o,t);
+			out(o, t) = (in(o, t) > 0) ? in(o, t) : -in(o, t);
 		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
