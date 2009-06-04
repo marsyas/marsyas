060663f92f9eeb5e96a6b1dc1ee7d16607b037cb
stefaanlippens <stefaanlippens@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-06-04 15:29:37 +0000

    added documentation and comments code style cleanup
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3531 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/ShiftInput.cpp
+++ src/marsyas/ShiftInput.cpp
@@ -1,18 +1,18 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
@@ -21,7 +21,7 @@
 using namespace std;
 using namespace Marsyas;
 
-ShiftInput::ShiftInput(string name):MarSystem("ShiftInput",name)
+ShiftInput::ShiftInput(string name): MarSystem("ShiftInput", name)
 {
 	winSize_ = 0;
 	hopSize_ = 0;
@@ -32,7 +32,7 @@ ShiftInput::~ShiftInput()
 {
 }
 
-ShiftInput::ShiftInput(const ShiftInput& a):MarSystem(a)
+ShiftInput::ShiftInput(const ShiftInput& a): MarSystem(a)
 {
 	winSize_ = 0;
 	hopSize_ = 0;
@@ -41,8 +41,8 @@ ShiftInput::ShiftInput(const ShiftInput& a):MarSystem(a)
 	ctrl_winSize_ = getctrl("mrs_natural/winSize");
 }
 
-MarSystem* 
-ShiftInput::clone() const 
+MarSystem*
+ShiftInput::clone() const
 {
 	return new ShiftInput(*this);
 }
@@ -50,12 +50,13 @@ ShiftInput::clone() const
 void
 ShiftInput::addControls()
 {
-	addctrl("mrs_natural/winSize", (mrs_natural)MRS_DEFAULT_SLICE_NSAMPLES, ctrl_winSize_);
-	setctrlState("mrs_natural/winSize", true);
+	// The control for the window size.
+	addControl("mrs_natural/winSize", (mrs_natural)MRS_DEFAULT_SLICE_NSAMPLES, ctrl_winSize_);
+	setControlState("mrs_natural/winSize", true);
 
-	//must be set true so we set the internal buffer to zeros the first
-	//time it is used (it will be set to false after that)
-	addctrl("mrs_bool/reset", true, ctrl_reset_);
+	// The control to reset the internal buffer, set to true so the buffer
+	// itself is properly initialized later.
+	addControl("mrs_bool/reset", true, ctrl_reset_);
 }
 
 void
@@ -63,51 +64,78 @@ ShiftInput::myUpdate(MarControlPtr sender)
 {
 	(void) sender;
 
+	// Cache the window and hop size for use in myProcess().
 	winSize_ = ctrl_winSize_->to<mrs_natural>();
 	hopSize_ = ctrl_inSamples_->to<mrs_natural>();
-	
-	if(hopSize_ < winSize_)
-		outSavedData_.stretch(ctrl_inObservations_->to<mrs_natural>(), winSize_- hopSize_);
 
+	// If the hop size is smaller than the window size, we need buffering to
+	// handle the overlap between successive windows.
+	// When the hop size is larger than the window size, we don't have overlap
+	// and don't need buffering.
+	if (hopSize_ < winSize_)
+	{
+		outSavedData_.stretch(ctrl_inObservations_->to<mrs_natural>(), winSize_ - hopSize_);
+	}
+
+	// Update the output stream format.
 	ctrl_onSamples_->setValue(ctrl_winSize_, NOUPDATE);
 	ctrl_onObservations_->setValue(ctrl_inObservations_, NOUPDATE);
 	ctrl_osrate_->setValue(ctrl_israte_, NOUPDATE);
+
+	// Update the onObsNames string.
+	// TODO: this does not work when there are multiple observations channels,
+	// define and use helper function for this. See for example the function
+	// prependObservationNames() in RunningStatistics.cpp.
 	ostringstream oss;
 	oss << "HopSize" << hopSize_ << "_WinSize" << winSize_ << "_" << ctrl_inObsNames_;
 	ctrl_onObsNames_->setValue(oss.str(), NOUPDATE);
 }
 
-void 
+void
 ShiftInput::myProcess(realvec& in, realvec& out)
 {
-	for(o=0; o<inObservations_; ++o)
+	for (o = 0; o<inObservations_; ++o)
 	{
-		if(hopSize_ < winSize_)
+		if (hopSize_ < winSize_)
 		{
-			//check if we must clear the audio buffer (due to a reset call)
-			if (ctrl_reset_->to<mrs_bool>()) 
+			// When the hop size is smaller than the window size, we have to work
+			// with a buffer.
+
+			// Check if we must clear the audio buffer (due to a reset call).
+			// TODO: shouldn't this be done in myUpdate?
+			if (ctrl_reset_->to<mrs_bool>())
 			{
 				outSavedData_.setval(0.0);
 				ctrl_reset_->setValue(false);
 			}
-			//copy previous output stored data to the output
-			for (t = 0; t < winSize_-hopSize_; t++)
+
+			// TODO: this can be done more efficiently with a circular buffer
+			// instead of shifting the buffer data all the time.
+
+			// First part of output: copy the buffered data.
+			for (t = 0; t < winSize_ - hopSize_; t++)
 			{
-				out(o,t) = outSavedData_(o, t);
+				out(o, t) = outSavedData_(o, t);
 			}
-			//add new hopSize samples from input to the end of output
-			for (t=winSize_-hopSize_; t < winSize_; t++)
+			// Second part of output: copy the new data from the input.
+			for (t = winSize_ - hopSize_; t < winSize_; t++)
 			{
-				out(o, t) = in(o, t-(winSize_-hopSize_));
+				out(o, t) = in(o, t - (winSize_ - hopSize_));
 			}
-			//store current output for next time
-			for (t = 0; t < winSize_-hopSize_; t++) 
-				outSavedData_(o, t) = out(o, t+hopSize_);
+			// Store part of the output to the buffer for the next time.
+			for (t = 0; t < winSize_ - hopSize_; t++)
+			{
+				outSavedData_(o, t) = out(o, t + hopSize_);
+			}
 		}
 		else
 		{
-			for(t=0; t<onSamples_; ++t)
-				out(o,t) = in(o,t);
+			// When the hop size is larger than the window size, we just need to
+			// copy the first onSamples_ samples and discard the rest.
+			for (t = 0; t < onSamples_; ++t)
+			{
+				out(o, t) = in(o, t);
+			}
 		}
 	}
 
@@ -115,20 +143,11 @@ ShiftInput::myProcess(realvec& in, realvec& out)
 	// MATLAB_PUT(in, "ShiftInput_in");
 	// MATLAB_PUT(out, "ShiftInput_out");
 	// MATLAB_EVAL("size(ShiftInput_in)");
-	
+
 // 	MATLAB_EVAL("plot(ShiftInput_in)");
 //	getchar();
 //  MATLAB_EVAL("plot(ShiftInput_out)");
 //	getchar();
-	
-	
+
+
 }
-
-
-
-
-
-
-
-
-
