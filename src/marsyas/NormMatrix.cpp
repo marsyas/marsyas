c944298710e5b157652c6e7372cf2d3d86be57c7
stefaanlippens <stefaanlippens@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-06-17 14:05:30 +0000

    code style cleanup, worked on documentation
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3542 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/NormMatrix.cpp
+++ src/marsyas/NormMatrix.cpp
@@ -1,18 +1,18 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
@@ -21,7 +21,7 @@
 using namespace std;
 using namespace Marsyas;
 
-NormMatrix::NormMatrix(string name):MarSystem("NormMatrix",name)
+NormMatrix::NormMatrix(string name): MarSystem("NormMatrix",name)
 {
 
 }
@@ -30,25 +30,15 @@ NormMatrix::~NormMatrix()
 {
 }
 
-MarSystem* 
-NormMatrix::clone() const 
+MarSystem*
+NormMatrix::clone() const
 {
 	return new NormMatrix(*this);
 }
 
-void 
+void
 NormMatrix::myProcess(realvec& in, realvec& out)
 {
 	out = in;
 	out.norm();
 }
-
-
-
-
-
-
-
-
-
-
