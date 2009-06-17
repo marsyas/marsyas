616b5b57ea7a62414a9131fdfdd03d3499534c9e
stefaanlippens <stefaanlippens@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-06-17 12:34:15 +0000

    code style cleanup, worked on documentation
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3540 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/AbsMax.cpp
+++ src/marsyas/AbsMax.cpp
@@ -1,18 +1,18 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
@@ -21,56 +21,47 @@
 using namespace std;
 using namespace Marsyas;
 
-AbsMax::AbsMax(string name):MarSystem("AbsMax",name)
+AbsMax::AbsMax(string name): MarSystem("AbsMax",name)
 {
-  //type_ = "AbsMax";
-  //name_ = name;
 }
 
 AbsMax::~AbsMax()
 {
 }
 
-
-MarSystem* 
-AbsMax::clone() const 
+MarSystem*
+AbsMax::clone() const
 {
-  return new AbsMax(*this);
+	return new AbsMax(*this);
 }
 
 void
 AbsMax::myUpdate(MarControlPtr sender)
 {
-	(void) sender;
-  setctrl("mrs_natural/onSamples",  (mrs_natural)1);
-  setctrl("mrs_natural/onObservations", getctrl("mrs_natural/inObservations"));
-  setctrl("mrs_real/osrate", getctrl("mrs_real/israte")->to<mrs_real>() / (mrs_real)getctrl("mrs_natural/inSamples")->to<mrs_natural>());  
+	// Start with the default MarSystem setup with equal input/output
+	// stream format ...
+	MarSystem::myUpdate(sender);
+
+	// ... but change the number and rate of output samples.
+	setControl("mrs_natural/onSamples",  (mrs_natural)1);
+	setControl("mrs_real/osrate",
+		getControl("mrs_real/israte")->to<mrs_real>() / getControl("mrs_natural/inSamples")->to<mrs_natural>()
+	);
 }
 
-void 
+void
 AbsMax::myProcess(realvec& in, realvec& out)
 {
-  //checkFlow(in,out);
-  for (o=0; o < inObservations_; o++)
-  {
-    max_ = 0.0;
-    for (t=0; t < inSamples_; t++)
+	for (o=0; o < inObservations_; o++)
+	{
+		max_ = 0.0;
+		for (t=0; t < inSamples_; t++)
 		{
-			if (fabs(in(o,t)) > max_) 
+			if (fabs(in(o,t)) > max_)
+			{
 				max_ = fabs(in(o,t));
+			}
 		}
-    out(o,0) = max_;
-  } 
+		out(o,0) = max_;
+	}
 }
-
-
-
-
-
-
-	
-
-	
-
-	
-	
--- src/marsyas/AbsMax.h
+++ src/marsyas/AbsMax.h
@@ -1,18 +1,18 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
@@ -23,38 +23,41 @@
 
 namespace Marsyas
 {
-/** 
-    \class AbsMax
+/**
+	\class AbsMax
 	\ingroup Analysis
-    \brief Calculate the maximum of the absolute values of the signal 
-    
-Calculates the maximum of the absolute values of 
-the singal. Used for sending waveform plots to editor. 
+	\brief Calculate the maximum absolute value for each observation
+	signal (per slice).
+
+	For each observation channel in each given slice, the maximum absolute
+	value is calculated.
+
+	This MarSystem has no extra controls.
 */
 
 
 class AbsMax: public MarSystem
 {
 private:
-  mrs_real max_;
+	mrs_real max_;
 
 	void myUpdate(MarControlPtr sender);
-  
+
 public:
-  AbsMax(std::string name);
-  ~AbsMax();
-  
-  MarSystem* clone() const;
-  void myProcess(realvec& in, realvec& out);
- 
+	AbsMax(std::string name);
+	~AbsMax();
+
+	MarSystem* clone() const;
+	void myProcess(realvec& in, realvec& out);
+
 };
 
 }//Marsyas namespace
 
 #endif
 
-	
 
-	
 
-	
+
+
+
