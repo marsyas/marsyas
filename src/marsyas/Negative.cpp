21eca7fb5b57d6624495c6d07c9921e654f0a2b2
stefaanlippens <stefaanlippens@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-06-17 12:26:34 +0000

    the Negative Marsystem didn't take the observations channels into account
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3538 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/Negative.cpp
+++ src/marsyas/Negative.cpp
@@ -1,18 +1,18 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
@@ -21,10 +21,8 @@
 using namespace std;
 using namespace Marsyas;
 
-Negative::Negative(string name):MarSystem("Negative",name)
+Negative::Negative(string name): MarSystem("Negative", name)
 {
-  //type_ = "Negative";
-  //name_ = name;
 }
 
 
@@ -32,24 +30,21 @@ Negative::~Negative()
 {
 }
 
-MarSystem* 
-Negative::clone() const 
+MarSystem*
+Negative::clone() const
 {
-  return new Negative(*this);
+	return new Negative(*this);
 }
 
-void 
+void
 Negative::myProcess(realvec& in, realvec& out)
 {
-  for (t = 0; t < inSamples_; t++)
-    out(t) = - in(t);
+	for (t = 0; t < inSamples_; t++)
+	{
+		for (o = 0; o < inObservations_; o++)
+		{
+			out(o, t) = -in(o, t);
+		}
+	}
 }
 
-
-
-
-
-
-	
-
-	
