a1203e3ca2338e38f81f7cf4814ac98f5c336359
stefaanlippens <stefaanlippens@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-06-24 13:41:57 +0000

    code style cleanup
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3574 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/Sum.cpp
+++ src/marsyas/Sum.cpp
@@ -1,18 +1,18 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
@@ -26,7 +26,7 @@ Sum::Sum(string name):MarSystem("Sum",name)
 	addControls();
 }
 
-Sum::Sum(const Sum& a): MarSystem(a) 
+Sum::Sum(const Sum& a): MarSystem(a)
 {
 	ctrl_weight_ = getctrl("mrs_real/weight");
 }
@@ -37,15 +37,15 @@ Sum::~Sum()
 {
 }
 
-void 
+void
 Sum::addControls()
 {
 	addctrl("mrs_real/weight", 1.0, ctrl_weight_);
 
 }
 
-MarSystem* 
-Sum::clone() const 
+MarSystem*
+Sum::clone() const
 {
 	return new Sum(*this);
 }
@@ -53,35 +53,28 @@ Sum::clone() const
 void
 Sum::myUpdate(MarControlPtr sender)
 {
-	(void) sender;
-	MRSDIAG("Sum.cpp - Sum:myUpdate");
+	// Start with the default MarSystem setup with equal input/output
+	//stream format ...
+	MarSystem::myUpdate(sender);
 
-	ctrl_onSamples_->setValue(ctrl_inSamples_, NOUPDATE);
+	/// ... but set the number of output observations to one.
 	ctrl_onObservations_->setValue(1, NOUPDATE);
-	ctrl_osrate_->setValue(ctrl_israte_, NOUPDATE);
-	ctrl_onObsNames_->setValue(ctrl_inObsNames_, NOUPDATE);
 }
 
 
-void 
+void
 Sum::myProcess(realvec& in, realvec& out)
 {
 	mrs_real weightValue = ctrl_weight_->to<mrs_real>();
 
-	out.setval(0.0);
-	for (o=0; o < inObservations_; o++)
-		for (t = 0; t < inSamples_; t++)
+	// Sum the observation channels per sample.
+	for (t = 0; t < inSamples_; t++)
+	{
+		out(0, t) = 0;
+		for (o = 0; o < inObservations_; o++)
 		{
-			out(0,t) += (weightValue * in(o,t));
+			out(0, t) += (weightValue * in(o, t));
 		}
+	}
 }
-
-
-
-
-
-
-
-
-
 
--- src/marsyas/Sum.h
+++ src/marsyas/Sum.h
@@ -1,61 +1,61 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
 #ifndef MARSYAS_SUM_H
 #define MARSYAS_SUM_H
 
-#include "MarSystem.h"	
+#include "MarSystem.h"
 
 namespace Marsyas
 {
-/** 
-    \class Sum
+/**
+	\class Sum
 	\ingroup Processing Basic
-    \brief Sum the input rows of observations into one row 
+	\brief Sum the input rows of observations into one row.
 
-    Sum the input rows of observation into one row. Frequently 
-used for mixing audio signals before SoudFileSink or AudioSink. 
+	Sum the input rows of observation into one row. Frequently
+	used for mixing audio signals before SoudFileSink or AudioSink.
 
 	Controls:
 	- \b mrs_real/weight [w] : scales input to avoid clipping.
-\todo Sum mrs_real/weight should be implemented as a meta-composite in
-marsystemmanager.  (?   -- lmartins said this)
+
+	\todo Sum mrs_real/weight should be implemented as a meta-composite in
+	marsystemmanager.  (?   -- lmartins said this)
 
 */
 
 
 class Sum: public MarSystem
 {
-private: 
-  void myUpdate(MarControlPtr sender);
-  MarControlPtr ctrl_weight_;
-  void addControls();  
+private:
+	void myUpdate(MarControlPtr sender);
+	MarControlPtr ctrl_weight_;
+	void addControls();
 public:
-  Sum(std::string name);
-  Sum(const Sum& a);
-  ~Sum();
-  MarSystem* clone() const;  
-  
-  void myProcess(realvec& in, realvec& out);
+	Sum(std::string name);
+	Sum(const Sum& a);
+	~Sum();
+	MarSystem* clone() const;
+
+	void myProcess(realvec& in, realvec& out);
 };
 
 }//namespace Marsyas
 
 #endif
 
-	
