ba923221c6cfd74798b2be9e167a034b399b29e7
stefaanlippens <stefaanlippens@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-06-30 12:59:28 +0000

    simplified the obsNames prefixing with obsNamesAddPrefix() function minor tweaks and comments
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3597 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/Mean.cpp
+++ src/marsyas/Mean.cpp
@@ -1,18 +1,18 @@
 /*
 ** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
-**  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation; either version 2 of the License, or
 ** (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software 
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
@@ -21,10 +21,8 @@
 using namespace std;
 using namespace Marsyas;
 
-Mean::Mean(string name):MarSystem("Mean",name)
+Mean::Mean(string name): MarSystem("Mean", name)
 {
-	//type_ = "Mean";
-	//name_ = name;
 }
 
 
@@ -33,8 +31,8 @@ Mean::~Mean()
 }
 
 
-MarSystem* 
-Mean::clone() const 
+MarSystem*
+Mean::clone() const
 {
 	return new Mean(*this);
 }
@@ -49,26 +47,19 @@ Mean::myUpdate(MarControlPtr sender)
 	ctrl_onObservations_->setValue(ctrl_inObservations_, NOUPDATE);
 	ctrl_osrate_->setValue(ctrl_israte_, NOUPDATE);
 
+	// Allocate memory for the row buffer.
 	obsrow_.create(ctrl_inSamples_->to<mrs_natural>());
 
 	//defaultUpdate(); [!]
+	// \todo what is this doing here?
 	inObservations_ = ctrl_inObservations_->to<mrs_natural>();
 
-	ostringstream oss;
-	string inObsNames = ctrl_inObsNames_->to<mrs_string>();
-	for (int i = 0; i < inObservations_; i++)
-	{
-		string inObsName;
-		string temp;
-		inObsName = inObsNames.substr(0, inObsNames.find(","));
-		temp = inObsNames.substr(inObsNames.find(",")+1, inObsNames.length());
-		inObsNames = temp;
-		oss << "Mean" << "_" << inObsName << ",";
-	}
-	ctrl_onObsNames_->setValue(oss.str(),NOUPDATE);
+	// Add prefix to the observation names.
+	mrs_string inObsNames = ctrl_inObsNames_->to<mrs_string>();
+	ctrl_onObsNames_->setValue(obsNamesAddPrefix(inObsNames, "Mean_"), NOUPDATE);
 }
 
-void 
+void
 Mean::myProcess(realvec& in, realvec& out)
 {
 	out.setval(0.0);
@@ -81,21 +72,6 @@ Mean::myProcess(realvec& in, realvec& out)
 		}
 		out(o,0) = obsrow_.mean();
 	}
-	// INEFFICIENT A LOT OF MEMORY COPYING AT EVERY TICK 
+	// INEFFICIENT A LOT OF MEMORY COPYING AT EVERY TICK
 	// out = in.meanObs();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
