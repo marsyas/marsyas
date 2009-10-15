fbe771b88aefa4f74fe8ccb6802b51b09bea8f12
gtzan <gtzan@0e98fd9d-079e-457f-9c5c-d482923f975d>
2009-10-15 20:17:11 +0000

    Hacked cmake stuff so that the swig bindings work with macports python 2.6
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3724 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ cmake-modules/FindPythonInterp.cmake
@@ -0,0 +1,46 @@
+# - Find python interpreter
+# This module finds if Python interpreter is installed and determines where the
+# executables are. This code sets the following variables:
+#
+#  PythonInterp_FOUND - Was the Python executable found
+#  PythonInterp_EXECUTABLE - path to the Python interpreter
+#  PythonInterp_VERSION - version of the Python interpreter
+#
+#  PYTHON_EXECUTABLE  - Same as PythonInterp_EXECUTABLE, kept for compatibility
+#  PYTHONINTERP_FOUND - Same as PythonInterp_FOUND, kept for compatibility
+#
+
+FIND_PROGRAM(PythonInterp_EXECUTABLE
+  NAMES python2.6 python2.5 python2.4 python2.3 python2.2 python2.1 python2.0 python1.6 python1.5 python
+  PATHS
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.5\\InstallPath]
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.4\\InstallPath]
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.3\\InstallPath]
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.2\\InstallPath]
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.6\\InstallPath]
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.5\\InstallPath]
+  )
+MARK_AS_ADVANCED(PythonInterp_EXECUTABLE)
+SET(PYTHON_EXECUTABLE ${PythonInterp_EXECUTABLE})
+
+SET(PythonInterp_FOUND)
+IF(PythonInterp_EXECUTABLE)
+  SET(PythonInterp_FOUND ON)
+  SET(PythonInterp_VERSION "NO" STRING DOCSTRING "Python version")
+  EXECUTE_PROCESS(COMMAND ${PythonInterp_EXECUTABLE}
+    -c  "import sys; print sys.version"
+    OUTPUT_VARIABLE PYTHONINTERP_version_output
+    ERROR_VARIABLE PYTHONINTERP_version_error
+    RESULT_VARIABLE PYTHONINTERP_version_result
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  IF(NOT ${PYTHONINTERP_version_result} EQUAL 0)
+    MESSAGE(SEND_ERROR "Command '${PythonInterp_EXECUTABLE} -c  \"import sys; print sys.version\" failed with output:\n${PythonInterp_EXECUTABLE_version_error}")
+  ELSE(NOT ${PYTHONINTERP_version_result} EQUAL 0)
+    STRING(REGEX REPLACE "^([0-9.]+).*" "\\1"
+      PythonInterp_VERSION "${PYTHONINTERP_version_output}")
+  ENDIF(NOT ${PYTHONINTERP_version_result} EQUAL 0)
+ENDIF(PythonInterp_EXECUTABLE)
+
+
---
+++ cmake-modules/FindPythonLibs.cmake
@@ -0,0 +1,150 @@
+# - Find python libraries
+# This module finds if Python is installed and determines where the
+# include files and libraries are. It also determines what the name of
+# the library is. This code sets the following variables:
+#
+#  PYTHON_LIBRARIES     = path to the python library
+#  PYTHON_INCLUDE_PATH  = path to where Python.h is found
+#  PYTHON_DEBUG_LIBRARIES = path to the debug library
+#
+
+EXECUTE_PROCESS(COMMAND python-config --prefix OUTPUT_VARIABLE PYTHON_PREFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
+INCLUDE(CMakeFindFrameworks)
+IF(WIN32)
+  FIND_LIBRARY(PYTHON_DEBUG_LIBRARY
+    NAMES python26_d python25_d python24_d python23_d python22_d python21_d python20_d python
+    PATHS
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.6\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.6\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.5\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.5\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.4\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.4\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.3\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.3\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.2\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.2\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.6\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.6\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.5\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.5\\InstallPath]/libs
+  )
+ENDIF(WIN32)
+
+FIND_LIBRARY(PYTHON_LIBRARY
+  NAMES python26 python2.6
+	python25 python2.5
+        python24 python2.4
+        python23 python2.3
+        python22 python2.2
+        python21 python2.1
+        python20 python2.0
+        python16 python1.6
+        python15 python1.5
+
+  PATHS
+    ${PYTHON_PREFIX}/lib
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.6\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.5\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.4\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.3\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.2\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.6\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.5\\InstallPath]/libs
+
+  PATH_SUFFIXES
+    python2.6/config
+    python2.5/config
+    python2.4/config
+    python2.3/config
+    python2.2/config
+    python2.1/config
+    python2.0/config
+    python1.6/config
+    python1.5/config
+  
+  # Avoid finding the .dll in the PATH.  We want the .lib.
+  NO_SYSTEM_ENVIRONMENT_PATH
+)
+
+# Search for the python framework on Apple.
+CMAKE_FIND_FRAMEWORKS(Python)
+SET(PYTHON_FRAMEWORK_INCLUDES)
+IF(Python_FRAMEWORKS)
+  IF(NOT PYTHON_INCLUDE_PATH)
+    FOREACH(version 2.6 2.5 2.4 2.3 2.2 2.1 2.0 1.6 1.5)
+      FOREACH(dir ${Python_FRAMEWORKS})
+        SET(PYTHON_FRAMEWORK_INCLUDES ${PYTHON_FRAMEWORK_INCLUDES}
+          ${dir}/Versions/${version}/include/python${version})
+      ENDFOREACH(dir)
+    ENDFOREACH(version)
+  ENDIF(NOT PYTHON_INCLUDE_PATH)
+ENDIF(Python_FRAMEWORKS)
+
+FIND_PATH(PYTHON_INCLUDE_PATH
+  NAMES Python.h
+
+  PATHS
+    ${PYTHON_PREFIX}/include
+    ${PYTHON_FRAMEWORK_INCLUDES}
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.6\\InstallPath]/include
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.5\\InstallPath]/include
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.4\\InstallPath]/include
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.3\\InstallPath]/include
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.2\\InstallPath]/include
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]/include
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]/include
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.6\\InstallPath]/include
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.5\\InstallPath]/include
+
+  PATH_SUFFIXES
+    python2.6
+    python2.5
+    python2.4
+    python2.3
+    python2.2
+    python2.1
+    python2.0
+    python1.6
+    python1.5
+
+  NO_DEFAULT_PATH
+)
+
+IF (WIN32)
+  MARK_AS_ADVANCED(
+    PYTHON_DEBUG_LIBRARY
+    PYTHON_LIBRARY
+    PYTHON_INCLUDE_PATH
+  )
+ENDIF(WIN32)
+
+# Python Should be built and installed as a Framework on OSX
+IF(Python_FRAMEWORKS)
+STRING(REGEX REPLACE "/Python.*" "" FRAMEWORK_PATH ${PYTHON_PREFIX})
+  # If a framework has been selected for the include path,
+  # make sure "-framework" is used to link it.
+  IF("${PYTHON_INCLUDE_PATH}" MATCHES "Python\\.framework")
+    SET(PYTHON_LIBRARY "")
+    SET(PYTHON_DEBUG_LIBRARY "")
+  ENDIF("${PYTHON_INCLUDE_PATH}" MATCHES "Python\\.framework")
+  IF(NOT PYTHON_LIBRARY)
+    SET (PYTHON_LIBRARY "-F${FRAMEWORK_PATH} -framework Python" CACHE FILEPATH "Python Framework" FORCE)
+  ENDIF(NOT PYTHON_LIBRARY)
+  IF(NOT PYTHON_DEBUG_LIBRARY)
+    SET (PYTHON_DEBUG_LIBRARY "-F${FRAMEWORK_PATH} -framework Python" CACHE FILEPATH "Python Framework" FORCE)
+  ENDIF(NOT PYTHON_DEBUG_LIBRARY)
+ENDIF(Python_FRAMEWORKS)
+
+# We use PYTHON_LIBRARY and PYTHON_DEBUG_LIBRARY for the cache entries
+# because they are meant to specify the location of a single library.
+# We now set the variables listed by the documentation for this
+# module.
+SET(PYTHON_LIBRARIES "${PYTHON_LIBRARY}")
+SET(PYTHON_DEBUG_LIBRARIES "${PYTHON_DEBUG_LIBRARY}")
