b3b44cbbf7454f252c5f3b72d123614e7cc86fcb
paulreimer <paulreimer@0e98fd9d-079e-457f-9c5c-d482923f975d>
2010-01-24 23:02:45 +0000

    Added default CTestConfig.cmake to publish testing results to build.marsyas.info, whenever you 'make Experimental', 'make Nightly', or 'make Continuous'
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3795 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ src/CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "Marsyas")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 PST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "build.marsyas.info")
+set(CTEST_DROP_LOCATION "/submit.php?project=Marsyas")
+set(CTEST_DROP_SITE_CDASH TRUE)
