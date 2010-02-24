df4f6a9a87f49f4e8be15e3c30fa34521a83cfaa
sness <sness@0e98fd9d-079e-457f-9c5c-d482923f975d>
2010-02-24 19:28:14 +0000

    Added marsyas_of directory which contains ofxMarsyas, the wrapper to get Marsyas running under OpenFrameworks
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3813 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ src/marsyas_of/ofxMarsyas/src/ofxMarsyasNetwork.cpp
@@ -0,0 +1,86 @@
+#include "ofxMarsyasNetwork.h"
+
+Marsyas::MarSystemManager ofxMarsyasNetwork::mng;
+
+ofxMarsyasNetwork::ofxMarsyasNetwork(string name)
+: Marsyas::Series(name)
+{
+	targetRate = rate = 0;
+	targetPriority = priority = 1;
+}
+
+ofxMarsyasNetwork::~ofxMarsyasNetwork()
+{
+	stop();
+}
+
+void ofxMarsyasNetwork::threadedFunction() 
+{
+	while( isThreadRunning() )
+	{
+		if (priority != targetPriority)
+		{
+			run();
+			priority = targetPriority;
+		}
+
+		// cout << "ofxMarsyasNetwork trying to lock" << endl;
+		if (lock())
+		{
+		  // cout << "ofxMarsyasNetwork in lock" << endl;
+		
+			tick();
+			thisTick = ofGetSystemTime();
+			rate = ofLerp(rate, 1000.0/(thisTick-lastTick), 0.001);		
+			lastTick = thisTick;
+
+			update();
+			unlock();
+			// cout << "ofxMarsyasNetwork after unlock()" << endl;
+
+		}
+		else {
+		  // cout << "ofxMarsyasNetwork sleeping" << endl;
+			ofSleepMillis(20);
+		}
+
+		 if (targetRate>0)
+		 	ofSleepMillis(1000.0/targetRate);
+	}
+}
+
+void ofxMarsyasNetwork::run() 
+{
+	if (isThreadRunning())
+		stopThread();
+
+#ifndef TARGET_WIN32
+	pthread_attr_t tattr;
+	int oldprio, policy;
+	sched_param param;
+
+	pthread_getschedparam(pthread_self(), &policy, &param);
+	oldprio = param.sched_priority;
+
+	// set a new priority
+	param.sched_priority *= oldprio * targetPriority;
+
+	// set the new scheduling param
+	pthread_attr_setschedparam (&tattr, &param);  
+#endif
+
+	startThread(true, false); // blocking, non-verbose
+
+#ifndef TARGET_WIN32
+	// restore the old priority to prevent scaling from escalating
+	param.sched_priority = oldprio;
+	// set the old scheduling param
+	pthread_attr_setschedparam(&tattr, &param);
+#endif
+}
+
+void ofxMarsyasNetwork::stop() 
+{
+	stopThread();	
+}
+
---
+++ src/marsyas_of/ofxMarsyas/src/ofxMarsyasNetwork.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxThread.h"
+#include "marsyas/Series.h"
+#include "marsyas/MarSystemManager.h"
+
+class ofxMarsyasNetwork : public ofxThread, public Marsyas::Series
+{
+public:
+	ofxMarsyasNetwork(string name = "net");
+	virtual ~ofxMarsyasNetwork();
+
+	virtual void setup()  {};
+	virtual void update() {};
+	virtual void draw()   {};
+	
+	void threadedFunction();
+	void run();
+	void stop();
+	
+	static Marsyas::MarSystemManager mng;
+
+	float targetRate;	
+	float rate;
+	
+	unsigned long thisTick;
+	unsigned long lastTick;
+
+#ifndef TARGET_WIN32
+	float targetPriority;
+	float priority;
+#endif
+};
+
