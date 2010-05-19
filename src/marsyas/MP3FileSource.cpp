fa89cb14be0da27e8a1439ca2147bb29d413c468
hasdisconnected <hasdisconnected@0e98fd9d-079e-457f-9c5c-d482923f975d>
2010-05-19 18:47:34 +0000

    mp3 reading fix - account for recent changes with common.h (added include common.h so the soundfilesource does something again)
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3938 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/MP3FileSource.cpp
+++ src/marsyas/MP3FileSource.cpp
@@ -16,8 +16,9 @@
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-
+#include "common.h"
 #include "MP3FileSource.h"
+
 
 using namespace std;
 using namespace Marsyas;
