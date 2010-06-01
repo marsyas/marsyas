3cdd10fa674fb21ddcc98c59de92dbe748087f21
alex_lerch <alex_lerch@0e98fd9d-079e-457f-9c5c-d482923f975d>
2010-06-01 20:04:15 +0000

    added inclusion of common.h to make marsyas link with Matlab enabled, too
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@3983 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/MarSystem.cpp
+++ src/marsyas/MarSystem.cpp
@@ -16,6 +16,8 @@
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
+#include "common.h"
+
 #include "MarSystem.h"
 #include "MarControlManager.h"
 #include "EvValUpd.h"
