5b6772dbf9e3670767f83dd275b0bca0d7b4af86
sness <sness@0e98fd9d-079e-457f-9c5c-d482923f975d>
2010-06-16 21:16:46 +0000

    Added tests for AimSAI
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4068 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ src/tests/unit_tests/AimSAI_runner.cpp
@@ -0,0 +1,30 @@
+/* Generated file, do not edit */
+
+#ifndef CXXTEST_RUNNING
+#define CXXTEST_RUNNING
+#endif
+
+#define _CXXTEST_HAVE_STD
+#include <cxxtest/TestListener.h>
+#include <cxxtest/TestTracker.h>
+#include <cxxtest/TestRunner.h>
+#include <cxxtest/RealDescriptions.h>
+#include <cxxtest/ErrorPrinter.h>
+
+int main() {
+ return CxxTest::ErrorPrinter().run();
+}
+#include "TestAimSAI.h"
+
+static AimSAI_runner suite_AimSAI_runner;
+
+static CxxTest::List Tests_AimSAI_runner = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_AimSAI_runner( "TestAimSAI.h", 22, "AimSAI_runner", suite_AimSAI_runner, Tests_AimSAI_runner );
+
+static class TestDescription_AimSAI_runner_test_wav_file : public CxxTest::RealTestDescription {
+public:
+ TestDescription_AimSAI_runner_test_wav_file() : CxxTest::RealTestDescription( Tests_AimSAI_runner, suiteDescription_AimSAI_runner, 34, "test_wav_file" ) {}
+ void runTest() { suite_AimSAI_runner.test_wav_file(); }
+} testDescription_AimSAI_runner_test_wav_file;
+
+#include <cxxtest/Root.cpp>
---
+++ src/tests/unit_tests/TestAimSAI.h
@@ -0,0 +1,100 @@
+// TestAimSAI.h
+//
+// Dick Lyon's Pole-Zero Filter Cascade - implemented in C++ by Tom
+// Walters from the AIM-MAT module based on Dick Lyon's code.
+//
+// (c) 2009 - sness@sness.net - GPL
+//
+
+
+#include <cxxtest/TestSuite.h>
+#include <cstdio>
+#include "Collection.h"
+#include "MarSystemManager.h"
+#include "AimSAI.h"
+#include "CommandLineOptions.h"
+
+#include <vector>
+
+using namespace std;
+using namespace Marsyas;
+
+class AimSAI_runner : public CxxTest::TestSuite
+{
+ public:
+
+  void
+  setUp()
+  {
+  }
+
+  //
+  // Test a sample file, test.wav
+  //
+  void test_wav_file(void) {
+    realvec out;
+
+    MarSystemManager mng;
+
+    MarSystem* net = mng.create("Series", "net");
+  
+    net->addMarSystem(mng.create("SoundFileSource", "src"));
+	net->addMarSystem(mng.create("AimPZFC", "aimpzfc"));
+    net->addMarSystem(mng.create("AimHCL", "aimhcl"));
+	net->addMarSystem(mng.create("AimLocalMax", "aimlocalmax"));
+	net->addMarSystem(mng.create("AimSAI", "aimsai"));
+
+    net->updctrl("SoundFileSource/src/mrs_string/filename", "./tests/unit_tests/files/test.wav");
+
+    net->tick();
+
+    out = net->getctrl("mrs_realvec/processedData")->to<mrs_realvec>();
+
+	// Check to see if the generated data is correct.  
+    //
+    // This data was generated with the AIM-C version of SAI.  I
+    // chose three points from the beginning, middle and end of the
+    // range, and compare the Marsyas SAI implementation with these
+    // results
+  	TS_ASSERT_DELTA(out(0,0),0.000493759,0.001);
+  	TS_ASSERT_DELTA(out(0,1),0.000492902,0.001);
+  	TS_ASSERT_DELTA(out(0,2),0.000490549,0.001);
+  	TS_ASSERT_DELTA(out(0,254),0.0,0.001);
+  	TS_ASSERT_DELTA(out(0,255),0.0,0.001);
+  	TS_ASSERT_DELTA(out(0,256),0.0,0.001);
+  	TS_ASSERT_DELTA(out(0,509),0.0,0.001);
+  	TS_ASSERT_DELTA(out(0,510),0.0,0.001);
+  	TS_ASSERT_DELTA(out(0,511),0.0,0.001);
+
+    //
+    // Tick the network 3 more times to see if the values still line up
+    //
+    net->tick();
+    net->tick();
+    net->tick();
+    out = net->getctrl("mrs_realvec/processedData")->to<mrs_realvec>();
+    
+	// Check to see if the generated data is correct.  
+    
+    // This data was generated with the AIM-C version of SAI.  I
+    // chose three points from the beginning, middle and end of the
+    // range, and compare the Marsyas SAI implementation with these
+    // results
+  	TS_ASSERT_DELTA(out(0,0),0.0467726,0.001);
+  	TS_ASSERT_DELTA(out(0,1),0.0467948,0.001);
+  	TS_ASSERT_DELTA(out(0,2),0.0468055,0.001);
+  	TS_ASSERT_DELTA(out(0,254),1.22491e-13,0.001);
+  	TS_ASSERT_DELTA(out(0,255),1.03889e-13,0.001);
+  	TS_ASSERT_DELTA(out(0,256),8.81089e-14,0.001);
+  	TS_ASSERT_DELTA(out(0,509),0.264658,0.001);
+  	TS_ASSERT_DELTA(out(0,510),0.263065,0.001);
+  	TS_ASSERT_DELTA(out(0,511),0.261408,0.001);
+    
+    
+    delete net;
+
+  }
+
+
+
+};
