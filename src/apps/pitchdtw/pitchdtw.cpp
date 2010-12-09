62b6cada7ae8bf9f4f0178b17761c5cabb8b10db
sness <sness@0e98fd9d-079e-457f-9c5c-d482923f975d>
2010-12-09 18:50:09 +0000

    Added ability of pitchdtw to calculate distances between files.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4328 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/apps/pitchdtw/pitchdtw.cpp
+++ src/apps/pitchdtw/pitchdtw.cpp
@@ -20,51 +20,105 @@ void usage()
   cout << "pitchdtw in1.txt in2.txt" << endl;
 }
 
-void pitchdtw(realvec input_realvec,int size1, int size2)
+void pitchdtw(vector<string> filenames, vector<vector<float> > data)
 {
-
-  int numObservations = 2;
-  int numSamples = input_realvec.getCols();
+  unsigned int max_size;
+  realvec input_realvec;
 
   MarSystemManager mng;
 
   // A series to contain everything
   MarSystem* net = mng.create("Series", "series");
-  net->updControl("mrs_natural/inSamples", numSamples);
-  net->updControl("mrs_natural/inObservations", numObservations);
-  net->addMarSystem(mng.create("RealvecSource", "src"));
-  net->updControl("RealvecSource/src/mrs_realvec/data", input_realvec);
-
-  mrs_realvec sizes;
-  sizes.create(2);
-  sizes(0) = size1;
-  sizes(1) = size2;
-
   MarSystem* sim = new SimilarityMatrix("sim");
-  sim->updControl("mrs_realvec/sizes",sizes);
-
+  
   MarSystem* met = new Metric2("met");
   met->updControl("mrs_string/metric","euclideanDistance");
 
   sim->addMarSystem(met);
   net->addMarSystem(sim);
+  
+  // Calculate the DTW cost
+  MarSystem* dtw = new DTW("dtw");
+  dtw->updControl("mrs_string/lastPos","end");
+  dtw->updControl("mrs_string/startPos","zero");
+  dtw->updControl("mrs_bool/weight",false);
+  dtw->updControl("mrs_string/mode","normal");
+  net->addMarSystem(dtw);
 
+  // We're comparing two files, so we always have two observations
+  int numObservations = 2;
 
-   MarSystem* dtw = new DTW("dtw");
-   dtw->updControl("mrs_string/lastPos","end");
-   dtw->updControl("mrs_string/startPos","zero");
-   dtw->updControl("mrs_bool/weight",false);
-   dtw->updControl("mrs_string/mode","normal");
-   net->addMarSystem(dtw);
+  // A vector of distances between all files
+  vector<vector<mrs_real> > distances;
 
-  // Just tick once to pass numSamples through the network
-   //    cout << "*********NETWORK**********" << endl;
-   //    cout << *net << endl;
-  net->tick();
+  // Find the DTW distance between all pairs of files
+  for (unsigned int i = 0; i < data.size(); i++) {
+	vector<mrs_real> tmp_distances;
+  	for (unsigned int j = 0; j < data.size(); j++) {
+		// Which of the two files is longer?
+		if (data[i].size() > data[j].size()) {
+			max_size = data[i].size();
+		} else {
+			max_size = data[j].size();
+		}
+		
+		// Create a realvec to hold both pairs of data
+		input_realvec.create(2,max_size);
 
-  //   cout << "********** output **********" << endl;
-  //   cout << net->getctrl("mrs_realvec/processedData")->to<mrs_realvec>() << endl;  
-   cout << dtw->getctrl("mrs_real/totalDistance")->to<mrs_real>() << endl;  
+		// Copy both data[i] and data[j] into input_realvec.  If one
+		// file is shorter than the other, pad it with zeros.
+ 		for(unsigned int k = 0; k < max_size; k++) {
+		  if (k < data[i].size()) {
+			input_realvec(0,k) = data[i][k];
+		  } else {
+			input_realvec(0,k) = 0;
+		  }
+
+		  if (k < data[j].size()) {
+			input_realvec(1,k) = data[j][k];
+		  } else {
+			input_realvec(1,k) = 0;
+		  }
+		}
+
+		// Update the SimilarityMatrix with the sizes
+		// of the two input vectors
+		mrs_realvec sizes;
+		sizes.create(2);
+		sizes(0) = data[i].size();
+		sizes(1) = data[j].size();
+		sim->updControl("mrs_realvec/sizes",sizes);
+
+		// Update the network with the size of the input data
+		int numSamples = input_realvec.getCols();
+		net->updControl("mrs_natural/inSamples", numSamples);
+		net->updControl("mrs_natural/inObservations", numObservations);
+
+		// Find the size of the output of the network
+		mrs_natural onSamples = net->getControl("mrs_natural/onSamples")->to<mrs_natural>();
+		mrs_natural onObservations = net->getControl("mrs_natural/onObservations")->to<mrs_natural>();
+
+		// Create an output realvec to store the output of the network
+		mrs_realvec output_realvec;
+		output_realvec.create(onObservations,onSamples);
+
+		// Process the data
+		net->process(input_realvec,output_realvec);
+
+		// The distance between this pair of files
+		tmp_distances.push_back(dtw->getctrl("mrs_real/totalDistance")->to<mrs_real>());
+	}
+	distances.push_back(tmp_distances);
+  }
+
+  // Print out all the distances
+  for (unsigned int i = 0; i < distances.size(); i++) {
+	cout << filenames[i] << " ";
+	for (unsigned int j = 0; j < distances[i].size(); j++) {
+	  cout << distances[i][j] << " ";
+	}
+	cout << endl;
+  }
 
 }
   
@@ -78,6 +132,8 @@ void read_file_of_floats_into_vector(string name, vector<float> &file) {
 
   do {
     readChars = fscanf(inFile,"%s", line );
+	if (readChars < 0) 
+	  break;
 	f = atof(line);
 	file.push_back(f);
   } while (readChars > 0);
@@ -86,54 +142,73 @@ void read_file_of_floats_into_vector(string name, vector<float> &file) {
 
 int main(int argc, const char **argv)
 {
-  string inFileName1;
-  string inFileName2;
-
-  vector<float> file1;
-  vector<float> file2;
-
-  realvec input_realvec;
-
-  if (argc < 2) {
-	usage();
-	exit(1);
-  } else {
-	inFileName1 = argv[1];
-	inFileName2 = argv[2];
+  // Read in the filenames from the command line
+  vector<string> filenames;
+  for (int i = 1; i < argc; i++) {
+	filenames.push_back(argv[i]);
   }
+
+  // A vector of vectors of floats to contain all the data from the
+  // files to be processed.
+  vector<vector<float> > data;
   
-  read_file_of_floats_into_vector(inFileName1,file1);
-  read_file_of_floats_into_vector(inFileName2,file2);
-
-  unsigned int max_size;
-
-  if (file1.size() > file2.size()) {
-	max_size = file1.size();
-  } else {
-	max_size = file2.size();
+  // Read in all the data
+  for (unsigned int i = 0; i < filenames.size(); i++) {
+	vector<float> tmp;
+	read_file_of_floats_into_vector(filenames[i],tmp);
+	data.push_back(tmp);
   }
 
-   input_realvec.create(2,max_size);
+  // Calculate the DTW distance for all pairs of files
+  pitchdtw(filenames,data);
+  
+  // string inFileName1;
+  // string inFileName2;
 
-   // Copy both file1 and file2 into input_realvec
-   for(unsigned int i = 0; i < max_size; i++) {
- 	if (i < file1.size()) {
- 	  input_realvec(0,i) = file1[i];
- 	} else {
- 	  input_realvec(0,i) = 0;
- 	}
+  // vector<float> data[i][j];
+  // vector<float> data[i][k];
 
- 	if (i < file2.size()) {
- 	  input_realvec(1,i) = file2[i];
- 	} else {
- 	  input_realvec(1,i) = 0;
- 	}
-   }
+  // realvec input_realvec;
+
+  // if (argc < 2) {
+  // 	usage();
+  // 	exit(1);
+  // } else {
+  // 	inFileName1 = argv[1];
+  // 	inFileName2 = argv[2];
+  // }
+  
+  // read_file_of_floats_into_vector(inFileName1,data[i][j]);
+  // read_file_of_floats_into_vector(inFileName2,data[i][k]);
+
+  // unsigned int max_size;
+
+  // if (data[i][j].size() > data[i][k].size()) {
+  // 	max_size = data[i][j].size();
+  // } else {
+  // 	max_size = data[i][k].size();
+  // }
+
+  //  input_realvec.create(2,max_size);
+
+  //  // Copy both data[i][j] and data[i][k] into input_realvec
+  //  for(unsigned int i = 0; i < max_size; i++) {
+  // 	if (i < data[i][j].size()) {
+  // 	  input_realvec(0,i) = data[i][j][i];
+  // 	} else {
+  // 	  input_realvec(0,i) = 0;
+  // 	}
+
+  // 	if (i < data[i][k].size()) {
+  // 	  input_realvec(1,i) = data[i][k][i];
+  // 	} else {
+  // 	  input_realvec(1,i) = 0;
+  // 	}
+  //  }
 
 
-   pitchdtw(input_realvec,file1.size(),file2.size());
 
-  exit(0);
+  // exit(0);
 
 }
 
