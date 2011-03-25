marsyas/marsyas

commit a7a44acbf580a60c086499cc1b5ab0ffa4c418fc

Made the WekaSource test actually do something
git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4441 0e98fd9d-079e-457f-9c5c-d482923f975d

src/marsyas/MP3FileSource.cpp
@@ -19,8 +19,6 @@
 #include "common.h"
 #include "MP3FileSource.h"
 
-using std::cout;
-using std::endl;
 
 
 using std::ostringstream;
@@ -104,7 +102,9 @@ MP3FileSource::MP3FileSource(const MP3FileSource& a):AbsSoundFileSource(a)
 	
 	ctrl_pos_ = getctrl("mrs_natural/pos");
 	ctrl_currentlyPlaying_ = getctrl("mrs_string/currentlyPlaying");
+	ctrl_previouslyPlaying_ = getctrl("mrs_string/previouslyPlaying");
 	ctrl_currentLabel_ = getctrl("mrs_natural/currentLabel");
+	ctrl_previousLabel_ = getctrl("mrs_natural/previousLabel");
 	ctrl_labelNames_ = getctrl("mrs_string/labelNames");
 	ctrl_nLabels_ = getctrl("mrs_natural/nLabels");
 }
@@ -150,6 +150,7 @@ MP3FileSource::addControls()
 	addctrl("mrs_natural/numFiles", 1);
 	
 	addctrl("mrs_string/currentlyPlaying", "daufile", ctrl_currentlyPlaying_);
+	addctrl("mrs_string/previouslyPlaying", "daufile", ctrl_previouslyPlaying_);
 	addctrl("mrs_natural/currentLabel", 0, ctrl_currentLabel_);
 	addctrl("mrs_string/labelNames",",", ctrl_labelNames_);
 	addctrl("mrs_natural/nLabels", 0, ctrl_nLabels_);
@@ -409,7 +410,9 @@ MP3FileSource::getHeader(mrs_string filename)
   
   
 	ctrl_currentlyPlaying_->setValue(filename, NOUPDATE);
+	ctrl_previouslyPlaying-_->setValue(filename, NOUPDATE);
 	ctrl_currentLabel_->setValue(0, NOUPDATE);
+	ctrl_previousLabel_->setValue(0, NOUPDATE);
 	ctrl_nLabels_->setValue(0, NOUPDATE);
 
 

src/tests/unit_tests/TestWekaSource.h
@@ -23,18 +23,31 @@ class WekaSource_runner : public CxxTest::TestSuite
 {
 public:
   realvec in,out;
-  MarSystemManager mng;
-  WekaSource *norm;
+  MarSystem *net;
 
   void
   setUp()
   {
-	norm = new WekaSource("norm");
   }
 
   void test_pass(void) 
   {
-	TS_ASSERT( 1 + 1 > 1 );
+    MarSystemManager mng;
+    MarSystem* net = mng.create("Series/net");
+    net->addMarSystem(mng.create("WekaSource/wsrc"));
+    net->updControl("WekaSource/wsrc/mrs_string/filename", "files/tiny.arff");
+
+
+    net->tick();
+    mrs_realvec processedData = net->getControl("mrs_realvec/processedData")->to<mrs_realvec>();
+    cout << processedData << endl;
+    TS_TRACE("Checking WekaSource reading data line 1");
+    TS_ASSERT_EQUALS(processedData(0,0), 0.056167);
+    net->tick();
+    processedData = net->getControl("mrs_realvec/processedData")->to<mrs_realvec>();
+    TS_TRACE("Checking WekaSource reading data line 2");
+    TS_ASSERT_EQUALS(processedData(0,0), 0.082136);
+    
   }
 
 

commit 76f8f5b3fd06a5bdb5d639b18ce4928192780178

Fix a typo resulting in a 'not declared in this scope' compilation error. Bug was introduced in r4441
git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4443 0e98fd9d-079e-457f-9c5c-d482923f975d

src/marsyas/MP3FileSource.cpp
@@ -410,7 +410,7 @@ MP3FileSource::getHeader(mrs_string filename)
   
   
 	ctrl_currentlyPlaying_->setValue(filename, NOUPDATE);
-	ctrl_previouslyPlaying-_->setValue(filename, NOUPDATE);
+	ctrl_previouslyPlaying_->setValue(filename, NOUPDATE);
 	ctrl_currentLabel_->setValue(0, NOUPDATE);
 	ctrl_previousLabel_->setValue(0, NOUPDATE);
 	ctrl_nLabels_->setValue(0, NOUPDATE);