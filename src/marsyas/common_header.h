b067efe736a29310ff0c863b36bc0097e6c520f4
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2011-04-10 05:49:31 +0000

    Use const instead of #define.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4462 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/common_header.h
+++ src/marsyas/common_header.h
@@ -21,6 +21,7 @@
 /************************************************************************/
 namespace Marsyas
 {
+	// variables
 	typedef double mrs_real;
 	typedef long mrs_natural;
 	typedef bool mrs_bool;
@@ -30,31 +31,35 @@ namespace Marsyas
 	class realvec;
 	typedef realvec mrs_realvec; //done at realvec.h
 
+	// constants
+	const mrs_natural MRS_DEFAULT_SLICE_NSAMPLES = 512;
+	const mrs_natural MRS_DEFAULT_SLICE_NOBSERVATIONS = 1;
+	const mrs_real    MRS_DEFAULT_SLICE_SRATE = 22050.0;
+
+	const mrs_natural FFT_FORWARD = 1;
+	const mrs_natural FFT_INVERSE = 0;
+
+	const mrs_string EMPTYSTRING = "MARSYAS_EMPTY";
+
+	const mrs_real MAXREAL = std::numeric_limits<mrs_real>::max();
+	const mrs_real MINREAL = std::numeric_limits<mrs_real>::min();
+	const mrs_natural MAXNATURAL = std::numeric_limits<mrs_natural>::max();
+	const mrs_natural MINNATURAL = std::numeric_limits<mrs_natural>::min();
+
+	// double precision
+	const mrs_real PI = 3.14159265358979323846;
+	const mrs_real TWOPI = 6.28318530717958647692;
+
+
 } //namespace Marsyas
 
 /************************************************************************/
 /*  common defines                                                      */
 /************************************************************************/
-#define MRS_DEFAULT_SLICE_NSAMPLES 512
-#define MRS_DEFAULT_SLICE_NOBSERVATIONS 1
-#define MRS_DEFAULT_SLICE_SRATE   22050.0
-
-#define FFT_FORWARD 1
-#define FFT_INVERSE 0
-
-#define EMPTYSTRING "MARSYAS_EMPTY"
-
-#define MAXREAL std::numeric_limits<mrs_real>::max()
-#define MINREAL std::numeric_limits<mrs_real>::min()
-#define MAXNATURAL std::numeric_limits<mrs_natural>::max()
-#define MINNATURAL std::numeric_limits<mrs_natural>::min()
 
 //used for PCM audio => should not be changed!
 #define PCM_MAXSHRT 32767 
 #define PCM_FMAXSHRT 32767.0f 
-
-#define PI 3.14159265358979323846 //double precision instead of 3.14159265359f
-#define TWOPI 6.28318530717958647692 //double precision instead of 6.28318530718f
 
 #ifdef NDEBUG 
 #define MRSASSERT(f) 
