5d058b1a118d4a074fa604e9cabdb0148f74917a
lgmartins <lgmartins@0e98fd9d-079e-457f-9c5c-d482923f975d>
2011-06-20 15:18:00 +0000

    added a simple (the simplest?) Marsyas project example (ever?) :-) - an hello world network which is basically a sine wave generator playing through the audio card.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4509 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ src/apps/helloWorld/helloWorld.cpp
@@ -0,0 +1,64 @@
+/*
+** Copyright (C) 2000-2010 George Tzanetakis <gtzan@cs.uvic.ca>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#include "MarSystemManager.h"
+
+using namespace std;
+using namespace Marsyas;
+
+int
+main(int argc, const char **argv)
+{
+	MRSDIAG("helloWorld.cpp - main");
+
+	cout << "This is probably the simplest Marsyas example code: it simply generates a sine wave with a frequency of 440Hz and send it to the audio card output. Simple press CTRL+C to quit." << endl;
+    
+    //we usualy start by creating a MarSystem manager to help us on MarSystem creation
+    MarSystemManager mng;
+    
+    //create the network, which is a simple Series network with a sine wave
+    //oscilator and a audio sink object to send the ausio data for playing 
+    //in the sound card
+    MarSystem *network = mng.create("Series", "network");
+    network->addMarSystem(mng.create("SineSource", "src"));
+    network->addMarSystem(mng.create("AudioSink", "dest"));
+    
+    //set the window (i.e. audio frame) size (in samples). Let's say, 256 samples.
+    //This is done in the outmost MarSystem (i.e. the Series/network) beacuse flow
+    //controls (as is the case o inSamples) are propagated through the network.
+    //Check the Marsyas documentation for mode details.    
+    network->updControl("mrs_natural/inSamples", 256);
+    
+    //set oscilator frequency to 440Hz
+    network->updControl("SineSource/src/mrs_real/frequency", 440.0);
+    
+    //configure audio sink module
+    network->updControl("AudioSink/dest/mrs_natural/bufferSize", 512); //512 is just some reasonable buffer size - you can try different ones and see what you get...
+    network->updControl("AudioSink/dest/mrs_bool/initAudio", true);
+    
+    //now it's time for ticking the network, ad aeternum (i.e. until the user quits by CTRL+C)
+    while (1) 
+    {
+        network->tick();
+    }
+    
+    //ok, this is not really necessary because we are quiting by CTRL+C, but it's a good habit anyway ;-)
+    delete network;
+    
+	return(0);
+}
