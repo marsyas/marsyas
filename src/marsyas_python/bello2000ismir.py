cd33d2b81ed242ad483826e97044a1e72fad3daa
gtzan <gtzan@0e98fd9d-079e-457f-9c5c-d482923f975d>
2011-06-23 22:51:30 +0000

    Nice parallel rendering of sine pitch track and original in bello2000ismir.py
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4511 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas_python/bello2000ismir.py
+++ src/marsyas_python/bello2000ismir.py
@@ -6,6 +6,7 @@
 
 from pylab import *
 import marsyas
+import sys
 
 msm = marsyas.MarSystemManager()
 
@@ -34,62 +35,111 @@ def multi_plots(net, control_list):
     hold(False)
 
 pitch_spec =  ["Series/pitchExtract",
-               ["SoundFileSource/src",
-                "Windowing/win",
-                "AutoCorrelation/acr",
-                "Peaker/pkr",
-                "MaxArgMax/mxr",
-                "Gain/id"
-                ]
+               [
+                 "SoundFileSource/src",
+                 ["FlowThru/thru",
+                  [
+                    "Windowing/win",
+                    "AutoCorrelation/acr",
+                    "Peaker/pkr",
+                    "MaxArgMax/mxr",
+                    "Gain/id"
+                    ]
+                  ]
+               ]
                ]
 
+sine1 = ["Series/sine1",
+         ["SineSource/src",
+          "Gain/gain"
+          ]
+         ]
+
+sine2 = ["Series/sine2",
+         ["SineSource/src",
+          "Gain/gain"
+           ]
+         ]
+
 sine_spec = ["Series/playBack",
-             ["SineSource/src",
-              "Gain/gain",
-              "SoundFileSink/dest"]
+             [["Fanout/sines",
+              [sine1, sine2]
+              ]
+             ]
              ]
 
-net_spec = ["Series/net",
-            [["Fanout/mix",
-             [pitch_spec, sine_spec]
+sine_spec = ["Series/playBack",
+             [
+               ["Fanout/sines",
+                [sine1, sine2]],
+               "Sum/sum"
              ]]
+
+
+net_spec = ["Series/net",
+            [
+              ["Fanout/mix",
+               [pitch_spec,sine_spec]
+               ],
+              "SoundFileSink/dest"]
             ]
 
 net = create(net_spec)
 
 fname = net.getControl("Fanout/mix/Series/pitchExtract/SoundFileSource/src/mrs_string/filename")
-fname.setValue_string("notes_2.wav")
-dest = net.getControl("Fanout/mix/Series/playBack/SoundFileSink/dest/mrs_string/filename");
+fname.setValue_string(sys.argv[1])
+dest = net.getControl("SoundFileSink/dest/mrs_string/filename");
 dest.setValue_string("output.wav")
-freq = net.getControl("Fanout/mix/Series/playBack/SineSource/src/mrs_real/frequency");
-amp = net.getControl("Fanout/mix/Series/playBack/Gain/gain/mrs_real/gain");
-amplitudes  = zeros(800)
-frequencies = zeros(800)
-pitches = zeros(800)
+freq1 = net.getControl("Fanout/mix/Series/playBack/Fanout/sines/Series/sine1/SineSource/src/mrs_real/frequency");
+freq2 = net.getControl("Fanout/mix/Series/playBack/Fanout/sines/Series/sine2/SineSource/src/mrs_real/frequency");
+amp1 = net.getControl("Fanout/mix/Series/playBack/Fanout/sines/Series/sine1/Gain/gain/mrs_real/gain");
+amp2 = net.getControl("Fanout/mix/Series/playBack/Fanout/sines/Series/sine1/Gain/gain/mrs_real/gain");
 
 
 
-for i in range(800):
+amplitudes  = zeros(200)
+frequencies = zeros(200)
+pitches = zeros(200)
+net.updControl("mrs_natural/inSamples", 2048)
+
+for i in range(200):
   net.tick()
-  multi_plots(net, ["Fanout/mix/Series/pitchExtract/SoundFileSource/src/mrs_realvec/processedData",
-                    "Fanout/mix/Series/pitchExtract/Windowing/win/mrs_realvec/processedData",
-                    "Fanout/mix/Series/pitchExtract/AutoCorrelation/acr/mrs_realvec/processedData"]);
+  # multi_plots(net, ["Fanout/mix/Series/pitchExtract/SoundFileSource/src/mrs_realvec/processedData",
+  #                   "Fanout/mix/Series/pitchExtract/Windowing/win/mrs_realvec/processedData",
+  #                   "Fanout/mix/Series/pitchExtract/AutoCorrelation/acr/mrs_realvec/processedData"]);
   # "Windowing/win/mrs_realvec/processedData",
   #                   "AutoCorrelation/acr/mrs_realvec/processedData",
   #                   "Peaker/pkr/mrs_realvec/processedData"])
   # raw_input("Press Enter to continue")
-  pitchres = net.getControl("Fanout/mix/Series/pitchExtract/MaxArgMax/mxr/mrs_realvec/processedData").to_realvec()
-  print pitchres
-  amplitudes[i] = pitchres[0]
-  if (amplitudes[i] > 0.005): 
-    frequencies[i] = 44100.0 / pitchres[1]
-    pitches[i] = 49 + math.floor(12 * (log(44100.0 / pitchres[1]) / log(2.0))+0.5);
-  else:
-    frequencies[i] = 0.0;
-    pitches[i] = 0.0
-  freq.setValue_real(frequencies[i])
-  
+  pitchres = net.getControl("Fanout/mix/Series/pitchExtract/FlowThru/thru/MaxArgMax/mxr/mrs_realvec/processedData").to_realvec()
 
+    
+  amplitudes[i] = pitchres[0]
+  frequencies[i] = 44100.0 / pitchres[1]
+  if pitchres[0] < 0.0001:
+    frequencies[i] = 5.0
+    amplitudes[i] = 0.0
+    pitches[i] = 20
+  else:
+    pitches[i] = 49 + math.floor(12 * (log(frequencies[i] / pitchres[1]) / log(2.0))+0.5);
+  
+  if (frequencies[i] > 1100):
+    frequencies[i] = 5.0
+    amplitudes[i] = 0.0
+    pitches[i] = 20
+  else:
+    pitches[i] = 49 + math.floor(12 * (log(frequencies[i] / pitchres[1]) / log(2.0))+0.5);
+    
+  if (pitches[i] < 20.0):
+    pitches[i]= 20.0
+  
+    
+  freq1.setValue_real(frequencies[i])
+  freq2.setValue_real(frequencies[i] * 2)
+  amp1.setValue_real(amplitudes[i]);
+  amp2.setValue_real(amplitudes[i] * 0.5);
+
+  
 figure(2)
 plot(amplitudes);
 figure(3)
