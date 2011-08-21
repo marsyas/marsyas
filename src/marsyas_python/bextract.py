cd65a8e3f02607267e3ed87f91d28c451409a638
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2011-08-21 00:04:58 +0000

    Revise marsyas python bextract.
    I can't see any way to simplify the linkControl and updateControl
    stuff.  Any ideas?
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4605 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas_python/bextract.py
+++ src/marsyas_python/bextract.py
@@ -1,50 +1,78 @@
+#!/usr/bin/env python
+
 # bextract implemented using the swig python Marsyas bindings
 # George Tzanetakis, January, 16, 2007 
+# revised Graham Percival, 2011 August 20
 
+import sys
 import marsyas
+import marsyas_util
+
+try:
+    mf_filename = sys.argv[1]
+    arff_filename = mf_filename.replace(".mf", ".arff")
+    mpl_filename = mf_filename.replace(".mpl", ".arff")
+except:
+    print "Usage: py-bextract.py filename.mf"
+    sys.exit(1)
 
 # Create top-level patch
-mng = marsyas.MarSystemManager()
-fnet = mng.create("Series", "featureNetwork")
-
-# functional short cuts to speed up typing 
-create = mng.create
-add = fnet.addMarSystem 
-link = fnet.linkControl
-upd = fnet.updControl
-get  = fnet.getControl
-
-# Add the MarSystems 
-add(create("SoundFileSource", "src"))
-add(create("TimbreFeatures", "featExtractor"))
-add(create("TextureStats", "tStats"))
-add(create("Annotator", "annotator"))
-add(create("WekaSink", "wsink"))
+net = marsyas_util.create(
+    ["Series/extract_network",
+        ["SoundFileSource/src",
+         "TimbreFeatures/featExtractor",
+         "TextureStats/tStats",
+         "Annotator/annotator",
+         "WekaSink/wsink",
+    ]])
 
 # link the controls to coordinate things
-link("mrs_string/filename", "SoundFileSource/src/mrs_string/filename")
-link("mrs_bool/hasData", "SoundFileSource/src/mrs_bool/hasData")
-link("WekaSink/wsink/mrs_string/currentlyPlaying","SoundFileSource/src/mrs_string/currentlyPlaying")
-link("Annotator/annotator/mrs_natural/label", "SoundFileSource/src/mrs_natural/currentLabel")
-link("SoundFileSource/src/mrs_natural/nLabels", "WekaSink/wsink/mrs_natural/nLabels")
+net.linkControl("mrs_string/filename",
+    "SoundFileSource/src/mrs_string/filename")
+net.linkControl("mrs_bool/hasData",
+    "SoundFileSource/src/mrs_bool/hasData")
+net.linkControl("WekaSink/wsink/mrs_string/currentlyPlaying",
+    "SoundFileSource/src/mrs_string/currentlyPlaying")
+net.linkControl("Annotator/annotator/mrs_real/label",
+    "SoundFileSource/src/mrs_real/currentLabel")
+net.linkControl("SoundFileSource/src/mrs_natural/nLabels",
+    "WekaSink/wsink/mrs_natural/nLabels")
 
-# update controls to setup things 
-upd("TimbreFeatures/featExtractor/mrs_string/disableTDChild", marsyas.MarControlPtr.from_string("all"))
-upd("TimbreFeatures/featExtractor/mrs_string/disableLPCChild", marsyas.MarControlPtr.from_string("all"))
-upd("TimbreFeatures/featExtractor/mrs_string/disableSPChild", marsyas.MarControlPtr.from_string("all"))
-upd("TimbreFeatures/featExtractor/mrs_string/enableSPChild", marsyas.MarControlPtr.from_string("MFCC/mfcc"))
-upd("mrs_string/filename", marsyas.MarControlPtr.from_string("bextract_single.mf"))
-upd("WekaSink/wsink/mrs_string/labelNames", 
-			  get("SoundFileSource/src/mrs_string/labelNames"))
-upd("WekaSink/wsink/mrs_string/filename", marsyas.MarControlPtr.from_string("bextract_python.arff"))
+# set up features to extract
+net.updControl("TimbreFeatures/featExtractor/mrs_string/enableTDChild",
+    marsyas.MarControlPtr.from_string("ZeroCrossings/zcrs"))
+net.updControl("TimbreFeatures/featExtractor/mrs_string/enableLPCChild",
+    marsyas.MarControlPtr.from_string("Series/lspbranch"))
+net.updControl("TimbreFeatures/featExtractor/mrs_string/enableLPCChild",
+    marsyas.MarControlPtr.from_string("Series/lpccbranch"))
+net.updControl("TimbreFeatures/featExtractor/mrs_string/enableSPChild",
+    marsyas.MarControlPtr.from_string("MFCC/mfcc"))
+net.updControl("TimbreFeatures/featExtractor/mrs_string/enableSPChild",
+    marsyas.MarControlPtr.from_string("SCF/scf"))
+net.updControl("TimbreFeatures/featExtractor/mrs_string/enableSPChild",
+    marsyas.MarControlPtr.from_string("Rolloff/rf"))
+net.updControl("TimbreFeatures/featExtractor/mrs_string/enableSPChild",
+    marsyas.MarControlPtr.from_string("Flux/flux"))
+net.updControl("TimbreFeatures/featExtractor/mrs_string/enableSPChild",
+    marsyas.MarControlPtr.from_string("Centroid/cntrd"))
+net.updControl("TimbreFeatures/featExtractor/mrs_string/enableSPChild",
+    marsyas.MarControlPtr.from_string("Series/chromaPrSeries"))
 
-# do the processing extracting MFCC features and writing to weka file 
+# setup filenames 
+net.updControl("mrs_string/filename",
+    marsyas.MarControlPtr.from_string(mf_filename))
+net.updControl("WekaSink/wsink/mrs_string/labelNames", 
+    net.getControl("SoundFileSource/src/mrs_string/labelNames"))
+net.updControl("WekaSink/wsink/mrs_string/filename",
+    marsyas.MarControlPtr.from_string(arff_filename))
+
+# do the processing, extracting features, and writing to weka file 
 previouslyPlaying = ""
-while get("SoundFileSource/src/mrs_bool/hasData").to_bool():
-	currentlyPlaying = get("SoundFileSource/src/mrs_string/currentlyPlaying").to_string()
-	if (currentlyPlaying != previouslyPlaying):
-		print "Processing: " +  get("SoundFileSource/src/mrs_string/currentlyPlaying").to_string()
-		
-	fnet.tick()
-	
-	previouslyPlaying = currentlyPlaying
+while net.getControl("SoundFileSource/src/mrs_bool/hasData").to_bool():
+    currentlyPlaying = net.getControl("SoundFileSource/src/mrs_string/currentlyPlaying").to_string()
+    if (currentlyPlaying != previouslyPlaying):
+        print "Processing: ",
+        print net.getControl("SoundFileSource/src/mrs_string/currentlyPlaying").to_string()
+    net.tick() # update time
+    previouslyPlaying = currentlyPlaying
+
