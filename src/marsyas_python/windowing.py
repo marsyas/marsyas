d88596162828dd073a042eab2c75a86094243812
tiagoft <tiagoft@0e98fd9d-079e-457f-9c5c-d482923f975d>
2011-09-23 18:41:52 +0000

    Changed windowing.py so that it uses the compact notation for MarSystems. Also, added comments on it.
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4649 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas_python/windowing.py
+++ src/marsyas_python/windowing.py
@@ -1,102 +1,111 @@
-# EXAMPLE OF USING Marsyas together with PyLab
-# for MATLAB-like plotting
-# run using ipython -pylab testPylab.py foo.wav 
-
 #!/usr/bin/env python
 
+# This source code will serve as good tutorial on how to program a more complex MarSystem network using a compact syntax.
+# This syntax will be interpreted by the functions in marsyas_util.py (which should be imported, as well).
+# This program should be the next step in learning after helloworld.py.
+# Some more advanced controls will be shown
 
 from pylab import *
 import sys
 from matplotlib import pyplot
-from marsyas import MarSystemManager,MarControlPtr
+from marsyas import *
+from marsyas_util import *
 
-# create a global MarSystemManager 
-msm = MarSystemManager()
+# marsyas_util gives us, automatically, the line:
+# msm = marsyas.MarSystemManager()
 
+# This is a parenthesis-like notation, in which we will specify all the basic blocks in our topology.
+# The specification works like this:
+# Every time you specify a new subsystem, it should be preceded by a [, which will close when the specification ends
+# Also, all subsystems are specified in the following list, so, you will get things like this:
+# ["Begin/System", ["SystemItem1", "SystemItem2"]]
+# That is very similar to LISP, but with brackets instead of parenthesis.
+# Can you draw the block diagram for this following specification?
+spec = ["Series/MySystem", 
+					[["Fanout/additive",
+						["SineSource/src1",
+						"SineSource/src2"]],
+					"Sum/sum",					
+					["Parallel/analyzers",
+						[["Series/branch1", 
+							["Spectrum/spk","PowerSpectrum/pspk","Gain/gain"]],
+						["Series/branch2", 
+							["Windowing/win","Spectrum/spk","PowerSpectrum/pspk","Gain/gain"]],
+						["Series/branch3", 
+							["Windowing/win","Spectrum/spk","PowerSpectrum/pspk","Gain/gain"]]]]]]
 
-# helper function to create a list of MarSystems from a list of names
-def create(msys_list_names):
-	return map(msm.create,msys_list_names)
+# We will call the create() function, which is defined in the marsyas_util.py file. It will create a marsystem just
+# as specified aboce.
+net = create(spec)
 
-# create a Series MarSystem composed of a list of MarSystems
-def series(name, msys_list):
-	net = msm.create("Series/"+name)
-	map(net.addMarSystem, msys_list)
-	return net
+# Now, we are going to link specific controls of our objects to top-level controls, so we can access them in
+# an easier way. Below, we will make code to access Fanout/additive/SineSource/src1/mrs_real/frequency
+# using simply mrs_real/frequency1
 
-# create a Fanout MarSystem composed of a list of MarSystems
-def fanout(name, msys_list):
-	net = msm.create("Fanout/"+name)
-	map(net.addMarSystem, msys_list)
-	return net
+# make top level controls for the frequencies of the two oscillators 
+net.linkControl("Fanout/additive/SineSource/src1/mrs_real/frequency", "mrs_real/frequency1");
+net.linkControl("Fanout/additive/SineSource/src2/mrs_real/frequency", "mrs_real/frequency2");
 
-# create the network and make nice interface with top-level controls 
-def create_network_fanout():
+# You can link two or more controls to the same top-level control, so the same control will change parameters for multiple
+# controls in the network.
 
-	# Input is the sum of two sinusoidal oscillators 
-	additive = fanout("additive", create(["SineSource/src1","SineSource/src2"]))
-	
-	# The magnitude spectrum in dB is calculated without and with Windowing 
-	branch1 = series("branch1",create(["Spectrum/spk","PowerSpectrum/pspk","Gain/gain"]))
-	branch2 = series("branch2",create(["Windowing/win","Spectrum/spk","PowerSpectrum/pspk","Gain/gain"]))
-	branch3 = series("branch3",create(["Windowing/win","Spectrum/spk","PowerSpectrum/pspk","Gain/gain"]))
-	
-	net = series("net", [additive,
-						 msm.create("Sum/sum"),
-						 fanout("mix",
-								[branch1,
-								 branch2,
-								 branch3])])
+# map all spectrum types to the same top level control 
+net.linkControl("Parallel/analyzers/Series/branch1/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
+net.linkControl("Parallel/analyzers/Series/branch2/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
+net.linkControl("Parallel/analyzers/Series/branch3/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
 
-	# make top level controls for the frequencies of the two oscillators 
-	net.linkControl("Fanout/additive/SineSource/src1/mrs_real/frequency", "mrs_real/frequency1");
-	net.linkControl("Fanout/additive/SineSource/src2/mrs_real/frequency", "mrs_real/frequency2");
+#	# make top level controls for the 3 spectrums 
+net.linkControl("Parallel/analyzers/Series/branch1/PowerSpectrum/pspk/mrs_realvec/processedData","mrs_realvec/spectrum1")
+net.linkControl("Parallel/analyzers/Series/branch2/PowerSpectrum/pspk/mrs_realvec/processedData","mrs_realvec/spectrum2")
+net.linkControl("Parallel/analyzers/Series/branch3/PowerSpectrum/pspk/mrs_realvec/processedData","mrs_realvec/spectrum3")
 
-	# map all spectrum types to the same top level control 
-	net.linkControl("Fanout/mix/Series/branch1/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
-	net.linkControl("Fanout/mix/Series/branch2/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
-	net.linkControl("Fanout/mix/Series/branch3/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
+# Now, we will setup our analyzers in each analyzer branch. 
+# In branch1, we have no window, hence it is a rectangular-shaped window.
+# In branch2, we will have a hamming window, which is the default (hence it does not have to be changed)
+# In branch3, we will have a hann window, which is set below:
 
-	# make top level controls for the 3 spectrums 
-	net.linkControl("Fanout/mix/Series/branch1/PowerSpectrum/pspk/mrs_realvec/processedData","mrs_realvec/spectrum1")
-	net.linkControl("Fanout/mix/Series/branch2/PowerSpectrum/pspk/mrs_realvec/processedData","mrs_realvec/spectrum2")
-	net.linkControl("Fanout/mix/Series/branch3/PowerSpectrum/pspk/mrs_realvec/processedData","mrs_realvec/spectrum3")
-
-	# make the third branch window by a Hann window instead of the default Hamming 
-	net.updControl("Fanout/mix/Series/branch3/Windowing/win/mrs_string/type", "Hann");
-	return net
-
-# set the controls and plot the data 								
-def plot_figure():
-	net = create_network_fanout()
-	
-	spectrumType = net.getControl("mrs_string/spectrumType");
-	spectrumType.setValue_string("decibels")
-	freq1 = net.getControl("mrs_real/frequency1");
-	freq2 = net.getControl("mrs_real/frequency2");
-	outData1 = net.getControl("mrs_realvec/spectrum1");
-	outData2 = net.getControl("mrs_realvec/spectrum2");
-	outData3 = net.getControl("mrs_realvec/spectrum3");
-	freq1.setValue_real(1500.0);
-	freq2.setValue_real(3000.0);
-
-	net.tick()
-	plot(linspace(0,11050, 257),outData1.to_realvec(), label="No windowing")
-	plot(linspace(0,11050, 257),outData2.to_realvec(), label="Hamming")
-	plot(linspace(0,11050, 257),outData3.to_realvec(), label="Hanning")
-	
-	xlabel("Frequency (Hz)")
-	ylabel("Magnitude (dB)");
-	suptitle("HELLO BABY");
-	legend()
+#	# make the third branch window by a Hann window instead of the default Hamming 
+net.updControl("Parallel/analyzers/Series/branch3/Windowing/win/mrs_string/type", "Hann");
 
 
-	# save .svg and .ps versions of the figure 
-	savefig('windowing.svg')
-	savefig('windowing.ps')
-	
-	show()
+# Using the getControl() method, we access the controls for a certain component of our net.
+# The following commands will set that we want our output for the spectrumTupe
+# (remember, we linked that to all three spectral analyzers above!)
+spectrumType = net.getControl("mrs_string/spectrumType");
+spectrumType.setValue_string("decibels")
 
+# We are doing something similar here. Note that this is equivalent to using the net.updControl() method properly,
+# but it might save you some work later. After you have executed this, uncomment the last two lines of this block and
+# see what the output is like!
+freq1 = net.getControl("mrs_real/frequency1");
+freq2 = net.getControl("mrs_real/frequency2");
+freq1.setValue_real(1500.0);
+freq2.setValue_real(3000.0);
+#net.updControl("mrs_real/frequency1", 2000.0);
+#net.updControl("mrs_real/frequency2", 4000.0);
 
-# call the plot function 
-plot_figure()
+# These lines are important, because they will link an output control (the processedData) to a variable
+# which we can easily access.
+outData1 = net.getControl("mrs_realvec/spectrum1");
+outData2 = net.getControl("mrs_realvec/spectrum2");
+outData3 = net.getControl("mrs_realvec/spectrum3");
+
+# And then, we run our network for one tick (we only need one frame!).
+net.tick()
+
+# These commands below are for plotting.
+plot(linspace(0,11050, 257),outData1.to_realvec(), label="No windowing")
+plot(linspace(0,11050, 257),outData2.to_realvec(), label="Hamming")
+plot(linspace(0,11050, 257),outData3.to_realvec(), label="Hanning")
+
+xlabel("Frequency (Hz)")
+ylabel("Magnitude (dB)");
+suptitle("Marsyas windowing demo");
+legend()
+
+# save .svg and .ps versions of the figure 
+savefig('windowing.svg')
+savefig('windowing.ps')
+
+show()
+
