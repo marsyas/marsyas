5f7e4580df29ee2a313a6d457f34d1cf493b5c42
tiagoft <tiagoft@0e98fd9d-079e-457f-9c5c-d482923f975d>
2012-03-07 00:21:06 +0000

    spectral_analysis.py now has workarounds to deal with weird sample rate issues from AudioSource
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4737 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas_python/spectral_analysis.py
+++ src/marsyas_python/spectral_analysis.py
@@ -18,13 +18,13 @@ import math
 # These are the parameters we want to set:
 # For the analysis:
 Window_len = 2048	# The number of samples in each analysis window
-Window_step = 1024	# The step (in samples) between two consecutive analysis
-Zero_padding = 2	# After windowing, the signal will be zero-padded to this value times its length
-Min_freq = 20			# Hz. The minimum frequency that will be analyzed
-Max_freq = 5000		# Hz. The maximum frequency that will be analyzed
+Window_step = 512	# The step (in samples) between two consecutive analysis
+Zero_padding = 1	# After windowing, the signal will be zero-padded to this value times its length
+Min_freq = 0			# Hz. The minimum frequency that will be analyzed
+Max_freq = 1700		# Hz. The maximum frequency that will be analyzed
 
 # The following lines will determine the structure of the marsystem
-spec_analyzer = ["Series/analysis", ["AudioSource/asrc", "Gain/pregain", "ShiftInput/sft", "Windowing/win","Spectrum/spk","PowerSpectrum/pspk"]] 
+spec_analyzer = ["Series/analysis", ["AudioSource/asrc",  "Sum/summation", "ShiftInput/sft", "Windowing/win","Spectrum/spk","PowerSpectrum/pspk"]] 
 net = marsyas_util.create(spec_analyzer)
 snet = marsyas_util.mar_refs(spec_analyzer)
 
@@ -36,22 +36,24 @@ net.updControl("mrs_real/israte", fs);
 net.updControl(snet["sft"]+"/mrs_natural/winSize", Window_len);
 net.updControl(snet["win"]+"/mrs_natural/zeroPadding", Window_len * (Zero_padding-1));
 net.updControl(snet["win"]+"/mrs_string/type", "Hanning"); # "Hamming", "Hanning", "Triangle", "Bartlett", "Blackman"
-net.updControl(snet["asrc"]+"/mrs_natural/nChannels", 1);
+net.updControl(snet["asrc"]+"/mrs_natural/nChannels", 2);
 net.updControl(snet["asrc"]+"/mrs_bool/initAudio", marsyas.MarControlPtr.from_bool(True));
 net.updControl(snet["pspk"]+"/mrs_string/spectrumType", "logmagnitude2");  # "power", "magnitude", "decibels", "logmagnitude" (for 1+log(magnitude*1000), "logmagnitude2" (for 1+log10(magnitude)), "powerdensity" 
 
 # These variables will avoid having to re-calculate stuff
-DFT_SIZE = Window_len * Zero_padding * 1.0; # This is the size of the DFT
+DFT_SIZE = Window_len * Zero_padding; # This is the size of the DFT
 DFT_SIZE_2 = net.getControl(snet["win"]+"/mrs_natural/onSamples").to_natural();
-#print DFT_SIZE
-#print DFT_SIZE_2
+print "Debug parameters"
+print DFT_SIZE
+print DFT_SIZE_2
 freq_bin = fs/DFT_SIZE; # this is the frequency hop for every frequency bin in the DFT
-
+print freq_bin
 # This is the size of data that will be shown
 visible_time = 15; # Seconds
 minK = int(math.floor(Min_freq/freq_bin))
 maxK = int(math.ceil(Max_freq/freq_bin))
 deltaK = maxK-minK+1
+print minK, maxK, deltaK
 nTime = int(math.ceil(visible_time*(fs*1.0/Window_step)))
 
 # Allocate memory for the image
@@ -68,6 +70,7 @@ try:
 		out = numpy.array(out)
 		out = out[minK:maxK+1]
 		out = out [::-1]
+		
 		if numpy.max(out)>0:
 			out = out/numpy.max(out)
 		else:
