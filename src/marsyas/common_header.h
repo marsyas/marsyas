5b01d2f4deaa7e3984e0b4c454f6be2c4462a4b4
lgmartins <lgmartins@0e98fd9d-079e-457f-9c5c-d482923f975d>
2012-06-26 10:47:29 +0000

    weird undefined max and min macros… WTF? ;-)
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4746 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/common_header.h
+++ src/marsyas/common_header.h
@@ -32,8 +32,8 @@ namespace Marsyas
 	const mrs_natural FFT_INVERSE = 0;
 
 	const mrs_string EMPTYSTRING = "MARSYAS_EMPTY";
-    #undef max
-    #undef min
+	#undef max //WTF??
+	#undef min //WTF??
 
 	const mrs_real MAXREAL = std::numeric_limits<mrs_real>::max();
 	const mrs_real MINREAL = std::numeric_limits<mrs_real>::min();
