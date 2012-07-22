7cfa66e3d33fc04c6770483aae46f997f44b3b87
lgauthie <lgauthie@0e98fd9d-079e-457f-9c5c-d482923f975d>
2012-07-22 07:40:00 +0000

    Cleaned up Source File a bit
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4766 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/marsyas/Plucked.cpp
+++ src/marsyas/Plucked.cpp
@@ -23,104 +23,105 @@ using namespace Marsyas;
 
 Plucked::Plucked(mrs_string name):MarSystem("Plucked",name)
 {
-  pointer1_ = 0;
-  pointer2_ = 0;
-  pointer3_ = 0;
-  a_ = 0;
-  b_ = 0;
-  noteon_ = 0.0;
-  delaylineSize_ = 0;
-  gain_ = NULL;
+	pointer1_ = 0;
+	pointer2_ = 0;
+	pointer3_ = 0;
+	a_ = 0;
+	b_ = 0;
+	noteon_ = 0.0;
+	delaylineSize_ = 0;
+	gain_ = NULL;
 
-  addControls();
+	addControls();
 }
 
 Plucked::~Plucked()
 {
-  delete gain_;
+	delete gain_;
 }
 
 MarSystem* 
 Plucked::clone() const 
 {
-  return new Plucked(*this);
+	return new Plucked(*this);
 }
 
 void 
 Plucked::addControls()
 {
-  addctrl("mrs_real/frequency", 100.0);
-  addctrl("mrs_real/pluckpos", 0.5);
-  addctrl("mrs_real/nton", 0.5);
-  addctrl("mrs_real/loss",1.0);
-  addctrl("mrs_real/stretch",0.2);
-  setctrlState("mrs_real/frequency", true);
-  setctrlState("mrs_real/nton", true);
-  setctrlState("mrs_real/loss", true);
+	addctrl("mrs_real/frequency", 100.0);
+	addctrl("mrs_real/pluckpos", 0.5);
+	addctrl("mrs_real/nton", 0.5);
+	addctrl("mrs_real/loss",1.0);
+	addctrl("mrs_real/stretch",0.2);
+	setctrlState("mrs_real/frequency", true);
+	setctrlState("mrs_real/nton", true);
+	setctrlState("mrs_real/loss", true);
 }
 
 void
 Plucked::myUpdate(MarControlPtr sender)
 {
 	mrs_natural t;
-  MRSDIAG("Plucked.cpp - Plucked:localuUpdate");
-  
-  MarSystem::myUpdate(sender);
+	MRSDIAG("Plucked.cpp - Plucked:localuUpdate");
 
-  gain_ = new Gain("pluckedGain");
-  gain_->updControl("mrs_natural/inSamples", getctrl("mrs_natural/inSamples"));
-  gain_->updControl("mrs_natural/inSamples", getctrl("mrs_natural/inSamples"));
-  gain_->updControl("mrs_real/israte", getctrl("mrs_real/israte"));
-  gain_->updControl("mrs_real/gain", 2.0);
-   
-  gout_.create(gain_->getctrl("mrs_natural/inObservations")->to<mrs_natural>(), 
-	       gain_->getctrl("mrs_natural/inSamples")->to<mrs_natural>());
-   
-  mrs_real freq = getctrl("mrs_real/frequency")->to<mrs_real>();
-  mrs_real pos = getctrl("mrs_real/pluckpos")->to<mrs_real>();
-  noteon_ = getctrl("mrs_real/nton")->to<mrs_real>();
+	MarSystem::myUpdate(sender);
 
-  loss_ = getctrl("mrs_real/loss")->to<mrs_real>();  
-  
-  s_ = getctrl("mrs_real/stretch")->to<mrs_real>();
+	gain_ = new Gain("pluckedGain");
+	gain_->updControl("mrs_natural/inSamples", getctrl("mrs_natural/inSamples"));
+	gain_->updControl("mrs_natural/inSamples", getctrl("mrs_natural/inSamples"));
+	gain_->updControl("mrs_real/israte", getctrl("mrs_real/israte"));
+	gain_->updControl("mrs_real/gain", 2.0);
 
-  // loweset frequency on a piano is 27.5Hz ... 22050/27.5 ~= 802*2 for commute
-  // this is the longest delay line required
-  
-  if (delaylineSize_ == 0) 
-    {
-      delaylineSize_ = 2048;
-      noise_.create((mrs_natural)delaylineSize_);
-      delayline1_.create((mrs_natural)delaylineSize_);
-      pickDelayLine_.create((mrs_natural)delaylineSize_);
-      
-      for (t = 0; t < delaylineSize_; t++)
+	gout_.create(gain_->getctrl("mrs_natural/inObservations")->to<mrs_natural>(), 
+		   gain_->getctrl("mrs_natural/inSamples")->to<mrs_natural>());
+
+	mrs_real freq = getctrl("mrs_real/frequency")->to<mrs_real>();
+	mrs_real pos = getctrl("mrs_real/pluckpos")->to<mrs_real>();
+	noteon_ = getctrl("mrs_real/nton")->to<mrs_real>();
+
+	loss_ = getctrl("mrs_real/loss")->to<mrs_real>();  
+
+	s_ = getctrl("mrs_real/stretch")->to<mrs_real>();
+
+	// loweset frequency on a piano is 27.5Hz ... 22050/27.5 ~= 802*2 for commute
+	// this is the longest delay line required
+	if (delaylineSize_ == 0) 
 	{
-	  noise_(t) = (mrs_real)(rand() / (RAND_MAX + 1.0) -0.5);
-	}
+		delaylineSize_ = 2048;
+		noise_.create((mrs_natural)delaylineSize_);
+		delayline1_.create((mrs_natural)delaylineSize_);
+		pickDelayLine_.create((mrs_natural)delaylineSize_);
+	  
+		for (t = 0; t < delaylineSize_; t++)
+		{
+			noise_(t) = (mrs_real)(rand() / (RAND_MAX + 1.0) -0.5);
+		}
     }
   
-  if (noteon_ > 0)
+	if (noteon_ > 0)
     {		
-      a_ = 0;
-      d_ = 2*22050/freq;
-      N_ = (mrs_natural)floor(d_);
-      g_=-(-1+d_)/(-d_-1);//for all pass implementation 
-      picklength_= (mrs_natural)floor(N_*pos);//for inverse comb implementation
-      
-      for (t = 0; t < N_; t++)
+		a_ = 0;
+		d_ = 2*22050/freq;
+		N_ = (mrs_natural)floor(d_);
+		g_=-(-1+d_)/(-d_-1);//for all pass implementation 
+		picklength_= (mrs_natural)floor(N_*pos);//for inverse comb implementation
+
+		for (t = 0; t < N_; t++)
 	{
-	  pickDelayLine_(0)=noise_(t);
-	  delayline1_(t) = noise_(t)+ (mrs_real)0.1 * pickDelayLine_(picklength_-1);
-	  
-	  
-	  //shift the pick delayline to the right 1 cell
-	  for(p=0; p<=picklength_-2; p++)
-	    pickDelayLine_(picklength_-1-p) = pickDelayLine_(picklength_-1-p-1);
+		pickDelayLine_(0)=noise_(t);
+		delayline1_(t) = noise_(t)+ (mrs_real)0.1 * pickDelayLine_(picklength_-1);
+
+
+		//shift the pick delayline to the right 1 cell
+		for(p=0; p<=picklength_-2; p++)
+		{
+			pickDelayLine_(picklength_-1-p) = pickDelayLine_(picklength_-1-p-1);
+		}
 	}
-      wp_ = 1;
-      wpp_ = 0;
-      rp_ = N_-1;
+		wp_ = 1;
+		wpp_ = 0;
+		rp_ = N_-1;
     }
 }
 
@@ -153,10 +154,3 @@ Plucked::myProcess(realvec &in, realvec &out)
   gain_->process(gout_, out);
 }
 
-
-
-
-
-
-
-	
