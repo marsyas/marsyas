58d07aeb762d15b59b2fd6f103add03c8c72dab3
avperrotta <avperrotta@0e98fd9d-079e-457f-9c5c-d482923f975d>
2012-08-02 12:26:34 +0000

    Commit with Georges soundsink bug fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@4791 0e98fd9d-079e-457f-9c5c-d482923f975d

---
+++ src/ofApps/MarsyasNetworkEditor/marsyas/Energy.h
@@ -0,0 +1,52 @@
+/*
+** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
+**  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software 
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef MARSYAS_ENERGY_H
+#define MARSYAS_ENERGY_H
+
+#include "MarSystem.h"	
+
+namespace Marsyas
+{
+/** 
+    \class Energy
+	\ingroup Analysis
+    \brief compute the Energy of the input observations into one column
+*/
+
+
+class Energy: public MarSystem
+{
+private: 
+  void myUpdate(MarControlPtr sender);
+public:
+  Energy(std::string name);
+  Energy(const Energy& a);
+  ~Energy();
+  MarSystem* clone() const;  
+  
+  void addControls();
+
+  void myProcess(realvec& in, realvec& out);
+};
+
+}//namespace Marsyas
+
+#endif
+
+	
---
+++ src/ofApps/MarsyasNetworkEditor/marsyas/Upsample.h
@@ -0,0 +1,60 @@
+/*
+** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
+**  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software 
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef MARSYAS_UPSAMPLE_H
+#define MARSYAS_UPSAMPLE_H
+
+#include "MarSystem.h"	
+
+namespace Marsyas
+{
+/** 
+    \class Upsample
+	\ingroup none
+    \brief Upsample input realvec by integer factor using chosen interpolation. Frame size is also increased by the same factor.
+
+		Upsamples a signal by a factor n so that out[t]=in[n*t]. Also runs some kind of interpolation to find out the value for unknown samples:
+		"none": unknown samples are unchanged.
+		"repeat": unknown sample [t] equals known sample [t+1]
+
+         Controls:
+         - \b mrs_natural/factor [w] : adjust the upsampling factor.
+				 - \b mrs_string/mode [w] : change the interpolation mode. So far, you can use "none" or "repeat".
+				 - \b mrs_float/default [w] : Unchanged values are set to this value.
+
+*/
+
+
+class Upsample: public MarSystem
+{
+private:
+	void addControls();
+	
+public:
+	Upsample(std::string name);
+	~Upsample();
+	MarSystem* clone() const;    
+	void myUpdate(MarControlPtr sender);
+	void myProcess(realvec& in, realvec& out);
+};
+	
+}//namespace Marsyas
+
+#endif
+
+	
