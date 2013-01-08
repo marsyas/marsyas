b69f4af74966702087c91dcfe0c636c70a748326
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2013-01-08 22:19:40 +0000

    Better method of skipping over the @relation line for timeline regtest
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@5002 0e98fd9d-079e-457f-9c5c-d482923f975d

--- scripts/timeline-test-wrapper.py
+++ scripts/timeline-test-wrapper.py
@@ -28,18 +28,27 @@ def run_command(bin_cmd_args, working_dir, env_vars):
 def compare_arffs(one_filename, two_filename):
     one_lines = open(one_filename).readlines()
     two_lines = open(two_filename).readlines()
-    for a, b in zip(one_lines[:-10], two_lines[:-10]):
-        if a != b:
+    for a, b in zip(one_lines, two_lines):
+        if a.startswith("@relation"):
+            continue
+        if a.rstrip() != b.rstrip():
             return False
     return True
 
 if __name__ == "__main__":
-    #print '----'
-    #print sys.argv
+    print '----'
+    print sys.argv
     #new_filename = os.path.abspath(os.path.join(
     #    sys.argv[8], '../output',
     #    os.path.basename(sys.argv[6])))
-    new_filename = sys.argv[6]
+    new_filename = ""
+    for i, item in enumerate(sys.argv):
+        print item
+        if item == "-w":
+            new_filename = sys.argv[i+1]
+            break
+    if new_filename == "":
+        sys.exit(1)
     print "comparing ", new_filename, good_filename
     run_command(bin_cmd_args, working_dir, env_vars)
     # python truth values are opposite to return values!
