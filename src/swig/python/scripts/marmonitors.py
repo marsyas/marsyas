20ce7824ff623182787e9552c243e85b18c11176
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2013-01-22 00:08:28 +0000

    Install marmonitors.py, read a config file instead of hard-coding marcontrols
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@5052 0e98fd9d-079e-457f-9c5c-d482923f975d

--- src/swig/python/scripts/marmonitors.py
+++ src/swig/python/scripts/marmonitors.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 
+import optparse
+import math
 
 from pylab import *
 from marsyas_util import * 
 import sys
 from marsyas import MarSystemManager,MarControlPtr
+
+class SubPlotInfo:
+    def __init__(self, control="", x="", y="", title=""):
+        self.marcontrol = control
+        self.x_label = x
+        self.y_label = y
+        self.plot_title = title
+
+    def __str__(self):
+        return "Plot info for %s" % (self.marcontrol)
 
 msm = MarSystemManager()
 
@@ -18,83 +30,66 @@ def load_network(pluginName):
 
 
 # set the controls and plot the data 								
-def plot_figure(pluginName):
+def plot_figure(subplotinfo_list, pluginName):
     net = load_network(pluginName)
     print "Loading " + pluginName
 
     figure(figsize=(14,8))
 
+    rows = (len(subplotinfo_list) / 4) + 1   # yes, integer division
+    cols = math.ceil( len(subplotinfo_list) / rows )
+    subplots_adjust(hspace=0.40,wspace=0.20)
+
     for i in range(1,300):
         net.tick();
 
-        subplot(2,3,1);
-        subplots_adjust(hspace=0.40,wspace=0.20)
-        hold(False)
-        marplot(control2array(net, "Series/onset_strength/ShiftInput/si2/mrs_realvec/processedData"), 
-                x_label="Analysis Frames", 
-                y_label="Onset Strength", 
-                plot_title = "Onset Strength Signal")
-
-        subplot(2,3,2);
-        hold(False)
-        marplot(control2array(net, "Series/onset_strength/mrs_realvec/processedData"), 
-                x_label="Analysis Frames", 
-                y_label="Onset Strength", 
-                plot_title = "Filtered Onset Strength Signal")
-        
-
-        
-        subplot(2,3,3);
-        hold(False)
-        marplot(control2array(net, "FlowThru/tempoInduction/AutoCorrelation/acr/mrs_realvec/processedData"), 
-                x_label = "Lag (samples)", 
-                y_label = "Strength Strength", 
-                plot_title = "Autocorrelation output",
-                ex=200)
-
-        
-        subplot(2,3,4);
-        hold(False)
-        marplot(control2array(net, "FlowThru/tempoInduction/BeatHistogram/histo/mrs_realvec/processedData"), 
-                x_label = "Tempo (BPM)", 
-                y_label = "Beat Strength", 
-                plot_title = "BeatHistogram output",
-                ex=200)
-        
-        subplot(2,3,5);
-        hold(False)
-        marplot(control2array(net, "FlowThru/tempoInduction/Sum/hsum/mrs_realvec/processedData"), 
-                x_label = "Tempo (BPM)", 
-                y_label = "Beat Strength", 
-                plot_title = "Sum output",
-                ex=200)
-        hold(False)
-        subplot(2,3,6);
-        marplot(control2array(net, "FlowThru/tempoInduction/Peaker/pkr1/mrs_realvec/processedData"), 
-                x_label = "Tempo (BPM)", 
-                y_label = "Beat Strength", 
-                plot_title = "Peaker output",
-                ex=200)
-        hold(False)
-
-        
-        title = "Beat Histogram"
-        thismanager = get_current_fig_manager()
+        for pj, subplotinfo in enumerate(subplotinfo_list):
+            subplot(rows, cols, pj+1);
+            marplot(control2array(net, subplotinfo.marcontrol),
+                x_label=subplotinfo.x_label,
+                y_label=subplotinfo.y_label,
+                plot_title=subplotinfo.plot_title,
+                )
+            hold(False)
         ion() 
         show()
         raw_input("Press any key to tick the network")
-	
 
 
-        
+def read_config(configfile):
+    subplotinfo_list = []
+    lines = open(configfile).readlines()
+    for line in lines:
+        sl = line.split(',')
+        subplotinfo = SubPlotInfo(sl[0], sl[1], sl[2], sl[3])
+        subplotinfo_list.append(subplotinfo)
+    return subplotinfo_list
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option("-c", "--config", type="string",
+        default="", metavar="FILENAME",
+        help="Config file for controls to display"
+        )
+    parser.add_option("-p", "--plugin", type="string",
+        default="", metavar="FILENAME",
+        help="Plugin file to process"
+        )
+
+    (option, args) = parser.parse_args()
+    print option, args
+    if option.plugin == "":
+        print parser.format_help()
+        exit(1)
+    if option.config == "":
+        subplotinfo_list = [ SubPlotInfo("mrs_realvec/processedData",
+            "x", "y", "overall output") ]
+    else:
+        subplotinfo_list = read_config(option.config)
+    
+    plot_figure(subplotinfo_list, option.plugin)
 
 
-# call the plot function 
-plot_figure(sys.argv[1])
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
 
