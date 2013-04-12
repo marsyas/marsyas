92fae41f7f1248ca9401d5cdcb01eeece32e34ac
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2013-04-12 00:05:47 +0000

    more support for MARSYAS_DATADIR
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@5148 0e98fd9d-079e-457f-9c5c-d482923f975d

--- scripts/large-evaluators/check-marsyas-tempo.py
+++ scripts/large-evaluators/check-marsyas-tempo.py
@@ -27,8 +27,9 @@ def marsyas_tempo(queue, datum):
     queue.put(cmd)
 
 def main(mf_dir, output_dir):
+    dirname = os.environ['MARSYAS_DATADIR']
     collections = glob.glob(os.path.expanduser(
-        "~/src/audio-research/*_tempos.mf"))
+        os.path.join(dirname, "*_tempos.mf")))
     collections.sort()
     data = []
     for coll in collections:
