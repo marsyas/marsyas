e68c762f34d56f1263fa23788131542b92812e78
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2013-05-17 22:53:56 +0000

    improvements to tempo sound distortion script
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@5174 0e98fd9d-079e-457f-9c5c-d482923f975d

--- scripts/large-evaluators/make-tempo-distortions.py
+++ scripts/large-evaluators/make-tempo-distortions.py
@@ -3,6 +3,8 @@
 import sys
 import os.path
 
+import scipy.io.wavfile
+
 DEBUG = False
 #DEBUG = True
 
@@ -14,9 +16,9 @@ sox /tmp/1_gsm.gsm -r44100 -s /tmp/2_gsm-post.wav rate
 sox /tmp/2_gsm-post.wav /tmp/3_filtered.wav sinc 500-2000
 sox /tmp/3_filtered.wav /tmp/4_volboost.wav vol 1.8
 sox /tmp/4_volboost.wav /tmp/5_reverb.wav reverb
-sox /tmp/5_reverb.wav /tmp/noise.wav synth white
-sox /tmp/noise.wav /tmp/quietnoise.wav vol -20d
-sox -m /tmp/5_reverb.wav /tmp/quietnoise.wav \"%(outfile)s\"
+#sox /tmp/5_reverb.wav /tmp/noise.wav synth white
+#sox /tmp/noise.wav /tmp/quietnoise.wav vol -20d
+#sox -m /tmp/5_reverb.wav /tmp/quietnoise.wav \"%(outfile)s\"
 """),
 #"sox %(infile)s %(outfile)s reverb 1 2000 1000 700 750 760 880"),
 #    ("noise",
@@ -45,34 +47,72 @@ def get_audio_files_to_process(input_dir, output_dir):
                 matches.append( (input_filename, output_filename) )
     return matches
 
-def process_file(in_out_filename, cmd):
+def process_file(in_out_filename):
     infile, outfile = in_out_filename
 
-    cmd = cmd % locals()
+    cmd = DISTORTIONS % locals()
     if DEBUG:
         print cmd
     os.system(cmd)
 
+    fs, data = scipy.io.wavfile.read("/tmp/5_reverb.wav")
+    SNR = 20.0
+    Px = (data**2).sum()
+    noise = scipy.rand(len(data)) - 0.5
+    Pnoise = (noise**2).sum()
+    scaling = scipy.sqrt( (Px/Pnoise) * (10**(-SNR/10)))
+    noisy_to_add = noise*scaling
+    noisyX = data + scipy.array(noisy_to_add, dtype=scipy.int16)
+    #print Px, Pnoise, scaling
+    if False:
+    #if True:
+        import pylab
+        pylab.plot(data[:100000])
+        pylab.plot(noise[:100000])
+        pylab.plot(noisy_to_add[:100000])
+        pylab.show()
+    scipy.io.wavfile.write(outfile, fs, noisyX)
+    #print infile, outfile
+    #exit(1)
+
+def process_files(input_dir, output_dir, filenames):
+    total = len(filenames)
+    for count, in_out_filename in enumerate(filenames):
+        process_file(in_out_filename)
+        print "Finished %i / %i" % (count+1, total)
+        #if DEBUG:
+        #    print in_out_filename[1]
+
+def process_mfs(input_dir, output_dir):
+    ### get list of mfs
+    inmfs = []
+    for root, dirnames, filenames in os.walk(input_dir):
+        for filename in filenames:
+            if filename.endswith(".mf"):
+                mf_filename = os.path.join(root, filename)
+                inmfs.append( mf_filename )
+    ### process
+    reldirs = [os.path.relpath(x, input_dir) for x in inmfs]
+    outmfs = [os.path.join(output_dir, x) for x in reldirs]
+    for inmf, outmf in zip(inmfs, outmfs):
+        lines = open(inmf).readlines()
+        out = open(outmf, "w")
+        for line in lines:
+            infilename, bpm = line.split("\t")
+            relfilename = infilename.replace("MARSYAS_DATADIR/", "")
+            outfilename = os.path.join(output_dir, relfilename)
+            text = "%s\t%s" % (outfilename, bpm)
+            out.write(text)
+        out.close()
 
 def main(input_dir):
-    for dist in DISTORTIONS:
-        name, cmd = dist
-        print "-----", name
-        output_dir = os.path.join(
-            os.path.split(os.path.dirname(input_dir))[0],
-            name
-            )
-        make_dir_tree(input_dir, output_dir)
-        filenames = get_audio_files_to_process(input_dir, output_dir)
-        if DEBUG:
-            filenames = filenames[:1]
-
-        total = len(filenames)
-        for count, in_out_filename in enumerate(filenames):
-            process_file(in_out_filename, cmd)
-            print "Finished %i / %i" % (count+1, total)
-            #if DEBUG:
-            #    print in_out_filename[1]
+    output_dir = os.path.join(
+        os.path.split(os.path.dirname(input_dir))[0],
+        "distorted")
+    #make_dir_tree(input_dir, output_dir)
+    #filenames = get_audio_files_to_process(input_dir, output_dir)
+    #process_files(input_dir, output_dir, filenames)
+    process_mfs(input_dir, output_dir)
 
 
 if __name__ == '__main__':
