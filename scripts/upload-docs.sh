adf5c42649880bd4f453ee8af8cca9f77e15dae1
gperciva <gperciva@0e98fd9d-079e-457f-9c5c-d482923f975d>
2013-06-20 21:44:26 +0000

    update marsyas.sness.net -> marsyas.info
    git-svn-id: svn+ssh://svn.code.sf.net/p/marsyas/code/trunk@5224 0e98fd9d-079e-457f-9c5c-d482923f975d

--- scripts/upload-docs.sh
+++ scripts/upload-docs.sh
@@ -4,8 +4,8 @@ marsyasDir=~/src/marsyas/
 
 cd $marsyasDir/build-doc/
 tar -czf marsyas-docs.tar.gz out-www/
-scp marsyas-docs.tar.gz marsyas@marsyas.sness.net:
-ssh -l marsyas marsyas.sness.net "rm -rf out-www/ ; tar -xf marsyas-docs.tar.gz ; mv marsyas-docs.tar.gz out-www/"
+scp marsyas-docs.tar.gz marsyas@marsyas.info:
+ssh -l marsyas marsyas.info "rm -rf out-www/ ; tar -xf marsyas-docs.tar.gz ; mv marsyas-docs.tar.gz out-www/"
 
 rm marsyas-docs.tar.gz
 
