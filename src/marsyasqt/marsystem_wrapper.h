6abbbf97c2442b08783e4e7961cd498a0cbf3002
Jakob Leben <jakob.leben@gmail.com>
2013-07-23 01:11:51 -0700

    marsyasqt: marsystem wrapper: fix indentation

--- src/marsyasqt/marsystem_wrapper.h
+++ src/marsyasqt/marsystem_wrapper.h
@@ -38,30 +38,30 @@ using Marsyas::RealTime::bad_any_cast;
 template<typename T>
 QVariant variant_from_any( const any & value )
 {
-	try {
-		const T & typed_value = any_cast<T>(value);
-		return QVariant::fromValue(typed_value);
-	} catch (bad_any_cast) {
-		return QVariant();
-	}
+  try {
+    const T & typed_value = any_cast<T>(value);
+    return QVariant::fromValue(typed_value);
+  } catch (bad_any_cast) {
+    return QVariant();
+  }
 }
 
 template<typename T>
 any any_from_variant( const QVariant & value )
 {
-	T typed_value = value.value<T>();
-	return any(typed_value);
+  T typed_value = value.value<T>();
+  return any(typed_value);
 }
 
 template<typename T>
 T safe_any_cast( const any & value )
 {
-	try {
-		const T & typed_value = any_cast<T>(value);
-		return typed_value;
-	} catch (bad_any_cast) {
-		return T();
-	}
+  try {
+    const T & typed_value = any_cast<T>(value);
+    return typed_value;
+  } catch (bad_any_cast) {
+    return T();
+  }
 }
 
 ////////////////////////////////
@@ -74,47 +74,47 @@ class Control;
  */
 class System : public QObject
 {
-	Q_OBJECT
+  Q_OBJECT
 
 public:
 
 
 public:
-	System( Marsyas::MarSystem * system, QObject * parent = 0 ):
-	QObject(parent),
-	m_runner(system)
-	{}
+  System( Marsyas::MarSystem * system, QObject * parent = 0 ):
+    QObject(parent),
+    m_runner(system)
+  {}
 
-	void start()
-	{
-		m_runner.start();
-	}
+  void start()
+  {
+    m_runner.start();
+  }
 
-	void stop()
-	{
-		m_runner.stop();
-	}
+  void stop()
+  {
+    m_runner.stop();
+  }
 
-	bool isRunning()
-	{
-		return m_runner.isRunning();
-	}
+  bool isRunning()
+  {
+    return m_runner.isRunning();
+  }
 
 #if 0
-	QVariant controlValue( const QString & path )
-	{
-		any value = thread.controlValue( path.toStdString() );
-		QVariant variant =
-	}
+  QVariant controlValue( const QString & path )
+  {
+    any value = thread.controlValue( path.toStdString() );
+    QVariant variant =
+  }
 
-	void setControlValue( const QString & path, const QVariant & variant )
-	{
-		any value =
-		thread.setControlValue( path.toStdString(), value );
-	}
+  void setControlValue( const QString & path, const QVariant & variant )
+  {
+    any value =
+        thread.setControlValue( path.toStdString(), value );
+  }
 #endif
 
-	Control * control( const QString & path );
+  Control * control( const QString & path );
 
   void update()
   {
@@ -122,160 +122,160 @@ public:
   }
 
 private slots:
-	void removeControl( QObject * object );
+  void removeControl( QObject * object );
 
 private:
-	friend class Control;
+  friend class Control;
 
-	Control * createControl( const QString & path );
+  Control * createControl( const QString & path );
 
-	Marsyas::RealTime::Runner m_runner;
-	QMap<QString, Control*> m_controls;
+  Marsyas::RealTime::Runner m_runner;
+  QMap<QString, Control*> m_controls;
 };
 
 ///////////////////////////////
 
 class Control : public QObject
 {
-	Q_OBJECT
+  Q_OBJECT
 public:
-	virtual QVariant value()
-	{
-		return variant_from_any( m_thread_control->value() );
-	}
+  virtual QVariant value()
+  {
+    return variant_from_any( m_thread_control->value() );
+  }
 
-	virtual void setValue( const QVariant & value, bool update = true )
-	{
-		m_thread_control->setValue( any_from_variant(value), update );
-	}
+  virtual void setValue( const QVariant & value, bool update = true )
+  {
+    m_thread_control->setValue( any_from_variant(value), update );
+  }
 
-	QString path() const { return QString::fromStdString( m_thread_control->path() ); }
+  QString path() const { return QString::fromStdString( m_thread_control->path() ); }
 
 protected:
-	friend class System;
+  friend class System;
 
-	Control( Marsyas::RealTime::Control *thread_control, System * parent ):
-	QObject(parent),
-	m_thread_control(thread_control)
-	{}
+  Control( Marsyas::RealTime::Control *thread_control, System * parent ):
+    QObject(parent),
+    m_thread_control(thread_control)
+  {}
 
-	virtual ~Control() {}
+  virtual ~Control() {}
 
-	virtual QVariant variant_from_any( const any & value ) = 0;
-	virtual any any_from_variant( const QVariant & value ) = 0;
+  virtual QVariant variant_from_any( const any & value ) = 0;
+  virtual any any_from_variant( const QVariant & value ) = 0;
 
 private:
-	Marsyas::RealTime::Control *m_thread_control;
+  Marsyas::RealTime::Control *m_thread_control;
 };
 
 class ControlBool : public Control
 {
-	Q_OBJECT
+  Q_OBJECT
 public slots:
-	void setValue(bool value)
-	{
-		Control::setValue( QVariant(value) );
-	}
+  void setValue(bool value)
+  {
+    Control::setValue( QVariant(value) );
+  }
 signals:
-	void valueChanged(bool value);
+  void valueChanged(bool value);
 
 protected:
-	friend class System;
+  friend class System;
 
-	ControlBool( Marsyas::RealTime::Control *thread_control, System * parent ):
-		Control(thread_control, parent)
-	{}
+  ControlBool( Marsyas::RealTime::Control *thread_control, System * parent ):
+    Control(thread_control, parent)
+  {}
 
-	QVariant variant_from_any( const any & value )
-	{
-		return MarsyasQt::variant_from_any<bool>(value);
-	}
-	any any_from_variant( const QVariant & value )
-	{
-		return MarsyasQt::any_from_variant<bool>(value);
-	}
+  QVariant variant_from_any( const any & value )
+  {
+    return MarsyasQt::variant_from_any<bool>(value);
+  }
+  any any_from_variant( const QVariant & value )
+  {
+    return MarsyasQt::any_from_variant<bool>(value);
+  }
 };
 
 class ControlNatural : public Control
 {
-	Q_OBJECT
+  Q_OBJECT
 public slots:
-	void setValue(int value)
-	{
-		Control::setValue( QVariant(value) );
-	}
+  void setValue(int value)
+  {
+    Control::setValue( QVariant(value) );
+  }
 signals:
-	void valueChanged(int value);
+  void valueChanged(int value);
 
 protected:
-	friend class System;
+  friend class System;
 
-	ControlNatural( Marsyas::RealTime::Control *thread_control, System * parent ):
-		Control(thread_control, parent)
-	{}
-	QVariant variant_from_any( const any & value )
-	{
-		return MarsyasQt::variant_from_any<mrs_natural>(value);
-	}
-	any any_from_variant( const QVariant & value )
-	{
-		return MarsyasQt::any_from_variant<mrs_natural>(value);
-	}
+  ControlNatural( Marsyas::RealTime::Control *thread_control, System * parent ):
+    Control(thread_control, parent)
+  {}
+  QVariant variant_from_any( const any & value )
+  {
+    return MarsyasQt::variant_from_any<mrs_natural>(value);
+  }
+  any any_from_variant( const QVariant & value )
+  {
+    return MarsyasQt::any_from_variant<mrs_natural>(value);
+  }
 };
 
 class ControlReal : public Control
 {
-	Q_OBJECT
+  Q_OBJECT
 public slots:
-	void setValue(double value)
-	{
-		Control::setValue( QVariant(value) );
-	}
+  void setValue(double value)
+  {
+    Control::setValue( QVariant(value) );
+  }
 signals:
-	void valueChanged(double value);
+  void valueChanged(double value);
 
 protected:
-	friend class System;
+  friend class System;
 
-	ControlReal( Marsyas::RealTime::Control *thread_control, System * parent ):
-		Control(thread_control, parent)
-	{}
-	QVariant variant_from_any( const any & value )
-	{
-		return MarsyasQt::variant_from_any<mrs_real>(value);
-	}
-	any any_from_variant( const QVariant & value )
-	{
-		return MarsyasQt::any_from_variant<mrs_real>(value);
-	}
+  ControlReal( Marsyas::RealTime::Control *thread_control, System * parent ):
+    Control(thread_control, parent)
+  {}
+  QVariant variant_from_any( const any & value )
+  {
+    return MarsyasQt::variant_from_any<mrs_real>(value);
+  }
+  any any_from_variant( const QVariant & value )
+  {
+    return MarsyasQt::any_from_variant<mrs_real>(value);
+  }
 };
 
 class ControlString : public Control
 {
-	Q_OBJECT
+  Q_OBJECT
 public slots:
-	void setValue(const QString & value)
-	{
-		Control::setValue( QVariant(value) );
-	}
+  void setValue(const QString & value)
+  {
+    Control::setValue( QVariant(value) );
+  }
 signals:
-	void valueChanged(const QString & value);
+  void valueChanged(const QString & value);
 
 protected:
-	friend class System;
+  friend class System;
 
-	ControlString( Marsyas::RealTime::Control *thread_control, System * parent ):
-		Control(thread_control, parent)
-	{}
-	QVariant variant_from_any( const any & value )
-	{
-		std::string string_value = safe_any_cast<mrs_string>(value);
-		return QVariant( QString::fromStdString(string_value) );
-	}
-	any any_from_variant( const QVariant & value )
-	{
-		return any( value.toString().toStdString() );
-	}
+  ControlString( Marsyas::RealTime::Control *thread_control, System * parent ):
+    Control(thread_control, parent)
+  {}
+  QVariant variant_from_any( const any & value )
+  {
+    std::string string_value = safe_any_cast<mrs_string>(value);
+    return QVariant( QString::fromStdString(string_value) );
+  }
+  any any_from_variant( const QVariant & value )
+  {
+    return any( value.toString().toStdString() );
+  }
 };
 
 } // namespace MarsyasQt
