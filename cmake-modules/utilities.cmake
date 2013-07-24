0d42abd26406c8de177d4d42760ea09674971bc1
Jakob Leben <jakob.leben@gmail.com>
2013-07-24 15:23:54 -0700

    cmake: remove utility for target-specific C++11 enablement

--- cmake-modules/utilities.cmake
+++
@@ -1,8 +0,0 @@
-
-macro(target_enable_cpp11 TARGET_NAME)
-#  if(CMAKE_COMPILER_IS_GNUCXX)
-#    set_property(TARGET ${TARGET_NAME} APPEND PROPERTY COMPILE_FLAGS "-std=c++11")
-#  elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL Clang)
-#    set_property(TARGET ${TARGET_NAME} APPEND PROPERTY COMPILE_FLAGS "-std=c++11 -stdlib=libc++")
-#  endif()
-endmacro()
