a04cb83e15b55001c4a2fd7775516147dcb28673
Graham Percival <graham@percival-music.ca>
2013-07-27 13:32:45 -0700

    tempo: fix final off-by-one in octave.

--- scripts/MATLAB/tempo/info_histogram.m
+++ scripts/MATLAB/tempo/info_histogram.m
@@ -24,6 +24,13 @@ for i = 2:len-1
 	end
 end
 
+if bpm2 > 0
+	bpm2 = bpm2-1
+end
+if bpm3 > 0
+	bpm3 = bpm3-1
+end
+
 num_non_zero = 0;
 for i = 1:len
 	if histo(i) > 0
@@ -57,8 +64,8 @@ info = [
 	str10,
 	str20,
 	1.0 - (str05+str10+str20),
-	double(bpm2-1) / bpm_cand,
-	double(bpm3-1) / bpm_cand,
+	double(bpm2) / bpm_cand,
+	double(bpm3) / bpm_cand,
 	double(num_non_zero),
 	bpm_cand
 	]';
