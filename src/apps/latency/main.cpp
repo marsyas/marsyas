8f391deb80a55c0fb0585965ab144cb83c3b9b78
Jakob Leben <jakob.leben@gmail.com>
2013-07-29 19:49:26 -0700

    apps: latency: add useful command-line options

--- src/apps/latency/main.cpp
+++ src/apps/latency/main.cpp
@@ -1,25 +1,35 @@
+#include <marsyas/CommandLineOptions.h>
 #include <marsyas/core/MarSystemManager.h>
 #include <marsyas/realtime/runner.h>
 #include <cstdlib>
 #include <iostream>
+#include <algorithm>
 
 using namespace Marsyas;
 using namespace Marsyas::RealTime;
 using namespace std;
 
-int main( int argc, char *argv[] )
+int main( int argc, const char **argv )
 {
   mrs_real delay_sec = 0.0;
 
-  if (argc > 1)
-  {
-    delay_sec = std::atof(argv[1]);
-    cout << "Using delay = " << delay_sec << " seconds" << endl;
-  }
-  else
-  {
-    cout << "Using default delay = " << delay_sec << " seconds" << endl;
-  }
+  CommandLineOptions opt;
+  opt.addBoolOption("real-time", "r", false);
+  opt.addRealOption("sample-rate", "s", 44100.0);
+  opt.addNaturalOption("block-size", "b", 128);
+  opt.addRealOption("delay", "d", 0.0);
+
+  opt.readOptions(argc, argv);
+
+  mrs_real delay = std::max((mrs_real)0.0, opt.getRealOption("delay"));
+  mrs_real sr = opt.getRealOption("sample-rate");
+  mrs_natural block = std::max((mrs_natural)1, opt.getNaturalOption("block-size"));
+  bool realtime = opt.getBoolOption("real-time");
+
+  cout << "realtime mode = " << (realtime ? "on" : "off") << endl;
+  cout << "sample rate = " << sr << " Hz" << endl;
+  cout << "block size = " << block << " frames" << endl;
+  cout << "delay = " << delay << " seconds" << endl;
 
   MarSystemManager mng;
 
@@ -28,12 +38,12 @@ int main( int argc, char *argv[] )
   system->addMarSystem( mng.create("Delay", "delay") );
   system->addMarSystem( mng.create("AudioSink", "out") );
 
-  system->setControl("mrs_real/israte", 44100.0);
-  system->setControl("mrs_natural/inSamples", 128);
-  system->setControl("Delay/delay/mrs_real/maxDelaySeconds", delay_sec);
-  system->setControl("Delay/delay/mrs_real/delaySeconds", delay_sec);
-  system->setControl("AudioSource/in/mrs_bool/realtime", true);
-  system->setControl("AudioSink/out/mrs_bool/realtime", true);
+  system->setControl("mrs_real/israte", sr);
+  system->setControl("mrs_natural/inSamples", block);
+  system->setControl("Delay/delay/mrs_real/maxDelaySeconds", delay);
+  system->setControl("Delay/delay/mrs_real/delaySeconds", delay);
+  system->setControl("AudioSource/in/mrs_bool/realtime", realtime);
+  system->setControl("AudioSink/out/mrs_bool/realtime", realtime);
 
   system->setControl("AudioSource/in/mrs_bool/initAudio", true);
   system->setControl("AudioSink/out/mrs_bool/initAudio", true);
