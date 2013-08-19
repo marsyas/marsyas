b228911f88461646debce54db754f019128c95b6
Jakob Leben <jakob.leben@gmail.com>
2013-08-19 13:05:10 -0700

    apps: debugger: refactor, use options, improve report

--- src/apps/debugger/debugger.cpp
+++ src/apps/debugger/debugger.cpp
@@ -16,12 +16,14 @@
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-#include <MarSystemManager.h>
+#include <marsyas/CommandLineOptions.h>
+#include <marsyas/core/MarSystemManager.h>
 #include <marsyas/debug/recorder.h>
 #include <marsyas/debug/file_io.h>
 #include <marsyas/debug/debugger.h>
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <fstream>
 
@@ -31,161 +33,199 @@ using namespace Marsyas;
 int perform_record( MarSystem * system, const std::string & filename, int tick_count );
 int perform_compare( MarSystem * system, const std::string & filename );
 
+void report
+( const Debug::Record & record, const Debug::BugReport & bugs, int tick_count );
+void report_bugs
+( const Debug::Record & record, const Debug::BugReport & bugs, int tick_count );
+void print_header(int tick_count);
+void print_entry
+(const std::string & path, const Debug::Record::Entry & record, const Debug::Bug & bug);
+
 void printUsage()
 {
-  cout << "Usage: marsyas-debug [record|compare] <plugin-file> <recording-file> <tick-count>" << endl;
+  cout << "Usage: marsyas-debug <system-file> [-r <recording-file>] [-c <reference-file>] [-t <tick-count>] [-b]" << endl;
 }
 
-int main (int argc, char *argv[])
+int main (int argc, const char *argv[])
 {
-  if (argc < 4) {
-    printUsage();
-    return 1;
-  }
+  CommandLineOptions opt;
+  opt.addStringOption("record", "r", "");
+  opt.addStringOption("compare", "c", "");
+  opt.addNaturalOption("ticks", "t", -1);
+  opt.addBoolOption("bugs-only", "b", false);
+  opt.readOptions(argc, argv);
 
-  string command( argv[1] );
-  if (command != "record" && command != "compare")
+  const std::vector<std::string> & arguments = opt.getRemaining();
+  if (!arguments.size())
   {
-    cerr << "Unrecognized command:" << command << endl;
+    cout << "Missing system file." << endl;
     printUsage();
     return 1;
   }
 
-  string plugin_filename( argv[2] );
-  string record_filename( argv[3] );
+  std::string system_filename = arguments[0];
+  std::string record_filename = opt.getStringOption("record");
+  std::string compare_filename = opt.getStringOption("compare");
 
-  cout << "Using plugin file: " << plugin_filename << endl;
-  cout << "Using recording file: " << record_filename << endl;
+  cout << "Using system file: " << system_filename << endl;
+  if (!record_filename.empty())
+    cout << "Recording to file: " << record_filename << endl;
+  if (!compare_filename.empty())
+    cout << "Comparing with file: " << compare_filename << endl;
 
   MarSystemManager mng;
-  ifstream plugin_stream( plugin_filename );
-  MarSystem *system = mng.getMarSystem(plugin_stream);
+  ifstream system_file_stream( system_filename );
+  MarSystem *system = mng.getMarSystem(system_file_stream);
   if (!system) {
     cout << "Failed to load plugin file." << endl;
     return 1;
   }
 
-  int result = 0;
+  Debug::Recorder recorder(system);
+  Debug::FileReader *reader = 0;
+  Debug::FileWriter *writer = 0;
 
-  if (command == "record")
+  if (!compare_filename.empty())
+    reader = new Debug::FileReader(compare_filename);
+  if (!record_filename.empty())
+    writer = new Debug::FileWriter(record_filename, system);
+
+  int ticks_remaining = (int) opt.getNaturalOption("ticks");
+  if (ticks_remaining < 1)
   {
-    int tick_count;
-    if (argc < 5)
-      tick_count = 1;
+    if (reader)
+      ticks_remaining = -1;
     else
-      tick_count = atoi(argv[4]);
-    if (tick_count < 1)
-    {
-      cerr << "Command 'record' requires 'tick-count' of 1 or greater." << endl;
-      return 1;
-    }
-    result = perform_record(system, record_filename, tick_count);
-  }
-  else if (command == "compare")
-  {
-    result = perform_compare(system, record_filename);
+      ticks_remaining = 1;
   }
 
-  return result;
-}
+  bool bugs_only = opt.getBoolOption("bugs-only");
 
-int perform_record( MarSystem * system, const std::string & filename, int tick_count )
-{
-  Debug::Recorder recorder(system);
-  Debug::FileWriter writer(filename, system);
-
-  while(tick_count--)
-  {
-    recorder.clear();
-    system->tick();
-    recorder.commit();
-    writer.write( recorder.record() );
-  }
-
-  cout << "Done recording." << endl;
-  return 0;
-}
-
-int perform_compare( MarSystem * system, const std::string & filename )
-{
-  Debug::FileReader reader(filename);
-  Debug::Recorder recorder(system);
-
-#if 0
-  cout << "Comparing " << rec->records.size() << " records..." << endl;
-  cout << "Comparing paths:" << endl;
-  for ( const auto & path : rec->paths )
-  {
-    cout << path << endl;
-  }
-
-    recorder online_recorder(system);
-  cout << "System paths:" << endl;
-  for ( const auto & path : online_recorder.paths() )
-    cout << path << endl;
-
-
-  if (online_recorder.paths() != rec->paths)
-  {
-    cerr << "System and recording paths do not match!" << endl;
-    return 1;
-  }
-
-  debugger debug(system, rec);
-#endif
-  bool ok = true;
+  int result = 0;
   int tick_count = 0;
 
-  Debug::Record reference_record;
-
-  while (reader.read(reference_record))
+  while(ticks_remaining)
   {
+    Debug::Record reference_record;
+    bool have_reference = reader && reader->read(reference_record);
+    if (!have_reference && ticks_remaining < 0)
+      break;
+
     system->tick();
     ++tick_count;
 
     recorder.commit();
     const Debug::Record & actual_record = recorder.record();
 
-    Debug::BugReport report;
-    Debug::compare(actual_record, reference_record, report);
+    Debug::BugReport bugs;
+    if (have_reference)
+      Debug::compare(actual_record, reference_record, bugs);
 
-    for (const auto & report_entry : report)
-    {
-      ok = false;
-      const std::string & path = report_entry.first;
-      const Debug::Bug & bug = report_entry.second;
+    if (!bugs.empty())
+      result = 1;
 
-      switch (bug.flags)
-      {
-      case Debug::path_missing:
-        cout << tick_count << " @ " << path << endl
-             << "\t" << "FAILURE: system has no control for path."
-             << endl;
-        break;
-      case Debug::format_mismatch:
-        cout << tick_count << " @ " << path << endl
-             << "\t" << "FAILURE: format mismatch: "
-             //<< rec_data.getCols() << "x" << rec_data.getRows() << " != "
-             //<< sys_data.getCols() << "x" << sys_data.getCols()
-             << endl;
-        break;
-      case Debug::value_mismatch:
-        cout << tick_count << " @ " << path << endl
-             << "\t" << "MISMATCH:"
-             << " average deviation = " << bug.average_deviation
-             << ", maximum deviation = " << bug.max_deviation
-             << endl;
-      default:
-        break;
-      }
-    }
+    if (bugs_only)
+      report_bugs(actual_record, bugs, tick_count);
+    else
+      report(actual_record, bugs, tick_count);
 
-    reference_record.clear();
+    if (writer)
+      writer->write( actual_record );
+
     recorder.clear();
+
+    if (ticks_remaining > 0)
+      --ticks_remaining;
   }
 
-  if (ok)
-    cout << "OK." << endl;
+  return result;
+}
 
-  return ok ? 0 : 1;
+void report( const Debug::Record & record,
+             const Debug::BugReport & bugs,
+             int tick_count )
+{
+  print_header(tick_count);
+
+  for (const auto & record_entry : record.entries())
+  {
+    const std::string & path = record_entry.first;
+    const Debug::Record::Entry & entry = record_entry.second;
+    Debug::Bug bug;
+
+    try
+    {
+      bug = bugs.at(path);
+    }
+    catch (const std::out_of_range & e)
+    {}
+
+    print_entry(path, entry, bug);
+  }
+}
+
+void report_bugs( const Debug::Record & record,
+                  const Debug::BugReport & bugs,
+                  int tick_count )
+{
+  if (bugs.empty())
+    return;
+
+  print_header(tick_count);
+
+  for (const auto & bug_entry : bugs)
+  {
+    const std::string & path = bug_entry.first;
+    const Debug::Bug & bug = bug_entry.second;
+    Debug::Record::Entry record_entry;
+
+    try
+    {
+      record_entry = record.entries().at(path);
+    }
+    catch (const std::out_of_range & e)
+    {}
+
+    print_entry(path, record_entry, bug);
+  }
+}
+
+static const unsigned int field_width = 13;
+static const unsigned int tick_field_width = 6;
+
+void print_header(int tick_count)
+{
+  std::ostringstream tick_string;
+  tick_string << "# " << tick_count;
+
+  std::string head_separator((tick_field_width+3)
+                             + (field_width+3)*3
+                             + field_width,
+                             '-');
+
+  cout << head_separator << endl;
+
+  cout << std::left
+       << setw(tick_field_width) << tick_string.str() << " | "
+       << setw(field_width) << "Real Time" << " | "
+       << setw(field_width) << "CPU Time" << " | "
+       << setw(field_width) << "Max Dev" << " | "
+       << setw(field_width) << "Avg Dev"
+       << endl;
+
+  cout << head_separator << endl;
+}
+
+void print_entry(const std::string & path,
+                 const Debug::Record::Entry & record,
+                 const Debug::Bug & bug)
+{
+  cout << path << endl;
+  cout << std::left
+       << setw(tick_field_width) << "" << " | "
+       << setw(field_width) << record.real_time << " | "
+       << setw(field_width) << record.cpu_time << " | "
+       << setw(field_width) << bug.max_deviation << " | "
+       << setw(field_width) << bug.average_deviation
+       << endl;
 }
