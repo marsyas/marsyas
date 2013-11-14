abb9ef37113a4bb73eca5692ae4d31d915dc1376
Jakob Leben <jleben@mess.cs.uvic.ca>
2013-11-14 11:32:01 -0800

    improve CTest script
    * Make passing configuration options more flexible
    * Fix CMake invocation

--- cmake-modules/test.cmake
+++ cmake-modules/test.cmake
@@ -8,15 +8,17 @@
 
 include(${CTEST_SCRIPT_DIRECTORY}/utilities.cmake)
 
-if(MARSYAS_CONFIGURATION STREQUAL "Default")
-  include(${CTEST_SCRIPT_DIRECTORY}/test_config_default.cmake)
-elseif(MARSYAS_CONFIGURATION STREQUAL "Complete")
-  include(${CTEST_SCRIPT_DIRECTORY}/test_config_complete.cmake)
-elseif(MARSYAS_CONFIGURATION STREQUAL "Minimal")
-  include(${CTEST_SCRIPT_DIRECTORY}/test_config_minimal.cmake)
-else()
-  message(FATAL_ERROR "Unknown Marsyas configuration: '${MARSYAS_CONFIGURATION}'")
+if(NOT MARSYAS_CONFIGURATION)
+  message(FATAL_ERROR "Missing MARSYAS_CONFIGURATION option.")
 endif()
+
+set(CONFIGURATION_FILE "${CTEST_SCRIPT_DIRECTORY}/test_config_${MARSYAS_CONFIGURATION}.cmake")
+
+if(NOT EXISTS ${CONFIGURATION_FILE})
+  message(FATAL_ERROR "File for configuration '${MARSYAS_CONFIGURATION}' does not exist: ${CONFIGURATION_FILE}")
+endif()
+
+include(${CONFIGURATION_FILE})
 
 #set(WITH_MEMCHECK TRUE)
 #set(WITH_COVERAGE TRUE)
@@ -43,16 +45,14 @@ endif()
 # The following command must be just the git executable, without arguments:
 set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
 
-list_to_space_separated_string(CTEST_BUILD_OPTIONS_STRING "${CTEST_BUILD_OPTIONS}")
-
-set(CTEST_CONFIGURE_COMMAND "${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=${CTEST_BUILD_CONFIGURATION}")
-set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} -DMARSYAS_TESTS=ON ${CTEST_BUILD_OPTIONS_STRING}")
-set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"-G${CTEST_CMAKE_GENERATOR}\"")
-set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"${CTEST_SOURCE_DIRECTORY}\"")
+list(APPEND CTEST_BUILD_OPTIONS
+  -DCMAKE_BUILD_TYPE=${CTEST_BUILD_CONFIGURATION}
+  -DMARSYAS_TESTS=ON
+)
 
 ctest_start(Experimental)
 ctest_update()
-ctest_configure()
+ctest_configure(OPTIONS "${CTEST_BUILD_OPTIONS}")
 ctest_build()
 ctest_test()
 if (WITH_COVERAGE AND CTEST_COVERAGE_COMMAND)
