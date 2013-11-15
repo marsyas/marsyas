f1e8edde6b71cc9aeaae1061eb18d3a496977a65
Jakob Leben <jakob.leben@gmail.com>
2013-11-14 19:58:46 -0800

    ctest: update nightly testing script

--- cmake-modules/test.cmake
+++ cmake-modules/test.cmake
@@ -1,12 +1,22 @@
-# This script expects the following variables to be set beforehand:
+# This script should be run with the following variable set:
 #
-# CTEST_CMAKE_GENERATOR
-# CTEST_SOURCE_DIRECTORY
-# CTEST_BINARY_DIRECTORY
-# CTEST_SITE
-# MARSYAS_CONFIGURATION
+# MARSYAS_CONFIGURATION - The name of the current build configuration.
+#
+# The above variable will make part of the build name reported on CDash.
+# Moreover, it will make this script include another "configuration" script
+# with filename "test_config_${MARSYAS_CONFIGURATION}.cmake".
+#
+# The configuration script should set the following variables:
+#
+# CTEST_SITE - The name of the build site (machine)
+# CTEST_SOURCE_DIRECTORY - Marsyas source dir
+# CTEST_BINARY_DIRECTORY - Desired build dir
+# CTEST_CMAKE_GENERATOR - CMake generator to use (e.g. "Unix Makefiles", "Visual Studio 12 Win64")
+# CTEST_BUILD_CONFIGURATION (optional) - CMake configuration (e.g. "Debug")
+# CTEST_BUILD_OPTIONS (optional) - CMake options (e.g. "-DMARSYAS_AUDIOIO=OFF")
+# MARSYAS_BRANCH (optional) - CMake branch to build (not yet working, always builds current branch)
 
-include(${CTEST_SCRIPT_DIRECTORY}/utilities.cmake)
+#include(${CTEST_SCRIPT_DIRECTORY}/utilities.cmake)
 
 if(NOT MARSYAS_CONFIGURATION)
   message(FATAL_ERROR "Missing MARSYAS_CONFIGURATION option.")
@@ -20,10 +30,17 @@ endif()
 
 include(${CONFIGURATION_FILE})
 
+if(NOT DEFINED MARSYAS_BRANCH)
+	set(MARSYAS_BRANCH "master")
+	message(STATUS "Maryas branch defaults to '${MARSYAS_BRANCH}'")
+endif()
+
 #set(WITH_MEMCHECK TRUE)
 #set(WITH_COVERAGE TRUE)
 
 #######################################################################
+
+set(CTEST_BUILD_NAME "${MARSYAS_BRANCH}-${MARSYAS_CONFIGURATION}")
 
 ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 
@@ -44,6 +61,7 @@ endif()
 
 # The following command must be just the git executable, without arguments:
 set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
+#set(CTEST_UPDATE_OPTIONS "origin ${MARSYAS_BRANCH}")
 
 list(APPEND CTEST_BUILD_OPTIONS
   -DCMAKE_BUILD_TYPE=${CTEST_BUILD_CONFIGURATION}
