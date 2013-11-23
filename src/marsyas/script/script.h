5d0f25d98db2f87b598fe5d5f2b33dbf827df724
Jakob Leben <jakob.leben@gmail.com>
2013-11-22 16:05:46 -0800

    scripting: export Marsyas::system_from_script()

--- src/marsyas/script/script.h
+++ src/marsyas/script/script.h
@@ -2,10 +2,12 @@
 #define MARSYAS_SCRIPT_INCLUDED
 
 #include <marsyas/system/MarSystem.h>
+#include <marsyas/export.h>
 #include <istream>
 
 namespace Marsyas {
 
+marsyas_EXPORT
 MarSystem *system_from_script(std::istream & script_stream);
 
 } // namespace Marsyas
