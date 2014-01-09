419aabd42f52323636650bb70ce891e7808d259c
Jakob Leben <jakob.leben@gmail.com>
2014-01-09 08:35:13 -0800

    fix export.h for MinGW

--- src/marsyas/export.h
+++ src/marsyas/export.h
@@ -1,21 +1,22 @@
 #ifndef _marsyas_DLLDEFINES_H
 #define _marsyas_DLLDEFINES_H
 
-#if defined (_WIN32)
-#include "config.h"
-#if defined MARSYAS_STATIC
-#define marsyas_EXPORT
-#else
-#if defined (marsyas_EXPORTS)
+#if defined (_WIN32) && !defined(MARSYAS_STATIC)
+
+#ifdef _MSC_VER
 #pragma warning(disable: 4251)
+#endif
+
+#if defined (marsyas_EXPORTS)
 #define marsyas_EXPORT __declspec(dllexport)
 #else
-#pragma warning(disable: 4251)
 #define marsyas_EXPORT __declspec(dllimport)
 #endif
-#endif
+
 #else
+
 #define marsyas_EXPORT
+
 #endif
 
 #endif
