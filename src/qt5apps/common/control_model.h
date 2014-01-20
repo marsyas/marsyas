a25224c3f99b6f62806a57c65fed169627955b7a
Jakob Leben <jakob.leben@gmail.com>
2014-01-20 14:00:59 -0800

    inspector: basic control editing support (no realvecs yet)

--- src/qt5apps/common/control_model.h
+++ src/qt5apps/common/control_model.h
@@ -40,7 +40,9 @@ public:
   int columnCount(const QModelIndex & parent = QModelIndex()) const;
   QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const;
   QModelIndex parent(const QModelIndex & index) const;
+  Qt::ItemFlags flags(const QModelIndex & index) const;
   QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+  bool setData(const QModelIndex & index, const QVariant & value, int role = Qt::EditRole);
   QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
 
 private:
