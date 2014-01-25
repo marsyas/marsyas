5011f3fcb263affbd70f02a24c6ff939956d77c8
George Tzanetakis <gtzan@cs.uvic.ca>
2014-01-25 07:15:39 +0100

    Minor tweaks to python files

--- src/marsyas_python/windowing.py
+++ src/marsyas_python/windowing.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-# This source code will serve as good tutorial on how to program a more complex MarSystem network using a compact syntax.
-# This syntax will be interpreted by the functions in marsyas_util.py (which should be imported, as well).
-# This program should be the next step in learning after helloworld.py.
-# Some more advanced controls will be shown, and how to specify networks in a more compact way (using marsyas_util)
-# as well.
+# An example of the effect of windowing on spectrum 
+# of two input sine waves. marsyas_util.py contain 
+# helper functions for the compact syntax 
+# for specifying Marsyas networks. This 
+# example also illustrate how Marsyas and matplotlib 
+# can interact 
 
 from pylab import *
 import sys
@@ -12,80 +13,99 @@ from matplotlib import pyplot
 from marsyas import *
 from marsyas_util import *
 
-# marsyas_util gives us, automatically, the line:
-# msm = marsyas.MarSystemManager()
 
-# This is a parenthesis-like notation, in which we will specify all the basic blocks in our topology.
-# The specification works like this:
-# Every time you specify a new subsystem, it should be preceded by a [, which will close when the specification ends
-# Also, all subsystems are specified in the following list, so, you will get things like this:
-# ["Begin/System", ["SystemItem1", "SystemItem2"]]
-# That is very similar to LISP, but with brackets instead of parenthesis.
+# The compact syntax for network specification is based 
+# on recursive specifying the network. The syntax is 
+# [name of composite pareant, [comma separated list of children]] 
+
 spec = ["Series/MySystem",
-					[["Fanout/additive",
-						["SineSource/src1",
-						"SineSource/src2"]],
-					"Sum/sum",
-					["Fanout/analyzers",
-						[["Series/branch1",
-							["Spectrum/spk","PowerSpectrum/pspk","Gain/gain"]],
-						["Series/branch2",
-							["Windowing/win","Spectrum/spk","PowerSpectrum/pspk","Gain/gain"]],
-						["Series/branch3",
-							["Windowing/win","Spectrum/spk","PowerSpectrum/pspk","Gain/gain"]]]]]]
+  	       [["Fanout/additive",
+	["SineSource/src1", "SineSource/src2"]],
+	"Sum/sum",
+                ["Fanout/analyzers",
+                      [["Series/branch1",
+		       ["Spectrum/spk",
+                        "PowerSpectrum/pspk",
+                        "Gain/gain"]],
+		["Series/branch2",
+			["Windowing/win",
+                         "Spectrum/spk",
+                         "PowerSpectrum/pspk",
+                         "Gain/gain"]],
+		["Series/branch3",
+			["Windowing/win",
+                         "Spectrum/spk",
+                         "PowerSpectrum/pspk",
+                         "Gain/gain"]]]]]]
 
-# We will call the create() function, which is defined in the marsyas_util.py file. It will create a marsystem just
-# as specified aboce.
+
+# The create function (defined in marsyas_util) takes 
+# as input the network specification and return 
+# the actual network that can be used for processing
 net = create(spec)
 
-# Now, we are going to link specific controls of our objects to top-level controls, so we can access them in
-# an easier way. Below, we will make code to access Fanout/additive/SineSource/src1/mrs_real/frequency
-# using simply mrs_real/frequency1
+
+# We can now create top-level controls for easier 
+# access to what we need. These provide the 
+# "client" interface to the functionality of the 
+# network. For example we will have shorter 
+# control names for the frequency controls of the sinusoids. 
 
 # make top level controls for the frequencies of the two oscillators 
-net.linkControl("Fanout/additive/SineSource/src1/mrs_real/frequency", "mrs_real/frequency1");
-net.linkControl("Fanout/additive/SineSource/src2/mrs_real/frequency", "mrs_real/frequency2");
+net.linkControl("Fanout/additive/SineSource/src1/mrs_real/frequency", 
+                "mrs_real/frequency1");
+net.linkControl("Fanout/additive/SineSource/src2/mrs_real/frequency", 
+                "mrs_real/frequency2");
 
-# You can link two or more controls to the same top-level control, so the same control will change parameters for multiple
-# controls in the network.
+# You can link two or more controls to the same top-level control, so
+# the same control will change parameters for multiple controls in the
+# network. For example map all spectrum types to the same top level control 
 
-# map all spectrum types to the same top level control 
-net.linkControl("Fanout/analyzers/Series/branch1/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
-net.linkControl("Fanout/analyzers/Series/branch2/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
-net.linkControl("Fanout/analyzers/Series/branch3/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
+net.linkControl(
+    "Fanout/analyzers/Series/branch1/PowerSpectrum/pspk/mrs_string/spectrumType",
+    "mrs_string/spectrumType")
+net.linkControl(
+    "Fanout/analyzers/Series/branch2/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
 
-#	# make top level controls for the 3 spectrums 
+net.linkControl(
+    "Fanout/analyzers/Series/branch3/PowerSpectrum/pspk/mrs_string/spectrumType","mrs_string/spectrumType")
+
+# make top level controls for accessing the 3 spectrums 
 net.linkControl("Fanout/analyzers/Series/branch1/PowerSpectrum/pspk/mrs_realvec/processedData","mrs_realvec/spectrum1")
 net.linkControl("Fanout/analyzers/Series/branch2/PowerSpectrum/pspk/mrs_realvec/processedData","mrs_realvec/spectrum2")
 net.linkControl("Fanout/analyzers/Series/branch3/PowerSpectrum/pspk/mrs_realvec/processedData","mrs_realvec/spectrum3")
 
-# Now, we will setup our analyzers in each analyzer branch. 
-# In branch1, we have no window, hence it is a rectangular-shaped window.
-# In branch2, we will have a hamming window, which is the default (hence it does not have to be changed)
-# In branch3, we will have a hann window, which is set below:
-
-#	# make the third branch window by a Hann window instead of the default Hamming 
+# Now, we will setup our analyzers in each analyzer branch.  In
+# branch1, we have no window, hence it is a rectangular-shaped window.
+# In branch2, we will have a hamming window, which is the default
+# (hence it does not have to be changed) In branch3, we will have a
+# hann window, which is set below:
 net.updControl("Fanout/analyzers/Series/branch3/Windowing/win/mrs_string/type", "Hann");
 
 
-# Using the getControl() method, we access the controls for a certain component of our net.
-# The following commands will set that we want our output for the spectrumTupe
-# (remember, we linked that to all three spectral analyzers above!)
+# Using the getControl() method, we access the controls of the 
+# different for a the components in the network
+# The following commands will set that we want our output for 
+# the spectrumTupe (remember, we linked that to all three spectral 
+# analyzers above!). This is an alternative syntax for setting 
+# control to the syntax above for setting the window type 
+
 spectrumType = net.getControl("mrs_string/spectrumType");
 spectrumType.setValue_string("decibels")
 
-# We are doing something similar here. Note that this is equivalent to using the net.updControl() method properly,
+# We are doing something similar here. Note that this is equivalent to
+# using the net.updControl() method properly,
 # but it might save you some work later. After you have executed this, uncomment the last two lines of this block and
 # see what the output is like!
 freq1 = net.getControl("mrs_real/frequency1");
 freq2 = net.getControl("mrs_real/frequency2");
 freq1.setValue_real(1500.0);
 freq2.setValue_real(3000.0);
-#net.updControl("mrs_real/frequency1", 2000.0);
-#net.updControl("mrs_real/frequency2", 4000.0);
+net.updControl("mrs_real/frequency1", 2000.0);
+net.updControl("mrs_real/frequency2", 4000.0);
 
-# These lines are important, because they will link an output control (the processedData) to a variable
-# which we can easily access.
+# These lines are important, because they will link an output control 
+# (the processedData) to a variable which we can easily access.
 outData1 = net.getControl("mrs_realvec/spectrum1");
 outData2 = net.getControl("mrs_realvec/spectrum2");
 outData3 = net.getControl("mrs_realvec/spectrum3");
