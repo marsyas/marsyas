a1a9da603f758fccce1fb732cca61de4014dca66
Jakob Leben <jakob.leben@gmail.com>
2014-02-24 18:25:37 -0800

    scripting: C++ API: allow user to provide MarSystemManager

--- src/marsyas/script/script.h
+++ src/marsyas/script/script.h
@@ -2,17 +2,19 @@
 #define MARSYAS_SCRIPT_INCLUDED
 
 #include <marsyas/system/MarSystem.h>
+#include <marsyas/system/MarSystemManager.h>
 #include <marsyas/export.h>
 #include <istream>
 
 namespace Marsyas {
 
 marsyas_EXPORT
-MarSystem *system_from_script(const std::string & filename);
+MarSystem *system_from_script(const std::string & filename, MarSystemManager *mng = 0);
 
 marsyas_EXPORT
 MarSystem *system_from_script(std::istream & script_stream,
-                              const std::string & working_directory = std::string());
+                              const std::string & working_directory = std::string(),
+                              MarSystemManager *mng = 0);
 
 } // namespace Marsyas
 
