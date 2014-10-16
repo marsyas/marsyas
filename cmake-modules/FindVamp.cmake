158bc9c6859b2e067304c00cf4bd91a00284a477
Jakob Leben <jakob.leben@gmail.com>
2014-10-16 13:15:57 -0700

    CMake: modernize FindVamp.cmake

--- cmake-modules/FindVamp.cmake
+++ cmake-modules/FindVamp.cmake
@@ -1,18 +1,6 @@
+include(FindPackageHandleStandardArgs)
+
 find_path(VAMP_INCLUDE_DIR vamp-sdk/Plugin.h)
 find_library(VAMP_LIBRARY NAMES vamp-sdk)
 
-if (VAMP_INCLUDE_DIR AND VAMP_LIBRARY)
-	set(VAMP_FOUND TRUE)
-endif (VAMP_INCLUDE_DIR AND VAMP_LIBRARY)
-
-if (VAMP_FOUND)
-	if (NOT VAMP_FIND_QUIETLY)
-		message (STATUS "Found VAMP: ${VAMP_LIBRARY}")
-	endif (NOT VAMP_FIND_QUIETLY)
-else (VAMP_FOUND)
-	if (VAMP_FIND_REQUIRED)
-		message (FATAL_ERROR "Could not find: VAMP")
-	endif (VAMP_FIND_REQUIRED)
-endif (VAMP_FOUND)
-
-
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Vamp DEFAULT_MSG VAMP_LIBRARY VAMP_INCLUDE_DIR)
