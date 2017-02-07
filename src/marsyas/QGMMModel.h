98461ff4e73c32a4b17754c5f8e44feaf3925081
George Tzanetakis <george.tzanetakis@smule.com>
2017-02-07 12:32:26 -0800

    Fixed warning in QGMMModel

--- src/marsyas/QGMMModel.h
+++ src/marsyas/QGMMModel.h
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 1998-2006 George Tzanetakis <gtzan@cs.uvic.ca>
+** Copyright (C) 1998-2016 George Tzanetakis <gtzan@cs.uvic.ca>
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-#if !defined(__QGMMMODEL_h)
+#if !defined(__QGMMMODEL_H)
 #define __QGMMMODEL_H
 
 #include <vector>
